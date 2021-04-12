3d835c546ffd05f5893a7fc128852fc333ca3e7b
heyset <c.inaciomatheus@gmail.com>
2021-04-12 16:12:42 -0300

    adiciona requisitos 8 e 9

--- script.js
+++ script.js
@@ -3,10 +3,26 @@ const addTaskButton = document.getElementById('criar-tarefa');
 
 const taskList = document.getElementById('lista-tarefas');
 
+let selectedTask;
+
+function selectTask(e) {
+  const task = e.target;
+  
+  task.style.backgroundColor = 'rgb(128, 128, 128)';
+  
+  if (selectedTask) {
+    selectedTask.style.backgroundColor = null;
+  }
+  selectedTask = task;
+}
+
 function addTask(text) {
-  taskList.insertAdjacentHTML('beforeend', 
-  `<li class="task">${text}</li>`
-  );
+  const newTask = document.createElement('li');
+  newTask.classList.add('task');
+  newTask.addEventListener('click', selectTask);
+  newTask.innerText = text;
+
+  taskList.appendChild(newTask);
 }
 
 function clearInput() {
@@ -24,4 +40,4 @@ addTaskInput.addEventListener('keydown', (e) => {
     addTask(addTaskInput.value);
     clearInput();
   }
-})
+});
