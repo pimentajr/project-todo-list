84ef4e50baddb0bacda30569d022ef6b406332ad
Hugo Mouto <hmoutol@gmail.com>
2021-04-12 20:06:44 -0300

    Critério 12

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-// get text input
+//// Load previous list
+window.onload = function(){
+  // plot items
+  taskList.innerHTML = fullListParsed;
+  console.log(fullListParsed)
+}
+
+//// Save Tasks
+function saveTasks() {
+  // prepare items to recover
+  fullListStringified = JSON.stringify(taskList.innerHTML)
+  localStorage.setItem('Saved List', fullListStringified)
+}
+
+ // get text input
 let taskInput = document.getElementById('texto-tarefa');
 // get task list
 let taskList = document.getElementById('lista-tarefas');
 // get submit button
 let submitButton = document.getElementById('criar-tarefa');
+// prepare items to store
+let fullListStringified = JSON.stringify(taskList.innerHTML);
+// store items
+let fullListParsed = JSON.parse(localStorage.getItem('Saved List')); 
 
-
-
-// Create New Task
+//// Create New Task
 function createNewTask(taskInputValue) {
 taskInputValue = taskInput.value
 let newTask = document.createElement('li');
@@ -16,7 +32,7 @@ taskList.appendChild(newTask);
 taskInput.value = '';
 }
 
-// Select item
+//// Select item
 taskList.addEventListener('click',function(event) {
 let listItem = document.getElementsByTagName('li');
 for (key of listItem){
@@ -24,17 +40,17 @@ for (key of listItem){
 }
 event.target.style.backgroundColor = 'rgb(128, 128, 128)'})
 
-// Strike-Unstrike Item
+//// Strike-Unstrike Item
 taskList.addEventListener('dblclick',function(event){
   event.target.classList.toggle('completed');
 })
 
-// Remove All
+//// Remove All
 function removeAllItems() {
   taskList.innerHTML = '';
 }
 
-// Remove Completed Items
+//// Remove Completed Items
 function removeCompleted() {
   let completedTasks = document.getElementsByClassName('completed'); 
   while (completedTasks.length > 0) {
@@ -42,5 +58,4 @@ function removeCompleted() {
     taskList.removeChild(completedTasks[0]);
   }
 }
-
 
