49071ad5b637b5b135e205ab01ed4d7d91ddedd5
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-12 16:56:03 -0300

    segunda funçao: selecionar toDo - lint corrections

--- script.js
+++ script.js
@@ -1,26 +1,23 @@
-const input = document.getElementById('texto-tarefa')
-const toDoList = document.querySelector('#lista-tarefas')
-const createToDo = document.getElementById('criar-tarefa')
-const toDos = document.querySelector("li")
+const input = document.getElementById('texto-tarefa');
+const toDoList = document.querySelector('#lista-tarefas');
+const createToDo = document.getElementById('criar-tarefa');
 
 function addToDo () {
   const toDo = document.createElement('li');
   toDo.innerText = input.value;
   toDoList.appendChild(toDo);
-  input.value = "";
+  input.value = '';
 }
 
 function toDoSelection (element) {
-  if(document.querySelector('.selected') === null){
-    if(element.target.parentNode === toDoList){
-      element.target.classList.add("selected")
-    }
-  }else{
-    if(element.target.parentNode === toDoList){
-      element.target.classList.remove("selected")
-    }
-  }
+  if (element.target.parentNode === toDoList) {
+    if (document.querySelector('.selected') === null) {
+      element.target.classList.add('selected')
+    }else{
+      element.target.classList.remove('selected')
+    };
+  };
 }
 
 createToDo.addEventListener('click', addToDo);
-addEventListener('click', toDoSelection)
+addEventListener('click', toDoSelection);
