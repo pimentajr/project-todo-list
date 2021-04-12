ea1766ccd5a5bccb93ad57a9b7a07589d584119c
Jose Antonio Moura Neto <netomoura22@gmail.com>
2021-04-12 16:53:34 -0300

    cumprimento do requisito 5 e 6

---
+++ script.js
@@ -0,0 +1,10 @@
+const caixaDeTexto = document.querySelector('#texto-tarefa');
+const botaoAdicionar = document.querySelector('#criar-tarefa');
+const listaDeTarefas = document.querySelector('#lista-tarefas');
+
+botaoAdicionar.addEventListener('click', event => {
+  const novoLi = document.createElement('li');
+  novoLi.innerText = caixaDeTexto.value;
+  listaDeTarefas.appendChild(novoLi);
+  caixaDeTexto.value = '';
+});
