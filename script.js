863cdcb6472f46e7a14e71ade9b33026c070bc04
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-12 16:47:24 -0300

    Corrige erros do lint - requisito 5 e 6

--- script.js
+++ script.js
@@ -2,22 +2,19 @@
 // Para adição de enter na lista, foi consultada a documentação em W3Schools sobre ações usando o "Enter"
 // Source: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 function addToList() {
-  let input = document.getElementById('texto-tarefa');
-  let addButton = document.getElementById('criar-tarefa');
-  let list = document.getElementById('lista-tarefas');
-
-  //Usando click
+  const input = document.getElementById('texto-tarefa');
+  const addButton = document.getElementById('criar-tarefa');
+  const list = document.getElementById('lista-tarefas');
   addButton.addEventListener('click', () => {
-    let li = document.createElement('li');
+    const li = document.createElement('li');
     li.innerText = input.value;
     input.value = '';
     list.appendChild(li);
   });
-
-  //Usando enter
+  // Usando enter
   input.addEventListener('keyup', (event) => {
-    if(event.keyCode === 13) {
-      let li = document.createElement('li');
+    if (event.keyCode === 13) {
+      const li = document.createElement('li');
       li.innerText = input.value;
       input.value = '';
       list.appendChild(li);
