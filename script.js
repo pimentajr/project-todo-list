6126a43612edba959198f4b7280a6ef228f132c9
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-12 20:21:40 -0300

    Passo 9

--- script.js
+++ script.js
@@ -15,21 +15,37 @@ function addListItem() {
 
 function removeSelect() {
   for (let i = 0; i < list.children.length; i += 1) {
-    list.children[i].className = 'tarefa';
+    list.children[i].classList.remove('selected');
     list.children[i].style.backgroundColor = document.body.style.backgroundColor;
   }
 }
 
 function selectItem() {
+  list.addEventListener('click', (e) => {
+    removeSelect();
+    if (e.target.className !== 'tarefa selected') {
+      e.target.classList.add('selected');
+      e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    }
+  });
+}
 
-    list.addEventListener('click', (e) => {
-      removeSelect();
-      if (e.target.className !== 'tarefa selected') {
-        e.target.classList.add('selected');
-        e.target.style.backgroundColor = 'rgb(128, 128, 128)';
-      }
-    });
+function removeComplete() {
+  for (let i = 0; i < list.children.length; i += 1) {
+    list.children[i].classList.remove('completed');
+  }
+}
+
+function completeItem() {
+  list.addEventListener('dblclick', (e) => {
+    if (e.target.classList.contains('completed')) {
+      removeComplete();
+    } else if (e.target.className !== 'tarefa completed') {
+      e.target.classList.add('completed');
+    }
+  });  
 }
 
 addListItem();
 selectItem();
+completeItem();
