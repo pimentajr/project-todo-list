70c3f7e7164c8bea28b2a3b6c278798641385806
VictorS <czpask@gmail.com>
2021-04-12 17:38:58 -0300

    Correcao lint

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 const getInput = document.querySelector('#texto-tarefa');
 const getButton = document.querySelector('#criar-tarefa');
-const getLista = document.querySelector('#lista-tarefas')
-const getLiItems = getLista.childNodes;
+const getLista = document.querySelector('#lista-tarefas');
 
-//funcao botao criar tarefa
+// funcao botao criar tarefa
 getButton.addEventListener('click', function () {
   const createLi = document.createElement('li');
   createLi.innerText = getInput.value;
@@ -11,35 +10,37 @@ getButton.addEventListener('click', function () {
   getInput.value = '';
 });
 
-//funcao selecionar tarefa e mudar a cor dela
+// funcao selecionar tarefa e mudar a cor dela
 getLista.addEventListener('click', function (event) {
   const getLiItems = getLista.childNodes;
   for (let i = 0; i < getLiItems.length; i += 1) {
     getLiItems[i].style.backgroundColor = 'white';
   }
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-})
+});
 
-//funcao doubleclick para riscar item
+// funcao doubleclick para riscar item
 getLista.addEventListener('dblclick', function (event) {
   if (event.target.className === 'completed') {
     event.target.className = '';
   } else {
     event.target.className = 'completed';
   }
-})
+});
 
-//funcao botao que apaga lista
+// funcao botao que apaga lista
 const dltButton = document.querySelector('#apaga-tudo');
-dltButton.addEventListener('click', function() {
+dltButton.addEventListener('click', function () {
   getLista.innerText = '';
-})
+});
 
-//funcao botao que apaga riscados
+// funcao botao que apaga riscados
 const dltCmpltButton = document.querySelector('#remover-finalizados');
-dltCmpltButton.addEventListener('click', function() {
-  let getComplets = document.querySelectorAll('.completed');
+dltCmpltButton.addEventListener('click', function () {
+  const getComplets = document.querySelectorAll('.completed');
   for (let i = 0; i < getComplets.length; i += 1) {
     getLista.removeChild(getComplets[i]);
   }
-})
+});
+
+
