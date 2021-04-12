cd7accf6bf85278207677e79c65f335e8b868644
Alberto Candido <albertocandido98@gmail.com>
2021-04-12 16:08:25 -0300

    Requisitos 7 e 8 - add funçao de selecionar tarefas da lista

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ function addTask() {
   const task = document.getElementById('texto-tarefa');
   const taskList = document.querySelector('ol');
   button.addEventListener('click', () => {
-    console.log('tes');
     const newTask = document.createElement('li');
     newTask.innerText = task.value;
     taskList.appendChild(newTask);
@@ -12,3 +11,17 @@ function addTask() {
 }
 
 addTask();
+
+function selectTask() {
+  const taskList = document.querySelector('ol');
+  taskList.addEventListener('click', (event) => {
+    const task = document.getElementsByTagName('li');
+    for (let i = 0; i < task.length; i += 1) {
+      task[i].classList.remove('selected');
+    }
+    const taskSelected = event.target;
+    taskSelected.classList.add('selected');
+  });
+}
+
+selectTask();
