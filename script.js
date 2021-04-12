3729aff2255e25a34a97cac19cd7a1f0b48b41db
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-12 15:31:01 -0300

    exercicio 2

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // Cria um Header
 function addHeader() {
-  let header = document.createElement('header')
-  header.id = 'header'
-  document.body.appendChild(header)
-  let title = document.createElement ('h1')
-  title.innerText = 'Minha Lista de Tarefas'
-  document.querySelector('#header').appendChild(title)
+  const header = document.createElement('header');
+  header.id = 'header';
+  document.body.appendChild(header);
+  const title = document.createElement('h1');
+  title.innerText = 'Minha Lista de Tarefas';
+  document.querySelector('#header').appendChild(title);
 }
 addHeader();
+
+// Cria uma section com um parágrafo e texto
+//2 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+function addFirstSection() {
+  const section = document.createElement('section');
+  section.id = 'firstsection';
+  document.body.appendChild(section);
+  const paragraph = document.createElement('p');
+  paragraph.id = 'funcionamento';
+  paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+  section.appendChild(paragraph);
+}
+addFirstSection();
