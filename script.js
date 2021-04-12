80c7fb3006458b2e8e509f93668c9ce9e9a03855
Filizzola <juliofilizzola@hotmail.com>
2021-04-12 17:30:08 -0300

    desafio 7

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 const list = document.querySelector('#lista-tarefas');
 const inputV = document.querySelector('#texto-tarefa');
-
-
+const liList = document.querySelectorAll('.list');
 
 // Add Button
 function createButton() {
   const classPai = document.querySelector('.button');
   const creatB = document.createElement('button');
   creatB.id = 'criar-tarefa';
+  creatB.className = 'list';
   creatB.innerHTML = 'Adicionar Item a Lista';
   classPai.appendChild(creatB);
 }
 
 createButton();
 
-
-
-const buttonADD = document.querySelector('#criar-tarefa')
+const buttonADD = document.querySelector('#criar-tarefa');
 // Função para pegar o valor do input e colocar dentro de uma li
 buttonADD.addEventListener('click', () => {
- 
   const creatLi = document.createElement('li');
   creatLi.innerHTML = inputV.value;
   list.appendChild(creatLi);
   inputV.value = '';
 });
 
-
+// add bg color.
+list.addEventListener('click', (event) => {
+  for (let index = 0; index < liList.length; index += 1) {
+    list[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+});
