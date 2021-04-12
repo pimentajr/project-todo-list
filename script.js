66612c4441b7f5b5ca3e6949a8bf3bee6d7cc477
Víctor Faria <vitilevi@hotmail.com>
2021-04-12 16:11:17 -0300

    Feito requisito 7 e 8

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const liItens = document.getElementsByTagName('li');
+
 // Create li element
 function createAndAddLiItem() {
   const orderedList = document.getElementById('lista-tarefas');
@@ -7,11 +9,27 @@ function createAndAddLiItem() {
   createLi.innerText = text;
   orderedList.appendChild(createLi);
   inputText.value = '';
+  createLiEventListener();
+}
+
+// Add selected class to Li
+function selectedItem(event) {
+  for (let index = 0; index < liItens.length; index += 1) {
+    liItens[index] = liItens[index].classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
+function createLiEventListener() {
+  for (let index = 0; index < liItens.length; index += 1) {
+    liItens[index].addEventListener('click', selectedItem);
+  }
 }
 
 function createEventlisteners() {
   const createActivity = document.getElementById('criar-tarefa');
   createActivity.addEventListener('click', createAndAddLiItem);
+  createLiEventListener();
 }
 
 function init() {
