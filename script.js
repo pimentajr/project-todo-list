084ff97893279cafcb55f457a40fdaa0adaf33e6
Renan <renan.vamo@gmail.com>
2021-04-12 17:00:03 -0300

    Ao clicar no item da lista, mudar o bgcolor

--- script.js
+++ script.js
@@ -1,16 +1,28 @@
 // Cria as variáveis recorrentes
 const createTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
-console.log(taskList);
 
 // Cria um novo item da lista OL e limpa o campo de input
 function createNewTask() {
   createTask.addEventListener('click', () => {
     let newTask = document.getElementById('texto-tarefa');
     const newLi = document.createElement('li');
+    newLi.className = 'item'
     taskList.appendChild(newLi);
     newLi.innerText = newTask.value;
     newTask.value = '';
+    newTask.focus()
   });
 }
 createNewTask();
+
+// Ao clicar no item da lista, ele finca com fundo rgb(128,128,128)
+function taskSelector() {
+   taskList.addEventListener('click', (event) => {
+    let items = document.querySelector('item');
+    if (event.target.className === 'item') {
+      event.target.style.backgroundColor = 'rgb(128,128,128)';
+   }
+  });
+}
+taskSelector();
