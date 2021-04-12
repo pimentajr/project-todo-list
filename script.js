b51e24eb0fd7ba9f952d22af8d0d08dd320d62de
Vitor <vitorguima@gmail.com>
2021-04-12 17:50:56 -0300

    inclusao exercicio 8

--- script.js
+++ script.js
@@ -13,3 +13,30 @@ function addNewTask() {
 }
 
 addNewTask();
+
+// remove selected class e cor de fundo
+function removeBackgroundColor() {
+  const getSelectedColor = document.querySelectorAll('.selected');
+  
+  if (getSelectedColor.length > 0) {
+    for (let index = 0; index < getSelectedColor.length; index += 1) {
+      getSelectedColor[index].style.backgroundColor = 'white';
+      getSelectedColor[index].classList.remove('selected');
+    }
+  }
+}
+
+// altera cor de fundo do item da lista
+function changeTaskBackground() {
+  const taskList = document.querySelector('#lista-tarefas');
+
+  taskList.addEventListener('click', function (event) {
+    removeBackgroundColor();
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    event.target.classList.add('selected');
+  })
+}
+
+changeTaskBackground();
+
+
