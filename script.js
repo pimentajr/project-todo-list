c1a958d1ecaa8b60bb53d5a8740eda24036a59ac
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-12 15:04:08 -0300

    Adiciona requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const header = document.createElement('header');
 header.innerHTML = '<strong>Minha Lista de Tarefas</strong>';
 document.body.appendChild(header);
+
+const paragraph = document.createElement('p');
+paragraph.id = 'funcionamento';
+paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+document.body.appendChild(paragraph);
