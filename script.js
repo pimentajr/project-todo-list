400b48c557ac2bbd1eadad81e6a06a60bfef0235
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-12 18:49:03 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -3,12 +3,23 @@ function addTask() {
   const taskList = document.getElementById('lista-tarefas');
   const task = document.createElement('li');
   taskList.appendChild(task);
-  let textTask = input;
+  const textTask = input;
   task.innerText = textTask;
-  document.getElementById('texto-tarefa').value = ''; 
+  document.getElementById('texto-tarefa').value = '';
 }
 
 const buttonAddTask = document.getElementById('criar-tarefa');
 
 buttonAddTask.addEventListener('click', addTask);
 
+const taskList = document.getElementById('lista-tarefas');
+
+function backgroundClassItem(event) {
+  let itens = taskList.children;
+  for (let index = 0; index < itens.length; index += 1) {
+    itens[index].classList.remove('selected');
+  }  
+  event.target.className = 'selected';
+}
+
+taskList.addEventListener('click', backgroundClassItem);
