028b1c508bf25e9c019e674223c3d5d50221a9e6
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-04-12 17:13:52 -0300

    add exercise 7 - alter backGroungColor task

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
 const btnCriarTarefa = document.querySelector('#criar-tarefa');
 const listaTarefas = document.querySelector('#lista-tarefas');
-const textoTarefa = document.querySelector('#texto-tarefa');
+const textTask = document.querySelector('#texto-tarefa');
 
-window.onload = function() {
-  function createTarefa() {
-    btnCriarTarefa.addEventListener('click', function() {
-      const createLi = document.createElement('li');
-      createLi.innerHTML = textoTarefa.value;
-      listaTarefas.appendChild(createLi);
-      textoTarefa.value = '';
-    })
-  }
-  createTarefa();
+
+function createTask() {
+  btnCriarTarefa.addEventListener('click', () => {
+    const createLi = document.createElement('li');
+    createLi.innerHTML = textTask.value;
+    createLi.className = 'li-assignment'
+    listaTarefas.appendChild(createLi);
+    textTask.value = '';
+  });
 }
+
+function alterBackColorTask() {
+  const liAssignment = document.querySelector('#lista-tarefas');
+    liAssignment.addEventListener('click', (event) => {
+      const selectedTask = document.getElementsByClassName('selected');
+      if (selectedTask[0]) {
+        selectedTask[0].classList.remove('selected');
+      }
+      event.target.classList.add('selected');
+    });
+};
+
+window.onload = () => {
+  createTask();
+  alterBackColorTask();
+};
