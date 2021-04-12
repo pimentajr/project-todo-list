4456b1d41a96fd363ccad757a7da73aedaaab46d
josanjohnata <josanjohnata@gmail.com>
2021-04-12 20:12:54 -0300

    add grey line double click

--- script.js
+++ script.js
@@ -8,5 +8,34 @@ function newListItem() {
         document.getElementById('lista-tarefas').appendChild(newLI);
     }
     document.getElementById('texto-tarefa').value = '';
-
 }
+
+let toDoList = document.querySelector('#lista-tarefas');
+let countLines = document.getElementsByTagName('li');
+toDoList.addEventListener('click', lineColor);
+
+function lineColor(event) {
+  for (index = 0; index < countLines.length; index += 1) {
+    countLines[index].style.backgroundColor = 'transparent';
+  }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+}
+toDoList.addEventListener('dblclick', doneTask);
+
+function doneTask(event) {
+  let trace = event.target;
+  let lineStrike = window.getComputedStyle(trace).getPropertyValue('  ');
+  console.log(lineStrike);
+  if (lineStrike === 'line-through solid rgb(0, 0, 0') {
+    event.target.className = '';
+  } else {
+    event.target.className = 'completed'
+  }
+}
+
+// let lista = document.querySelector('ol');
+// list.addEventListener('dblclick', function(event) {
+//   if (event.target.tagName === 'li') {
+//     event.target.classList.toggle('checked');
+//   }
+// }, false);
