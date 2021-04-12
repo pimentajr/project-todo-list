27ab01924f32ea0de2d4dd3b2baabd2ff01b41ae
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-04-12 16:25:54 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -3,6 +3,22 @@ const newTaskInput = document.querySelector('#texto-tarefa');
 const tasksList = document.querySelector('#lista-tarefas');
 const clearAllBtn = document.querySelector('#apaga-tudo');
 const removeFinishedBtn = document.querySelector('#remover-finalizados');
+const saveTasksBtn = document.querySelector('#salvar-tarefas');
+
+window.onload = function () {
+  let savedTasks = [];
+  let savedTasksClass = [];
+  savedTasks = localStorage.getItem('tasks');
+  savedTasks = savedTasks.split(',');
+  savedTasksClass = localStorage.getItem('task-class');
+  savedTasksClass = savedTasksClass.split(',');
+  for (let index = 0; index < savedTasks.length; index += 1) {
+    const newTaskListItem = document.createElement('li');
+    newTaskListItem.innerText = savedTasks[index];
+    newTaskListItem.className = savedTasksClass[index];
+    tasksList.appendChild(newTaskListItem);
+  }
+}
 
 function addNewTask() {
   if (newTaskInput.value !== '') {
@@ -47,7 +63,23 @@ function clearFinishedTasks() {
   }
 }
 
-[createTaskBtn, tasksList, clearAllBtn, removeFinishedBtn].forEach((item) => {
+function saveCurrentTasks() {
+  const allTasks = document.querySelectorAll('#lista-tarefas li');
+  if (allTasks.length !== 0) {
+    let tasksToSave = [];
+    let classToSave = [];
+    for (let index = 0; index < allTasks.length; index += 1) {
+      tasksToSave[index] = allTasks[index].innerText;
+      classToSave[index] = allTasks[index].className;
+    }
+    localStorage.setItem('tasks', tasksToSave);
+    localStorage.setItem('task-class', classToSave);
+  } else {
+    localStorage.clear();
+  }
+}
+
+[createTaskBtn, tasksList, clearAllBtn, removeFinishedBtn, saveTasksBtn].forEach((item) => {
   item.addEventListener('click', (event) => {
     if (item === createTaskBtn) {
       addNewTask();
@@ -57,6 +89,8 @@ function clearFinishedTasks() {
       clearAllTasks();
     } else if (item === removeFinishedBtn) {
       clearFinishedTasks();
+    } else if (item === saveTasksBtn) {
+      saveCurrentTasks();
     }
   });
 });
@@ -68,3 +102,5 @@ function clearFinishedTasks() {
     }
   });
 });
+
+
