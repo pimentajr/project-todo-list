60b0426757eee0784af2d85b913e93843b7525fd
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-12 15:07:40 -0300

    requisito 12 e 13 feitos

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const getLi = document.getElementsByClassName('list');
 const getClearButton = document.querySelector('#apaga-tudo');
 const getCompletedButton = document.querySelector('#remover-finalizados');
 const getCompletedCss = document.getElementsByClassName('completed');
+const getSaveButton = document.querySelector('#salvar-tarefas');
+const getMoveUpButton = document.querySelector('#mover-cima');
+const getMoveDownButton = document.querySelector('#mover-baixo');
 
 function createList(value) {
   const createLi = document.createElement('li');
@@ -69,3 +72,40 @@ removeCompletedButton();
 /* stackoverflow = https://stackoverflow.com/questions/4777077/removing-elements-by-class-name */
 /* documentação while = https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while */
 
+function saveTasks() {
+  getSaveButton.addEventListener('click', () => {
+    localStorage.setItem('saveTask', getOl.innerHTML);
+  });
+  const saved = localStorage.getItem('saveTask');
+  if (saved) getOl.innerHTML = saved;
+}
+/* save with localStorage = https://gomakethings.com/saving-html-to-localstorage-with-vanilla-js/ */
+
+function moveUpList() {
+  getMoveUpButton.addEventListener('click', () => {
+    for (let index = 0; index < getLi.length; index += 1) {
+      if (getLi[index].style.backgroundColor === 'rgb(128, 128, 128)' && getLi[index].previousElementSibling !== null) {
+        const getIndex = getLi[index];
+        getOl.insertBefore(getIndex, getIndex.previousSibling)
+      }
+    }
+  })
+}
+
+function moveDownList() {
+  getMoveDownButton.addEventListener('click', () => {
+    for (let index = getLi.length - 1; index >= 0; index -= 1) {
+      if (getLi[index].style.backgroundColor === 'rgb(128, 128, 128)' && getLi[index].nextElementSibling !== null) {
+        const getIndex2 = getLi[index]
+        getOl.insertBefore(getIndex2.nextSibling, getIndex2)
+      }
+    }
+  })
+}
+/* mover itens = https://www.codegrepper.com/code-examples/javascript/javascript+move+list+items+up+and+down */
+
+window.onload = () => {
+  saveTasks();
+  moveUpList();
+  moveDownList();
+};
