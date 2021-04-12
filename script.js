e230c452568f1063a3f4c23d040f09e7dc6a4a28
Tales Coelho <tales-coelho@live.com>
2021-04-12 15:22:35 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Cria item de lista
 function createTasks(textoTarefa, criarTarefa, listaTarefa) {
   criarTarefa.addEventListener('click', () => {
     let task = document.createElement('li');
@@ -5,14 +6,24 @@ function createTasks(textoTarefa, criarTarefa, listaTarefa) {
     console.log(textoTarefa.value);
     listaTarefa.appendChild(task);
     textoTarefa.value = '';
-  })
+  });
 }
 
-//Carregar meus arquivos ao carregar a página
+// Altera a cor de fundo do item clicado
+function changeBackgroundListColor(listaTarefa) {
+  listaTarefa.addEventListener('click', (event) => {
+    if (event.target.localName === 'li') {
+      event.target.style.backgroundColor = `rgb(128, 128, 128)`;
+    }
+  });
+}
+
+// Carregar meus arquivos ao carregar a página
 window.onload = () => {
   const textoTarefa = document.getElementById('texto-tarefa');
   const criarTarefa = document.getElementById('criar-tarefa');
   const listaTarefa = document.getElementById('lista-tarefas');
   createTasks(textoTarefa, criarTarefa, listaTarefa);
+  changeBackgroundListColor(listaTarefa);
 }
 
