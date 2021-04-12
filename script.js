256d07300464d4bf2f8f5d77ab9569c606b249b7
Amandha W. Barok <amandha.barok@gmail.com>
2021-04-12 13:33:16 -0700

    Exercício 7

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-const button = document.getElementById('criar-tarefa');
+let button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addButton);
 
 function addButton() {
-  let olList = document.getElementById('lista-tarefas');
-  let inputTextElement = document.getElementById('texto-tarefa');
-  let inputText = inputTextElement.value;
+  // pegando os elementos
+  const olList = document.getElementById('lista-tarefas');
+  const inputTextElement = document.getElementById('texto-tarefa');
+  // pegando o valor (texto) que está dentro do input
+  const inputText = inputTextElement.value;
+  // criando LI
   let createLi = document.createElement('li');
+  createLi.classList.add('listItem');
+  // atribuindo o INNERTEXT para o olList
   createLi.innerText = inputText;
+  createLi.addEventListener('click', changeColor);
+
   olList.appendChild(createLi);
+  // limpando o elemento:
   inputTextElement.value = '';
+}
+
+function changeColor(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
