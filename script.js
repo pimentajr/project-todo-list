4241fa8cfa73d0bc5f1303872f12a87525fafbd4
Gustavo Lemes <gustavolemes997@gmail.com>
2021-04-12 19:44:00 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const olList = document.querySelector('#lista-tarefas');
 const btnDeleteList = document.querySelector('#apaga-tudo');
 const btnDeleteCompleted = document.querySelector('#remover-finalizados');
 const btnDeleteSelected = document.querySelector('#remover-selecionado');
+const btnSaveTasks = document.querySelector('#salvar-tarefas');
 
 function paintSelectedELement(event) {
   const element = event.target;
@@ -33,10 +34,23 @@ function changeSelected(event) {
 function lineThrough(event) {
   const element = event.target;
 
-  if (element.className === 'selected') {
+  if (!element.classList.contains('completed')) {
     element.classList.add('completed');
-  } else if (element.className === 'selected completed') {
+  } else {
     element.classList.remove('completed');
+  }
+}
+
+function checkClassNull() {
+  const listItem = document.querySelectorAll('#list-item');
+  for (let index = 0; index < listItem.length; index += 1) {
+    if (
+      listItem[index].classList === null || listItem[index].className === 'null'
+    ) {
+      listItem[index].removeAttribute('class');
+    } else if (listItem[index].classList.contains('null')) {
+      listItem[index].classList.remove('null');
+    }
   }
 }
 
@@ -77,3 +91,33 @@ btnDeleteSelected.addEventListener('click', () => {
     listItem[index].remove();
   }
 });
+
+btnSaveTasks.addEventListener('click', () => {
+  const listItem = document.querySelectorAll('#list-item');
+  const listItemAttributes = [];
+
+  for (let index = 0; index < listItem.length; index += 1) {
+    listItemAttributes.push({
+      id: listItem[index].getAttribute('id'),
+      class: listItem[index].getAttribute('class'),
+      text: listItem[index].innerText,
+    });
+  }
+  localStorage.setItem('listItems', JSON.stringify(listItemAttributes));
+});
+
+function batatatinha() {
+  const storageTasks = JSON.parse(localStorage.getItem('listItems'));
+
+  for (let index = 0; index < storageTasks.length; index += 1) {
+    const createTaskStorage = document.createElement('li');
+    createTaskStorage.setAttribute('class', storageTasks[index].class);
+    createTaskStorage.setAttribute('id', storageTasks[index].id);
+    createTaskStorage.innerText = storageTasks[index].text;
+
+    olList.appendChild(createTaskStorage);
+  }
+  checkClassNull();
+}
+
+window.onload = batatatinha;
