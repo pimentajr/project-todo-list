4636871b9525694c590c2a818fd542e0b153402a
Renan <renan.vamo@gmail.com>
2021-04-12 17:40:22 -0300

    Cria os botoes por JS

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 // Cria as variáveis recorrentes
-const createTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+const buttonArea = document.getElementById('btn-area');
 
+// Criar o botão de input
+function createTextInput() {
+  let inputText = document.createElement('input');
+  inputText.type = 'text';
+  inputText.placeHolder = 'Insira a nova tarefa:';
+  inputText.id = 'texto-tarefa';
+  buttonArea.appendChild(inputText);
+  let inputButton = document.createElement('input');
+  inputButton.type = 'button';
+  inputButton.id = 'criar-tarefa';
+  inputButton.value = 'Criar Tarefa'
+  buttonArea.appendChild(inputButton);
+}
+createTextInput();
+
+const createTask = document.getElementById('criar-tarefa');
 
 // Cria um novo item da lista OL e limpa o campo de input
 function createNewTask() {
   createTask.addEventListener('click', () => {
-    let newTask = document.getElementById('texto-tarefa');
+    const newTask = document.getElementById('texto-tarefa');
     const newLi = document.createElement('li');
     newLi.className = 'item';
     taskList.appendChild(newLi);
@@ -21,11 +37,11 @@ createNewTask();
 function taskSelector() {
   taskList.addEventListener('click', (event) => {
     if (event.target.className === 'item') {
-      let previousSelectedItem = document.querySelector('.selected');
+      const previousSelectedItem = document.querySelector('.selected');
       if (previousSelectedItem) previousSelectedItem.classList.remove('selected');
-      let selectedItem = event.target;
+      const selectedItem = event.target;
       selectedItem.classList.add('selected');
-   }
+    }
   });
 }
 taskSelector();
