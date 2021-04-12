42d3a69eaa1a84bc8d623ce29020c4a0792ba179
Raphael <raphaellfpg@gmail.com>
2021-04-12 15:42:30 -0300

    Requisito 5 correcao 2

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const buttom = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
-const lista = document.querySelector('#lista-tarefas') ;
+const lista = document.querySelector('#lista-tarefas');
 
 function addList() {
   buttom.addEventListener('click', function toDo() {
-    lista.appendChild(document.createElement('li')).innerHTML = input.value
+    lista.appendChild(document.createElement('li')).innerHTML = input.value;
+    input.value = ''
   });
 }
 addList();
