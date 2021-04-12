ec1eaa573ebb66fbcf7069b3425bb4e95cd04d7a
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-04-12 15:53:26 -0300

    Adicionada a tag ol no html atendendo ao requisito 4.

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ let button = document.getElementById('criar-tarefa');
 // Criar uma função que adiciona um elemento na tela
 // primeiro pego o  main
 const mainProject = document.getElementById('main');
-// feito isso inicio a construção da função. Primeiro criar o elemento lista
-const taskList = document.createElement('ol');
+// feito isso inicio a construção da função. Primeiro pegar o elemento ol do dom
+const taskList = document.getElementById('lista-tarefas');
 // depois criamos o elemento que irá ser cada iten da lista, a tag li
 let itenList = document.createElement('li');
 // Agora precisamos adicionar o conteudo do input na tag li
 itenList.innerText = inputValue;
-// Agora é necessario vincular a lli em uma tag do dom.
+// Agora é necessario vincular a li na tag ol
 
 // Feito o vinculo agora é necessario transformar tudo em uma função.
 
