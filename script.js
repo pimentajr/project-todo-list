4b8d29e8b18a4e2ef6e4c1a1a89fed529c147089
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-04-12 17:43:45 -0300

    task 2

--- script.js
+++ script.js
@@ -8,14 +8,28 @@ function add(father, child) {
   return father.appendChild(child);
 }
 
+// Task 1
+// body and header declaration
 const { body } = document;
 const header = create('header');
 add(body, header);
 
+// add text to header
 header.innerText = 'Minha Lista de Tarefas';
 
+// Task 2
+// description declaration
+const descrp = create('p');
+add(body, descrp);
+
+// add text to descrp
+descrp.id = 'funcionamento';
+descrp.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+// start function
 function start() {
 
 }
 
+// window.onload
 window.onload = () => start();
