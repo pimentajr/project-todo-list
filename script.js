f035d998eace5640340e265286feb53ef4d91cc0
Hamander <1hamander@gmail.com>
2021-04-12 15:10:20 -0300

    Requisito_4 Completo. Lista Posicionada abaixo do input.

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 function assignmentTextPlaceHolder() {
   const textHolder = document.createElement('input');
-  const textHolderPlacement = document.querySelector('#section-interactive')
+  const textHolderPlacement = document.querySelector('#section-interactive');
   textHolderPlacement.appendChild(textHolder);
   textHolder.id = 'texto-tarefa';
 }
-assignmentTextPlaceHolder()
+assignmentTextPlaceHolder();
+
+function assigmentList() {
+  const listHolder = document.createElement('ol')
+  const listHolderPlacement = document.querySelector('#section-interactive');
+  listHolderPlacement.appendChild(listHolder);
+  textHolder.id = 'lista-tarefas'
+}
+assigmentList();
