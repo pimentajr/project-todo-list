a0f25a87be65342b8b64124c5bc098be6cf120de
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-04-12 17:37:01 -0300

    Requisito 9 - Concluido

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
 const list = document.querySelector('#lista-tarefas');
-
-function addNewTask() {
-  const input = document.querySelector('#texto-tarefa');
-  const itemList = document.createElement('li');
-  itemList.innerText = input.value;
-  input.value = '';
-  list.appendChild(itemList);
-}
+const input = document.querySelector('#texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
 
 function newTask() {
-  const button = document.querySelector('#criar-tarefa');
+  function addNewTask() {
+    const itemList = document.createElement('li');
+    itemList.innerText = input.value;
+    input.value = '';
+    list.appendChild(itemList);
+  }
   button.addEventListener('click', addNewTask);
 }
 
 function clearSelected() {
   for (let index = 0; index < list.children.length; index += 1) {
-    list.children[index].className = '';
+    list.children[index].classList.remove('bg-list');
   }
 }
 
-function addNewColor(event) {
-  const selected = event.target;
-  clearSelected();
-  selected.className = 'bg-list';
+function selectedTask() {
+  function addNewColor(event) {
+    const selected = event.target;
+    clearSelected();
+    selected.classList.add('bg-list');
+  }
+  list.addEventListener('click', addNewColor);
 }
 
-function selectedTask() {
-  list.addEventListener('click', addNewColor);
+function taskCompleted() {
+  function taskLine(event) {
+    const selected = event.target;
+    if (selected.classList[0] === 'completed' || selected.classList[1] === 'completed') {
+      selected.classList.remove('completed');
+    } else {
+      selected.classList.add('completed');
+    }
+  }
+  list.addEventListener('dblclick', taskLine);
 }
 
 newTask();
 selectedTask();
+taskCompleted();
