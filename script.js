21ee1d99412710bf5da46a196e76e2bb5626a02e
Víctor Faria <vitilevi@hotmail.com>
2021-04-12 15:06:44 -0300

    Feito requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
-function init() {
+// Create li element
+function createAndAddLiItem() {
+  const orderedList = document.getElementById('lista-tarefas');
+  const createLi = document.createElement('li');
+  const inputText = document.getElementById('texto-tarefa');
+  const text = inputText.value;
+  createLi.innerText = text;
+  orderedList.appendChild(createLi);
+  inputText.value = '';
+}
 
+function createEventlisteners() {
+  const createActivity = document.getElementById('criar-tarefa');
+  createActivity.addEventListener('click', createAndAddLiItem);
+}
+
+function init() {
+  createEventlisteners();
 }
 
 window.onload = init;
