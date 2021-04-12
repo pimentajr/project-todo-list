9d24d45da71bb976926a27eff27a067ef00d001d
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-04-12 20:41:38 -0300

    req7

--- script.js
+++ script.js
@@ -2,22 +2,27 @@ const createTask = document.getElementById('criar-tarefa');
 const inputText = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
-
-
 // limpando o input
 function clearInput() {
     inputText.value = '';
 }
 // adicionando nova li
-function newTask () {
+function newTask() {
   const li = document.createElement('li');
-  let text = inputText.value;
+  const text = inputText.value;
   li.classList.add('listItem');
   taskList.appendChild(li);
   li.innerHTML = text;
 }
+// selecionando item da lista
+function selectedSwitch(event) {
+  document.querySelectorAll('.listItem')[0].classList.remove('selected');
+  const eventTarget = event.target;
+  eventTarget.className += ' selected';
+}
+
+taskList.addEventListener('click',selectedSwitch);
 
 
-
-// createTask.addEventListener('click', newTask);
+createTask.addEventListener('click', newTask);
 createTask.addEventListener('click', clearInput);
