23ca9b944ca99d4fa1a4dbe1cf3efe27fdf0863c
Matheus Gaspar <msouzagaspar@gmail.com>
2021-04-12 18:49:19 -0300

    requisito 10: adiciona botao para limpar lista

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const inputElement = document.getElementById('texto-tarefa');
 const toDoList = document.getElementById('lista-tarefas');
 const newTaskButton = document.getElementById('criar-tarefa');
+const clearAllButton = document.getElementById('apaga-tudo');
 
 function newTask() {
   const task = document.createElement('li');
@@ -23,9 +24,14 @@ function toggleCompletedTask(event) {
   task.classList.toggle('completed');
 }
 
+function deleteAllTasks() {
+  toDoList.querySelectorAll('li').forEach((e) => e.remove());
+}
+
 newTaskButton.addEventListener('click', newTask);
 inputElement.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') newTaskButton.click();
 });
 toDoList.addEventListener('click', selectTask);
 toDoList.addEventListener('dblclick', toggleCompletedTask);
+clearAllButton.addEventListener('click', deleteAllTasks);
