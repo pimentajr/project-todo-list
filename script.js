04ecc41a658ec6d235985272afdac00b96ef9ae5
Gisele Costa  <jikinha@gmail.com>
2021-04-12 19:04:34 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -4,24 +4,34 @@ const taskList = document.querySelector('#lista-tarefas');
 
 // Adiciona nova tarefa
 function addTask(text) {
-  const newTask = document.createElement('li');
-  newTask.classList.add('task');
-  // newTask.addEventListener('click', selectTask);
-  // newTask.addEventListener('dblclick', toggleCompleted);
-  newTask.innerText = text;
-  taskList.appendChild(newTask);
+  if ((input.value !== '')) {
+    const newTask = document.createElement('li');
+    newTask.classList.add('task');
+    // newTask.addEventListener('click', selectTask);
+    // newTask.addEventListener('dblclick', toggleCompleted);
+    newTask.innerText = text;
+    taskList.appendChild(newTask);
+  }
+  input.focus();
+  input.value = '';
 }
 
 btnAdd.addEventListener('click', () => {
   addTask(input.value);
-  input.focus();
-  input.value = '';
 });
 
 input.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     addTask(input.value);
-    input.focus();
-    input.value = '';
+  }
+});
+
+taskList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    const classSelected = document.querySelector('.selected');
+    if (classSelected != null) {
+      classSelected.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
   }
 });
