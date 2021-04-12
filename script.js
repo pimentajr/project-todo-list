1c8c81e84f80e3f8b896ba03675b01a2e2edae51
Marcelo Araujo <almeidadearaujomarcelo@gmail.com>
2021-04-12 15:08:14 -0300

    Requesitos 6 e 7

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const addBtn = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
 function addListItem() {
-  const task = document.getElementById('text-tarefa');
+  const task = document.getElementById('texto-tarefa');
   const listItem = document.createElement('li');
-  const taskList = document.getElementById('lista-tarefas');
   listItem.innerText = task.value;
   taskList.appendChild(listItem);
   task.value = '';
 }
 
+function changeListItemBackgroundColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
 addBtn.addEventListener('click', addListItem);
+
+taskList.addEventListener('click', changeListItemBackgroundColor)
