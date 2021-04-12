670a756444aa01d5278ff8260af131e2d2369edc
Arthur Hermann <arthurhermannc@gmail.com>
2021-04-12 17:06:30 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function addHeader() {
 }
 addHeader();
 
-// Cria uma section com um parágrafo e texto
+// Cria primeira section com um parágrafo e texto
 function addFirstSection() {
   const section = document.createElement('section');
   section.id = 'firstsection';
@@ -29,10 +29,40 @@ function addInput() {
 }
 addInput();
 
+// Cria o botão que irá adicionar um novo item a lista
+function addBtnNewItem() {
+  const button = document.createElement('button');
+  button.id = 'criar-tarefa'
+  button.innerText = 'Adicionar'
+  document.querySelector('#firstsection').appendChild(button);
+}
+addBtnNewItem();
+
+// Adiciona função ao botão Adicionar
+function addItem() {
+  const addButton = document.querySelector('#criar-tarefa');
+  addButton.addEventListener('click', () => {
+    const valueItem = document.querySelector('#texto-tarefa').value;
+    const itemList = document.createElement('li');
+    itemList.innerText = valueItem;
+    document.querySelector('#lista-tarefas').appendChild(itemList);
+    document.querySelector('#texto-tarefa').value = '';
+  })
+}
+addItem ();
+
+// Cria segunda section
+function addSecondSection() {
+  const section = document.createElement('section');
+  section.id = 'secondsection';
+  document.body.appendChild(section);
+}
+addSecondSection();
+
 // Cria uma lista ordenada
 function addOl() {
   const ol = document.createElement('ol');
   ol.id = 'lista-tarefas';
-  document.querySelector('#firstsection').appendChild(ol);
+  document.querySelector('#secondsection').appendChild(ol);
 }
 addOl();
