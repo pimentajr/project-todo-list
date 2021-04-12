05268368278b372a93848a59defdcb55dea5ac18
IsleneGomes <islenegomes@gmail.com>
2021-04-12 14:30:44 -0300

    Iniciando Projeto

---
+++ script.js
@@ -0,0 +1,14 @@
+function tags(elemento) {
+  return document.createElement(elemento);
+}
+
+
+
+
+
+
+
+window.onload = function carregar() {
+  const botao = document.querySelector('#criar-tarefa');
+
+};
