4b95e6f2d18edc1397cee50d9b25e308fbfceab9
Raphael <raphaellfpg@gmail.com>
2021-04-12 15:58:53 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -3,9 +3,19 @@ const input = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 
 function addList() {
-  buttom.addEventListener('click', function toDo() {
+  buttom.addEventListener('click', ()=> {
     lista.appendChild(document.createElement('li')).innerHTML = input.value;
-    input.value = ''
+    input.value = '';
   });
 }
+
+function backGroundGray() {
+lista.addEventListener('click', (event)=> {
+let click = event.target
+click.classList.add('gray')
+console.log(click)
+})
+}
+
+backGroundGray()
 addList();
