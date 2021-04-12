97f4e5ec62121aae8f3c289f78525206c8d07003
Gisele Costa  <jikinha@gmail.com>
2021-04-12 18:37:19 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const input = document.querySelector('#texto-tarefa');
 const btnAdd = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+
+// Adiciona nova tarefa
+function addTask(text) {
+  const newTask = document.createElement('li');
+  newTask.classList.add('task');
+  // newTask.addEventListener('click', selectTask);
+  // newTask.addEventListener('dblclick', toggleCompleted);
+  newTask.innerText = text;
+  taskList.appendChild(newTask);
+}
+
+btnAdd.addEventListener('click', () => {
+  addTask(input.value);
+  input.focus();
+});
