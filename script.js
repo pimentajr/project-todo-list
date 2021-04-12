397342ef63d3f7c7fee6caa66e22dce5c9765b5f
Daniel <danielfdoficial@gmail.com>
2021-04-12 15:56:37 -0300

    adiciona evento que muda backgroundColor da lista

--- script.js
+++ script.js
@@ -3,12 +3,19 @@ const texto = document.querySelector('#texto-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 
 function addList() {
-	button.addEventListener('click', function() {
-	  const item = document.createElement('li');
-	  lista.appendChild(item);
-	  const novoItem = texto.value;
-	  item.innerText = novoItem;
+  button.addEventListener('click', function() {
+    const item = document.createElement('li');
+    lista.appendChild(item);
+    const novoItem = texto.value;
+    item.innerText = novoItem;
 		texto.value = '';
   });
 }
 addList();
+
+function changeBackgroundColor() {
+  lista.addEventListener('click', function(event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
+}
+changeBackgroundColor();
