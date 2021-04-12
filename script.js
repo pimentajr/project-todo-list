6df201085588501d884f02972550a6c317ad9a49
rubia braga <rubiaemilly29@gmail.com>
2021-04-12 19:16:47 -0300

    requisito 6 - 7

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const task = document.querySelector('#criar-tarefa');
 
-  task.addEventListener('click', () => {
-		let captura = '';
-    captura = document.querySelector('#texto-tarefa');
-    const li = document.createElement('li');
-    document.querySelector('#lista-tarefas').appendChild(li);
-    li.innerHTML = captura.value;
-		captura.value = '';
-  });
+task.addEventListener('click', () => {
+  let captura = '';
+  captura = document.querySelector('#texto-tarefa');
+  const li = document.createElement('li');
+  document.querySelector('#lista-tarefas').appendChild(li);
+  li.innerHTML = captura.value;
+  captura.value = '';
+});
+
+const ol = document.querySelector('ol');
+ol.addEventListener('click', (evento) => {
+  const eve = evento.target;
+  const li = document.getElementsByTagName('li');
+  for (let index = 0; index < li.length; index += 1) {
+		li[index].classList.remove('select');
+  }
+	eve.classList.add('select');
+});
