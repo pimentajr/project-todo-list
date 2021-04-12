291cfa1ac458f8be65f79a631b6bf3194f416908
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-12 15:43:34 -0300

    requisito 7 completo

--- script.js
+++ script.js
@@ -2,11 +2,12 @@ const createTaskButton = document.querySelector('#criar-tarefa');
 
 function addTaskOnList() {
   const textFromInputArea = document.getElementsByTagName('input')[0].value;
-  let toDoList = document.createElement('li');
+  const toDoList = document.createElement('li');
   const toDoOrderedList = document.querySelector('#lista-tarefas');
+  toDoList.className = 'tasks';
   toDoList.innerHTML = textFromInputArea;
   toDoOrderedList.appendChild(toDoList);
-};
+}
 
 function clearInputField() {
   document.getElementsByTagName('input')[0].value = '';
@@ -14,3 +15,11 @@ function clearInputField() {
 
 createTaskButton.addEventListener('click', addTaskOnList);
 createTaskButton.addEventListener('click', clearInputField);
+
+const orderedList = document.querySelector('#lista-tarefas');
+
+function changeListItemBackgroundColor(event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+orderedList.addEventListener('click', changeListItemBackgroundColor);
