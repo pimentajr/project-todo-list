012c066ddbf9928d6a331e06eb62368a65c7e7df
Renan <renan.vamo@gmail.com>
2021-04-12 18:01:44 -0300

    Muda a criação do botão de JS para HTML e o element input:button para button

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
 // Cria as variáveis recorrentes
 const taskList = document.getElementById('lista-tarefas');
 const buttonArea = document.getElementById('btn-area');
-
-// Criar o botão de input
-function createTextInput() {
-  let inputText = document.createElement('input');
-  inputText.type = 'text';
-  inputText.placeHolder = 'Insira a nova tarefa:';
-  inputText.id = 'texto-tarefa';
-  buttonArea.appendChild(inputText);
-  let inputButton = document.createElement('input');
-  inputButton.type = 'button';
-  inputButton.id = 'criar-tarefa';
-  inputButton.value = 'Criar Tarefa'
-  buttonArea.appendChild(inputButton);
-}
-createTextInput();
-
 const createTask = document.getElementById('criar-tarefa');
 
 // Cria um novo item da lista OL e limpa o campo de input
