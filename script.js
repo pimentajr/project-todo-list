996642078e67e0b3c0e281b81e0250e8058a2557
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-12 18:24:04 -0300

    terceira funçao: selecionar itens completos

--- script.js
+++ script.js
@@ -2,22 +2,32 @@ const input = document.getElementById('texto-tarefa');
 const toDoList = document.querySelector('#lista-tarefas');
 const createToDo = document.getElementById('criar-tarefa');
 
-function addToDo () {
+function addToDo() {
   const toDo = document.createElement('li');
   toDo.innerText = input.value;
   toDoList.appendChild(toDo);
   input.value = '';
 }
 
-function toDoSelection (element) {
+function toDoSelection(element) {
   if (element.target.parentNode === toDoList) {
-    if (document.querySelector('.selected') === null) {
-      element.target.classList.add('selected')
-    }else{
-      element.target.classList.remove('selected')
-    };
-  };
+    if (element.target.classList.contains('selected')) {
+      element.target.classList.remove('selected');
+    } else if (document.querySelector('.selected') !== null) {
+      document.querySelector('.selected').classList.remove('selected');
+      element.target.classList.add('selected');
+    } else {
+      element.target.classList.add('selected');
+    }
+  }
 }
 
+function toDoCompleted(element) {
+  if (element.target.parentNode === toDoList) {
+    element.target.classList.toggle('completed')
+  }
+}
+
+addEventListener("dblclick", toDoCompleted);
 createToDo.addEventListener('click', addToDo);
 addEventListener('click', toDoSelection);
