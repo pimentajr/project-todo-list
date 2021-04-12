8197d2a7f27c7f165b8f539348de06ff8a97b437
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-12 20:42:42 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 const list = document.getElementById('lista-tarefas');
 const inptText = document.querySelector('#texto-tarefa');
 const btnAdd = document.getElementById('criar-tarefa');
+const clear = document.getElementById('apaga-tudo');
 
-function makingList () {
+function makingList() {
   const mkLi = document.createElement('li');
   mkLi.innerHTML = inptText.value;
   list.appendChild(mkLi);
   inptText.value = '';
 }
-btnAdd.addEventListener('click', makingList)
+
+btnAdd.addEventListener('click', makingList);
+
+function liBackColor(event) {
+  if (event.target.tagName === 'LI') {
+    const selected = document.querySelector('li.selected');
+    if(selected) selected.className= '';
+    event.target.className= 'selected';
+  }
+}
+
+list.addEventListener('click', liBackColor);
+
+function clearAll () {
+  list.innerHTML = '';
+}
+
+clear.addEventListener('click',clearAll);
