85facc4d5d9ad60431ec4073e456a86572afa226
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-12 19:12:09 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const button = document.getElementById('criar-tarefa');
 const orderList = document.getElementById('lista-tarefas');
 
 function addToList () {
 const list = document.getElementById('texto-tarefa');
 const listValue = list.value;
 const itemList = document.createElement('li');
-if(listValue === '') {
+if (listValue === '') {
 alert('Escreva uma Tarefa');
 } else {
 itemList.innerHTML = listValue;
@@ -13,5 +12,12 @@ orderList.appendChild(itemList);
 list.value = '';
 }
 }
+addToList();
 
+const changeListColor = document.getElementsByTagName('li');
+const colorUl = document.getElementById('lista-tarefas');
 
+function changeItemColor(evento) {
+   evento.target.style.backgroundColor = 'grey';
+}
+colorUl.addEventListener('click', changeItemColor);
