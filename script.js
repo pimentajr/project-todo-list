86a8392fd88ca7659500677bbbe9de1931b93807
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-12 16:51:00 -0300

    Correcoes Lint

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const listOfTasks = document.querySelector('#lista-tarefas');
 
-document.querySelector('#criar-tarefa'). addEventListener('click', () => {
+document.querySelector('#criar-tarefa').addEventListener('click', () => {
   const task = document.createElement('li');
-  const input = document.querySelector('#texto-tarefa')
+  const input = document.querySelector('#texto-tarefa');
 
   task.innerText = input.value;
   input.value = '';
 
   listOfTasks.appendChild(task);
-} );
+});
