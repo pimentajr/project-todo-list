5d9becff56716fffca8ed6a2f2f0cf4fc8f274d7
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-04-12 16:34:45 -0300

    Criada funçao inputTask que coloca a li com o conteudo do inout na lista.

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
 // ex 5
 // No campo de input será digitado o texto de uma tarefa qualquer e, em seguida, clicar-se-á no botão de criar tarefa. Será verificado que, após o clique, o texto digitado aparece na lista e desaparece do input.
-// pegar input no dom como metodo para adicionar o input a um objeto que eu possa manipular em formato de lista.
-let inputValue = document.getElementById('texto-tarefa').value;
+// Criando lo para ex 4
+let taskList = document.createElement('ol');
+taskList.id ='lista-tarefa'
+// Pegando o main do DOM
+const mainProject = document.getElementById('main');
+
 // pegar botão
 let button = document.getElementById('criar-tarefa');
 // Criar uma função que adiciona um elemento na tela
-// primeiro pego o  main
-const mainProject = document.getElementById('main');
-// feito isso inicio a construção da função. Primeiro pegar o elemento ol do dom
-const taskList = document.getElementById('lista-tarefas');
-// depois criamos o elemento que irá ser cada iten da lista, a tag li
-let itenList = document.createElement('li');
-// Agora precisamos adicionar o conteudo do input na tag li
-itenList.innerText = inputValue;
-// Agora é necessario vincular a li na tag ol
 
-// Feito o vinculo agora é necessario transformar tudo em uma função.
 
+
+// Função para colocar input na lista
+function inputTask() {
+  // pegar o valor do input no dom 
+  let inputValue = document.getElementById('texto-tarefa').value;
+  // depois criamos o elemento que irá ser cada iten da lista, a tag li
+  let itenList = document.createElement('li');
+  // Agora precisamos adicionar o conteudo do input na tag li
+  itenList.innerText = inputValue;
+  // Agora é necessario vincular a li na tag ol
+  taskList.appendChild(itenList);
+  // Adiciona a lista no main
+  mainProject.appendChild(taskList);
+}
 // Apos tudo ser transformado em uma função devemos adicionar essa função ao evento click no botão Adicionar
 
 // Adicionar eventLetener no botão para que ele reaja ao click acionando a função que pega o input e coloca em uma lista
