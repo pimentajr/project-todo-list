f7d2589691c1b6fcd05e5c861f79bcf390c817d1
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-04-12 17:06:26 -0300

    Requisito 7 e 8 - Concluido

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const list = document.querySelector('#lista-tarefas');
+
 function addNewTask() {
-  const list = document.querySelector('#lista-tarefas');
   const input = document.querySelector('#texto-tarefa');
   const itemList = document.createElement('li');
   itemList.innerText = input.value;
@@ -12,4 +13,21 @@ function newTask() {
   button.addEventListener('click', addNewTask);
 }
 
+function clearSelected() {
+  for (let index = 0; index < list.children.length; index += 1) {
+    list.children[index].className = '';
+  }
+}
+
+function addNewColor(event) {
+  const selected = event.target;
+  clearSelected();
+  selected.className = 'bg-list';
+}
+
+function selectedTask() {
+  list.addEventListener('click', addNewColor);
+}
+
 newTask();
+selectedTask();
