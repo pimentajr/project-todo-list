04d88613a02e4cea7d7063d2236b7df5ba7a62c5
Marcelo Leite <marsleite@gmail.com>
2021-04-12 20:44:07 -0300

    requisito 7

--- script.js
+++ script.js
@@ -6,11 +6,18 @@ const formList = document.getElementById('texto-tarefa');
 function addLi(textForm) {
   const item = document.createElement('li');
   item.innerText = `${textForm}`;
-  taskList.appendChild(item);
+  taskList.appendChild(item).classList.add('task-list');
   formList.value = '';
   formList.focus();
+}
+
+function printSelectedItem(event) {
+  const changeBg = event.target;
+  changeBg.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 btnAdd.addEventListener('click', () => {
   addLi(formList.value);
 });
+
+taskList.addEventListener('click', printSelectedItem);
