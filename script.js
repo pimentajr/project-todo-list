b51f326a47d6e3a517cb42d36593f52a8ff7b777
Felipe Buso <felipe_buso@hotmail.com>
2021-04-12 17:07:20 -0300

    itens 7 e 8

--- script.js
+++ script.js
@@ -6,11 +6,21 @@ const buttonNewJob = document.querySelector('#criar-tarefa');
 
 // botão para criar um novo item na lista
 buttonNewJob.addEventListener('click', function() {
-  let newItemList = document.createElement('li');
+  const newItemList = document.createElement('li');
   newItemList.className = 'Item-List';
-  let textItem = document.querySelector('#texto-tarefa');
+  const textItem = document.querySelector('#texto-tarefa');
   // console.log(textItem.value);
   newItemList.innerText = textItem.value;
   ordenedList.appendChild(newItemList);
-  textItem.value = "";
+  textItem.value = '';
+});
+
+// Colocar fundo cinza em um item clicado
+ordenedList.addEventListener('click', function(event) {
+  const itensList = document.querySelectorAll('.clicked');
+  for (let index = 0; index < itensList.length; index += 1){
+    itensList[index].classList.remove('clicked');
+  }
+  event.target.classList.add('clicked');
+  console.log(event.target);
 });
