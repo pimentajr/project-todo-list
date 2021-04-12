50f122cb71aa2f0c547c0b4000f9d52387706871
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-12 16:10:22 -0300

    Finaliza requisito 5

---
+++ script.js
@@ -0,0 +1,12 @@
+function addToList() {
+  let input = document.getElementById('texto-tarefa');
+  let addButton = document.getElementById('criar-tarefa');
+  let list = document.getElementById('lista-tarefas');
+
+  addButton.addEventListener('click', () => {
+    let li = document.createElement('li');
+    li.innerText = input.value;
+    list.appendChild(li);
+  });
+}
+addToList();
