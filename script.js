30a001a54b714bf78725ebc6fab9cb0d5a9b2df0
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-12 18:48:33 -0300

    5.1 add button and event click

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-// Start
+const addTask = document.getElementById('criar-tarefa');
+const inputTask = addTask.previousElementSibling;
+
+function addTaskEvent() {
+  const ol = document.getElementById('lista-tarefas');
+  const li = document.createElement('li');
+  li.innerText = inputTask.value;
+  inputTask.value = '';
+  ol.appendChild(li);
+}
+
+addTask.addEventListener('click', addTaskEvent);
