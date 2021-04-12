081509049e9c00ba3038a1aa3c0e49a0e00c17f9
Leonardo Pinto <leonardoan.pinto@gmail.com>
2021-04-12 20:11:13 -0300

    Requisito 14

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const clearButton = document.getElementById('apaga-tudo');
 const jobItems = document.getElementsByClassName('job');
 const finishedJobs = document.getElementById('remover-finalizados');
 const storageButton = document.getElementById('salvar-tarefas');
-const teste = document.getElementById('jobs-container');
+const upButton = document.getElementById('mover-cima');
+const downButton = document.getElementById('mover-baixo');
+const removeSelecItem = document.getElementById('remover-selecionado');
 
 function createJob() {
   jobButton.addEventListener('click', () => {
@@ -65,14 +67,34 @@ removeFinishedJobs();
 
 function storeJobs() {
   storageButton.addEventListener('click', () => {
-    localStorage['list'] = jobListContainer.innerHTML;
-  })
+    localStorage.list = jobListContainer.innerHTML;
+  });
 }
 storeJobs();
 
-function retrieveJobs () {
-  if (localStorage['list']) {
-    jobListContainer.innerHTML = localStorage['list'];
+function retrieveJobs() {
+  if (localStorage.list) {
+    jobListContainer.innerHTML = localStorage.list;
   }
 }
 retrieveJobs();
+
+/*function upJob() {
+  upButton.addEventListener('click', () => {
+    let selectedItem = document.querySelector('.selected');
+    jobListContainer.replaceChild(selectedItem, selectedItem.previousSibling);
+  })
+}
+upJob();
+*/
+
+function removeSelected () {
+  removeSelecItem.addEventListener('click', () => {
+    for (let index = 0; index < jobItems.length; index += 1) {
+      if (jobItems[index].classList.contains('selected')) {
+        jobListContainer.removeChild(jobItems[index]);
+      }
+    }
+  })
+}
+removeSelected();
