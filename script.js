da5e362df09465399a1bf5185b1204098dc1d5f0
Thiago Cavalcante do Espirito Santo <thiagofox00@gmail.com>
2021-04-12 19:36:37 -0300

    requisito 8 Finalizado

--- script.js
+++ script.js
@@ -1,23 +1,34 @@
 function addTask() {
-  const userInput = document.getElementById('texto-tarefa').value
-  let typed = userInput
+  const userInput = document.getElementById('texto-tarefa').value;
+  const typed = userInput;
   const listTask = document.getElementById('lista-tarefas');
-  let task = document.createElement('li');
+  const task = document.createElement('li');
   task.classList.add('tarefa');
+  task.addEventListener('click', selectTask);
   listTask.appendChild(task);
   task.innerHTML = typed;
   document.getElementById('texto-tarefa').value = '';
+  
 }
 
 function clickButtonTask() {
   const buttonTask = document.getElementById('criar-tarefa');
   buttonTask.addEventListener('click', addTask);
-} 
+}
 
-
-
+function selectTask(event) {
+  if (event.target.className !== 'selected') {
+    event.target.classList.add('selected');
+  }else {
+    event.target.classList.remove('selected');
+  }
+}
 
 window.onload = function load() {
 
-clickButtonTask();  
-}
+clickButtonTask(); 
+
+};
+
+
+
