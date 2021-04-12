948538095b35a0d7ce56c4b202bf2e07fedfd574
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-12 15:38:10 -0300

    ouvindo a palavra de linter

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 // referencia para a manipulação do input 'https://www.w3schools.com/jsref/dom_obj_text.asp'
-let buttonTaskCreate = document.querySelector('#criar-tarefa');
-buttonTaskCreate.addEventListener('click', function () {
-
-    // procura pelo que foi digitado no input
-    let inputCamp = document.getElementById('texto-tarefa').value;
-
-    // procura pela lista ordenada 
-    let locateLista = document.querySelector('#lista-tarefas');
-
-    // cria uma linha para a lista 
-    if (inputCamp !== ''){
-        let createLi = document.createElement('li');
-        createLi.innerText = inputCamp;
-        locateLista.appendChild(createLi);
-        clearTextInput();
-    }
-});
 
 // limpa a barra de inserir texto
 function clearTextInput() {
-    document.querySelector('#texto-tarefa').value = '';
+  document.querySelector('#texto-tarefa').value = '';
 }
+
+const buttonTaskCreate = document.querySelector('#criar-tarefa');
+buttonTaskCreate.addEventListener('click', function buttonAction() {
+  // procura pelo que foi digitado no input
+  const inputCamp = document.getElementById('texto-tarefa').value;
+  // procura pela lista ordenada
+  const locateLista = document.querySelector('#lista-tarefas');
+  // cria uma linha para a lista
+  if (inputCamp !== ''){
+    const createLi = document.createElement('li');
+    createLi.innerText = inputCamp;
+    locateLista.appendChild(createLi);
+    clearTextInput();
+  }
+});
