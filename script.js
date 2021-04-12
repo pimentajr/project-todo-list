cce1bb4ff1412f58d64eeda37a77742bf637d6a6
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-12 18:01:30 -0300

    implementa requisito 11

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const taskList = document.getElementById('lista-tarefas');
+
 function SelectTask(event) {
   const selected = document.querySelector('.selected');
   if (selected !== null) {
@@ -15,22 +17,28 @@ function CompleteTask(event) {
 }
 
 function clearAll() {
-  const list = document.getElementById('lista-tarefas');
-  const size = list.childElementCount;
+  const size = taskList.childElementCount;
   for (let index = 0; index < size; index += 1) {
-    list.removeChild(list.lastElementChild);
+    taskList.removeChild(taskList.lastElementChild);
+  }
+}
+
+function clearCompleted() {
+  const completedList = document.getElementsByClassName('completed');
+  const size = completedList.length;
+  for (let index = size - 1; index >= 0; index -= 1) {
+    taskList.removeChild(completedList[index]);
   }
 }
 
 function CreateTask() {
   const element = document.createElement('li');
   const text = document.getElementById('texto-tarefa').value;
-  const parent = document.getElementById('lista-tarefas');
   element.innerText = text;
   element.classList.add('tarefa');
   element.addEventListener('click', SelectTask);
   element.addEventListener('dblclick', CompleteTask);
-  parent.appendChild(element);
+  taskList.appendChild(element);
   document.getElementById('texto-tarefa').value = '';
   // let criado = document.querySelector('tarefa');
   // criado.addEventListener('click',SelectTask);
@@ -46,7 +54,13 @@ function addEventClearAll() {
   button.addEventListener('click', clearAll);
 }
 
+function addEventClearCompleted() {
+  const button = document.getElementById('remover-finalizados');
+  button.addEventListener('click', clearCompleted);
+}
+
 window.onload = function load() {
   addEventCreateTask();
   addEventClearAll();
+  addEventClearCompleted();
 };
