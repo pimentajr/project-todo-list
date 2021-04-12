03d65b949eaec6e31c92d92fee242e1476f1f4b1
Daniel <danielfdoficial@gmail.com>
2021-04-12 17:43:51 -0300

    seleciona so um elemento da lista

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ addList();
 
 function changeBackgroundColor() {
   lista.addEventListener('click', function (event) {
+    let coloridos = document.getElementsByTagName('li');
+    for (let index = 0; index < coloridos.length; index += 1) {
+      coloridos[index].style.backgroundColor = 'white';
+    }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   });
 }
