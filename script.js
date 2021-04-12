bb1cb472a5cfacaa8f7448ad911b6c7015dac521
marcoswarmling <marcoswarmling15@gmail.com>
2021-04-12 18:29:45 -0300

    pintando

--- script.js
+++ script.js
@@ -8,3 +8,11 @@ let texto = document.querySelector('#texto-tarefa');
 novo.insertAdjacentHTML('afterbegin',texto.value);
 texto.value = '';
 })
+
+listatarefa.addEventListener('click',function(event) {
+    let coloridos = document.getElementsByTagName('li');
+    for (let index = 0; index < coloridos.length; index += 1) {
+      coloridos[index].style.backgroundColor = 'white';
+    }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
