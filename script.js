dc9aeac444dc7c3b58af3a1fea75b26eb59108ad
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-12 19:07:13 -0300

    Tentando corrigir passo 7

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
+const list = document.getElementById('lista-tarefas');
+
 function addListItem() {
   const button = document.getElementById('criar-tarefa');
   const input = document.getElementById('texto-tarefa');
-  const list = document.getElementById('lista-tarefas');
 
   button.addEventListener('click', () => {
     const listItem = document.createElement('li');
@@ -13,33 +14,21 @@ function addListItem() {
 }
 
 function removeSelect() {
-  const list = document.getElementById('lista-tarefas');
-
   for (let i = 0; i < list.children.length; i += 1) {
     list.children[i].className = 'tarefa';
+    list.children[i].style.backgroundColor = document.body.style.backgroundColor;
   }
 }
 
 function selectItem() {
-  const list = document.getElementById('lista-tarefas');
-  const body = document.body;
 
-  list.addEventListener('click', () => {
-    for (let i = 0; i < list.children.length; i += 1) {
-      list.children[i].addEventListener('click', () => {
-        removeSelect();
-        if (list.children[i].className !== 'tarefa selected') {
-          list.children[i].classList.add('selected');
-        }
-      });
-
-      if (list.children[i].className === 'tarefa') {
-        list.children[i].style.backgroundColor = body.style.backgroundColor;
-      } else if (list.children[i].className === 'tarefa selected'){
-        list.children[i].style.backgroundColor = 'rgb(128, 128, 128)'
+    list.addEventListener('click', (e) => {
+      removeSelect();
+      if (e.target.className !== 'tarefa selected') {
+        e.target.classList.add('selected');
+        e.target.style.backgroundColor = 'rgb(128, 128, 128)';
       }
-    }
-  });
+    });
 }
 
 addListItem();
