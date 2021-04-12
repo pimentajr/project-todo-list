17a578f6cbd5a5793bd0660805c745b0adbb0143
Danilo Pereira <danilodps22@hotmail.com>
2021-04-12 16:37:12 -0300

    Req.7 - Cor de fundo da tarefa

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
 const createTaskButton = document.querySelector('#criar-tarefa');
 const taskInput = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
 
 function createTask() {
   const textTask = taskInput.value;
   const newTask = document.createElement('li');
-  const taskList = document.querySelector('#lista-tarefas');
 
   newTask.innerText = textTask;
   taskList.appendChild(newTask);
   taskInput.value = '';
+
+  // console.log(taskList);
+}
+
+function taskSelection(task) {
+  const selectedTask = task.target;
+  selectedTask.className = 'selected';
+  console.log(selectedTask);
 }
 
 createTaskButton.addEventListener('click', createTask);
+taskList.addEventListener('click', taskSelection);
