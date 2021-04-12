739b1b41899c3e4fa32e5b464b29cf2397fa23d1
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-04-12 17:49:35 -0300

    ajusta o lint

--- script.js
+++ script.js
@@ -43,11 +43,19 @@ orderedList.addEventListener('dblclick', crossOutCompletedTask);
 
 const eraseButton = document.querySelector('#apaga-tudo');
 
-function eraseEverything() {
-  let orderedList = document.querySelector('#lista-tarefas');
-    while (orderedList.firstChild) {
-      orderedList.removeChild(orderedList.firstChild);
-    }
+function removeEverything() {
+  const orderedList = document.querySelector('#lista-tarefas');
+  while (orderedList.firstChild) {
+    orderedList.removeChild(orderedList.firstChild);
+  }
 }
 
-eraseButton.addEventListener('click', eraseEverything);
+eraseButton.addEventListener('click', removeEverything);
+
+const removeCompletedButton = document.querySelector('#remover-finalizados');
+
+function removeCompleted() {
+
+}
+
+removeCompletedButton.addEventListener('click', removeCompleted);
