8ee39cfffac3a09e73edf14e977d5c2e3df7ec65
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-12 18:04:43 -0300

    Passo 7

--- script.js
+++ script.js
@@ -1,14 +1,46 @@
 function addListItem() {
-  let button = document.getElementById('criar-tarefa');
-  let input = document.getElementById('texto-tarefa');
-  let list = document.getElementById('lista-tarefas');
+  const button = document.getElementById('criar-tarefa');
+  const input = document.getElementById('texto-tarefa');
+  const list = document.getElementById('lista-tarefas');
 
   button.addEventListener('click', () => {
-    let listItem = document.createElement('li');
+    const listItem = document.createElement('li');
+    listItem.className = 'tarefa';
     listItem.innerText = input.value;
     list.appendChild(listItem);
     input.value = '';
   });
 }
 
+function removeSelect() {
+  const list = document.getElementById('lista-tarefas');
+
+  for (let i = 0; i < list.children.length; i += 1) {
+    list.children[i].className = 'tarefa';
+  }
+}
+
+function selectItem() {
+  const body = document.body;
+  const list = document.getElementById('lista-tarefas');
+
+  list.addEventListener('click', () => {
+    for (let i = 0; i < list.children.length; i += 1) {
+      list.children[i].addEventListener('click', () => {
+        removerSelecao();
+        if (list.children[i].className !== 'tarefa selected') {
+          list.children[i].classList.add('selected');
+        }
+      });
+
+      if (list.children[i].className === 'tarefa') {
+        list.children[i].style.backgroundColor = body.style.backgroundColor;
+      } else if (list.children[i].className === 'tarefa selected'){
+        list.children[i].style.backgroundColor = 'rgb(128, 128, 128)'
+      }
+    }
+  });
+}
+
 addListItem();
+listItemClick();
