9671af16b54ea16013fe04af9408525c8d77ce0e
IsaacMagno <isaacmagno98@gmail.com>
2021-04-12 15:30:56 -0300

    desafio 4

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-var slctDiv = document.querySelector('div')
-var crtElement = document.createElement('p')
-var newCnt = 'Clique duas vezes em um item para marcá-lo como completo'
-crtElement.setAttribute('id', 'funcionamento')
-crtElement.innerText = newCnt
-slctDiv.appendChild(crtElement)
+function createP(){
+  var slctDiv = document.querySelector('div')
+  var crtElement = document.createElement('p')
+  var newCnt = 'Clique duas vezes em um item para marcá-lo como completo'
+  crtElement.setAttribute('id', 'funcionamento')
+  crtElement.innerText = newCnt
+  slctDiv.appendChild(crtElement)
+}
+
+createP()
+
+function createLi(){
+  var slctDiv = document.getElementById('list')
+  var crtElement = document.createElement('ol')
+  crtElement.setAttribute('id', 'lista-tarefas')
+  slctDiv.appendChild(crtElement)
+}
+
+createLi()
