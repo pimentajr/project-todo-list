dc346ba473f5bbe2cc07d9f49918cdeded857ddc
Renan <renan.vamo@gmail.com>
2021-04-12 15:10:38 -0300

    limpa o campo de input após adiciona a lista de tarefas

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// Cria as variáveis recorrentes 
-let createTask = document.getElementById('criar-tarefa');
-let taskList = document.getElementById('lista-tarefas');
+// Cria as variáveis recorrentes
+const createTask = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 console.log(taskList);
 
+// Cria um novo item da lista OL e limpa o campo de input
 function createNewTask() {
   createTask.addEventListener('click', () => {
-    let newTask = document.getElementById('texto-tarefa').value;
-    let newLi = document.createElement('li');
+    let newTask = document.getElementById('texto-tarefa');
+    const newLi = document.createElement('li');
     taskList.appendChild(newLi);
-    newLi.innerText = newTask;
-  })
+    newLi.innerText = newTask.value;
+    newTask.value = '';
+  });
 }
 createNewTask();
