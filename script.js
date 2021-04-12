a0e5931348879ba46b78931ddb595d91c0595216
Diego Ribeiro <dihrm2001@gmail.com>
2021-04-12 18:14:14 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const toDoList = document.querySelector('#lista-tarefas');
 const input = document.querySelector('#texto-tarefa');
 const createTaskBtn = document.querySelector('#criar-tarefa');
-
 function addTask() {
   const newTask = document.createElement('li');
   newTask.innerText = input.value;
@@ -36,9 +35,9 @@ toDoList.addEventListener('dblclick', completeTask);
 const clearBtn = document.querySelector('#apaga-tudo');
 
 function clearList() {
-  const listItems = document.querySelector('#lista-tarefas');
-  while (listItems.firstChild) {
-    listItems.firstChild.remove();
+  const itemsList = document.querySelector('#lista-tarefas');
+  while (itemsList.firstChild) {
+    itemsList.firstChild.remove();
   }
 }
 
@@ -54,3 +53,42 @@ function removeCompleted() {
 }
 
 clearCompletedBtn.addEventListener('click', removeCompleted);
+
+const saveListBtn = document.querySelector('#salvar-tarefas');
+
+function saveClasses(lista) {
+  const classes = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    classes.push(lista[index].className);
+  }
+  return classes;
+}
+
+function saveTask(lista) {
+  const task = [];
+  for (let index = 0; index < lista.length; index += 1) {
+    task.push(lista[index].innerText);
+  }
+  return task;
+}
+
+function saveList() {
+  const lista = document.querySelectorAll('#lista-tarefas li');
+  const listClasses = saveClasses(lista);
+  const listTasks = saveTask(lista);
+  localStorage.setItem('classes', listClasses);
+  localStorage.setItem('tasks', listTasks);
+}
+
+saveListBtn.addEventListener('click', saveList);
+
+if (localStorage.tasks) {
+  const tasksArray = localStorage.tasks.split(',');
+  const classesArray = localStorage.classes.split(',');
+  for (let index = 0; index < tasksArray.length; index += 1) {
+    const newTask = document.createElement('li');
+    newTask.innerText = tasksArray[index];
+    newTask.className = classesArray[index];
+    toDoList.appendChild(newTask);
+  }
+}
