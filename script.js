7cf136a36056ca51765731270b2859a3b08cdcad
Víctor Faria <vitilevi@hotmail.com>
2021-04-12 16:24:03 -0300

    Feito requisito 9

--- script.js
+++ script.js
@@ -20,9 +20,18 @@ function selectedItem(event) {
   event.target.classList.add('selected');
 }
 
+function crossedItem(event) {
+  if(event.target.className != '' && event.target.className != 'selected') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }  
+}
+
 function createLiEventListener() {
   for (let index = 0; index < liItens.length; index += 1) {
     liItens[index].addEventListener('click', selectedItem);
+    liItens[index].addEventListener('dblclick', crossedItem);
   }
 }
 
