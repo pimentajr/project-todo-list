775c252da973e89c83611d890ad62a2468ade643
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-04-12 19:02:31 -0300

    Mudando cor de fundo da task selecionada

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+function changeBgColor(event) {
+  const task = event.target;
+  task.style.backgroundColor = 'rgb(128,128,128)';
+}
+
+function selectTodo() {
+  const toDoSelected = document.querySelectorAll('#todo');
+
+  for (let index = 0; index < toDoSelected.length; index += 1) {
+    toDoSelected[index].addEventListener('click', changeBgColor);
+  }
+}
+
 function addListItem() {
   const input = document.getElementById('texto-tarefa').value;
-  console.log(input);
   document.getElementById('texto-tarefa').value = '';
   const ol = document.getElementById('lista-tarefas');
   const li = document.createElement('li');
+  li.id = 'todo';
   li.innerText = input;
-  return ol.appendChild(li);
+  ol.appendChild(li);
+  selectTodo();
 }
 
 function addToDo() {
