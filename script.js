c7edce5ec31f73127febc6943e127c2875606948
Jean Esteves <jean.esteves2001@gmail.com>
2021-04-12 14:47:10 -0300

    requisito 9 e 10 feitos

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const getInput = document.querySelector('#texto-tarefa');
 const getButton = document.querySelector('#criar-tarefa');
 const getOl = document.querySelector('#lista-tarefas');
 const getLi = document.getElementsByClassName('list');
+const getClearButton = document.querySelector('#apaga-tudo');
 
 function createList(value) {
   const createLi = document.createElement('li');
@@ -30,3 +31,27 @@ function clickListModifiesColor(color) {
   });
 }
 clickListModifiesColor('rgb(128, 128, 128)');
+
+function doubleClickRiskText() {
+  getOl.addEventListener('dblclick', (event) => {
+    const myEvent = event.target;
+    if (myEvent.className === 'list completed') {
+      myEvent.classList.remove('completed');
+    } else {
+      myEvent.classList.add('completed');
+    }
+  });
+  getOl.addEventListener('mousedown', (event) => {
+    event.preventDefault();
+  }, false);
+}
+doubleClickRiskText();
+/* preventDefault source = https://stackoverflow.com/questions/880512/prevent-text-selection-after-double-click */
+
+function clearButton() {
+  getClearButton.addEventListener('click', () => {
+    getOl.innerHTML = '';
+  });
+}
+clearButton();
+/* remove all child elements = https://stackoverflow.com/questions/3955229/remove-all-child-elements-of-a-dom-node-in-javascript */
