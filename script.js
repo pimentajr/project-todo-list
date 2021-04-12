156291098b1e29ae84937dc35b549801efadbb7e
LeoFuna <leo.funa@gmail.com>
2021-04-12 14:06:21 -0300

    [ADD]BGC on task list on click

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+function changeBgcOfTask(element) {
+  const taskSelected = element.target;
+  taskSelected.style.backgroundColor = 'rgb(128,128,128)';
+}
+
 function addTask() {
-  let element = document.createElement('li');
+  const element = document.createElement('li');
   element.innerText = document.querySelector('#texto-tarefa').value;
   document.querySelector('#lista-tarefas').appendChild(element);
   document.querySelector('#texto-tarefa').value = '';
+  for (let index = 0; index < document.querySelectorAll('li').length; index += 1) {
+    document.querySelectorAll('li')[index].addEventListener('click', changeBgcOfTask);
+  }
 }
-
 document.querySelector('#criar-tarefa').addEventListener('click', addTask);
-
