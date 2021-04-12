d1d7adae651271c4b1c866751486b2028727ec07
Gustavo Lemes <gustavolemes997@gmail.com>
2021-04-12 15:32:23 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -2,9 +2,16 @@ const btnCreateTask = document.querySelector('#criar-tarefa');
 const olList = document.querySelector('#lista-tarefas');
 
 btnCreateTask.addEventListener('click', () => {
-  let inputData = document.getElementById('texto-tarefa').value;
+  const inputData = document.getElementById('texto-tarefa').value;
   const createTask = document.createElement('li');
   createTask.innerText = inputData;
   olList.appendChild(createTask);
   document.getElementById('texto-tarefa').value = '';
 });
+
+function paintSelectedELement(element) {
+  element = element.target;
+  element.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+olList.addEventListener('click', paintSelectedELement);
