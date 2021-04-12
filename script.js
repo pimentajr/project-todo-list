885f86d08ea18f4e5da835c50bd86ad9fb6f7a93
igordmf <igordanielmf@gmail.com>
2021-04-12 15:25:09 -0300

    Adiciona requisito 3 4 e 5

---
+++ script.js
@@ -0,0 +1,11 @@
+// 5.
+function criarTarefa() {
+  const oList = document.getElementById('lista-tarefas');
+  const inputList = document.getElementById('texto-tarefa');
+  const lItem = document.createElement('li');
+  lItem.innerText = inputList.value;
+  oList.appendChild(lItem);
+  inputList.value = '';
+}
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+buttonCriarTarefa.addEventListener('click',  criarTarefa);
