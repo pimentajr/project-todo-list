bfbc92eb55c34495819a9db70b5024c2b30b27c1
Juliana Happatsch <jhappatsch@gmail.com>
2021-04-12 16:58:08 -0300

    Passo 5

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
- 
+function addListItem() {
+  let button = document.getElementById('criar-tarefa');
+  let input = document.getElementById('texto-tarefa');
+  let list = document.getElementById('lista-tarefas');
+
+  button.addEventListener('click', () => {
+    let listItem = document.createElement('li');
+    listItem.innerText = input.value;
+    list.appendChild(listItem);
+    input.value = '';
+  });
+}
+
+addListItem();
