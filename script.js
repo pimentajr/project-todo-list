4298293886aecd557ff01a458d237c0111f74504
KaioHPs <kaiohpsalternativo@gmail.com>
2021-04-12 16:04:00 -0300

    Requisito 8

--- script.js
+++ script.js
@@ -3,7 +3,13 @@ function listItemBackgroundOnClick() {
   for (let index = 0; index < listItems.length; index += 1) {
     const listItem = listItems[index];
     listItem.addEventListener('click', () => {
+      const currentSelected = document.querySelector('.selected');
+      if (currentSelected !== null) {
+        currentSelected.classList.remove('selected');
+        currentSelected.style.backgroundColor = 'transparent';
+      }
       listItem.style.backgroundColor = 'rgb(128, 128, 128)';
+      listItem.classList.add('selected');
     });
   }
 }
