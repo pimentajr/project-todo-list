e282ec501ba5ecf50018738422581f0b6212b6e4
Gustavo Zanette <gmzo.gustavo@gmail.com>
2021-04-12 16:17:28 -0300

    requisito 5

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('First commit');
+// Adiciona tarefas
+const button = document.querySelector('#criar-tarefa');
+button.addEventListener('click', () => {
+  const ol = document.querySelector('#lista-tarefas');
+  const tarefa = document.createTextNode(
+    document.querySelector('#texto-tarefa').value
+  );
+  const li = document.createElement('li');
+  li.appendChild(tarefa);
+  ol.appendChild(li);
+  document.querySelector('#texto-tarefa').value = '';
+});
