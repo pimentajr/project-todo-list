ebf33cf6be4d484091b11f261506b537ae0d248f
Renan <renan.vamo@gmail.com>
2021-04-12 17:16:43 -0300

    Seleciona apenas um elemento por vez na lista de tarefas

--- script.js
+++ script.js
@@ -2,26 +2,29 @@
 const createTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
+
 // Cria um novo item da lista OL e limpa o campo de input
 function createNewTask() {
   createTask.addEventListener('click', () => {
     let newTask = document.getElementById('texto-tarefa');
     const newLi = document.createElement('li');
-    newLi.className = 'item'
+    newLi.className = 'item';
     taskList.appendChild(newLi);
     newLi.innerText = newTask.value;
     newTask.value = '';
-    newTask.focus()
+    newTask.focus();
   });
 }
 createNewTask();
 
-// Ao clicar no item da lista, ele finca com fundo rgb(128,128,128)
+// Ao clicar no item da lista, ele passa a ter a class selected, e o último item selecionado perde essa classe
 function taskSelector() {
-   taskList.addEventListener('click', (event) => {
-    let items = document.querySelector('item');
+  taskList.addEventListener('click', (event) => {
     if (event.target.className === 'item') {
-      event.target.style.backgroundColor = 'rgb(128,128,128)';
+      let previousSelectedItem = document.querySelector('.selected');
+      if (previousSelectedItem) previousSelectedItem.classList.remove('selected');
+      let selectedItem = event.target;
+      selectedItem.classList.add('selected');
    }
   });
 }
