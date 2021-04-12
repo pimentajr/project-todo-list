73e060953ecf1b698006dda4c0d913fd898f849a
Leonardo Lima <lima.leonardo.r@gmail.com>
2021-04-12 17:50:22 -0300

    cumpre requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonForTaskCreation = document.getElementById('criar-tarefa');
 const inputTextElement = document.getElementById('texto-tarefa');
 const taskList = document.getElementById('lista-tarefas');
+const tasks = document.getElementsByClassName('tasks');
 
 function addTask() {
   const task = inputTextElement.value;
@@ -8,10 +9,25 @@ function addTask() {
   const TaskElement = document.createElement('li');
   taskList.appendChild(TaskElement).className = 'tasks';
   taskList.lastChild.innerText = task;
+  console.log(taskList.lastChild);
+}
+
+function taskSelector(event) {
+  for (let index = 0; index < tasks.length; index += 1) {
+    const classes = tasks[index].className;
+    if (classes === 'tasks selected') {
+      tasks[index].classList.remove('selected');
+    }
+  }
+
+  const targetTask = event.target;
+  targetTask.classList.add('selected');
+  console.log(event.target);
 }
 
 function addListeners() {
   buttonForTaskCreation.addEventListener('click', addTask);
+  taskList.addEventListener('click', taskSelector);
 }
 
 addListeners();
