c64f059879889f1b6209f947b65e48124d2349ca
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-04-12 15:38:32 -0300

    implementa requisito 5 e 6

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-window.onload = function load() {
+function CreateTask() {
+  let element = document.createElement('li');
+  let text = document.getElementById('texto-tarefa').value;
+  let parent = document.getElementById('lista-tarefas');
+  element.innerText = text;
+  parent.appendChild(element);
+  document.getElementById('texto-tarefa').value = '';
+}
 
+function addEventCreateTask() {
+  let button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', CreateTask);
+}
+
+window.onload = function load() {
+  addEventCreateTask();
 };
