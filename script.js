28a10fe0c822d73a723f9cd1915c2bf1abc6cc20
Ana Carolina Cavalheiro Giorgiani <cavalheiro.anacarolina@gmail.com>
2021-04-12 18:18:34 -0300

    Desafio 2 project to do list

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ const header = document.createElement('header');
 header.innerText = 'Minha Lista de Tarefas';
 header.id = 'header';
 body.appendChild(header);
+
+const paragraph = document.createElement('p');
+paragraph.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+paragraph.id = 'funcionamento';
+body.appendChild(paragraph);
