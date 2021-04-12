8f1f3277d7f5cb4503d9b2eb9750811b184be38a
KaioHPs <kaiohpsalternativo@gmail.com>
2021-04-12 15:51:10 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
+function listItemBackgroundOnClick() {
+  const listItems = document.querySelectorAll('#lista-tarefas li');
+  for (let index = 0; index < listItems.length; index += 1) {
+    const listItem = listItems[index];
+    listItem.addEventListener('click', () => {
+      listItem.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+  }
+}
+
 function newTaskButton() {
   const btnNewTask = document.querySelector('#criar-tarefa');
   const inputNewTask = document.querySelector('#texto-tarefa');
   const tasksList = document.querySelector('#lista-tarefas');
   btnNewTask.addEventListener('click', () => {
-    console.log(inputNewTask);
     const newTask = document.createElement('li');
     newTask.innerText = inputNewTask.value;
     tasksList.appendChild(newTask);
     inputNewTask.value = '';
+    listItemBackgroundOnClick();
   });
 }
 
