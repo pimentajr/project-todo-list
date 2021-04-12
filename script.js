67cdbe803f5f770215ffa698dde41bb8af9a0b80
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-04-12 18:19:04 -0300

    add exercise 10 - task deleted

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 const btnCreateTask = document.querySelector('#criar-tarefa');
 const listTask = document.querySelector('#lista-tarefas');
 const textTask = document.querySelector('#texto-tarefa');
+const selectedTask = document.getElementsByClassName('selected');
+const btnDelTasks = document.querySelector('#apaga-tudo');
 
 function createTask() {
   btnCreateTask.addEventListener('click', () => {
     if (textTask.value !== '') {
-    const createLi = document.createElement('li');
-    createLi.innerHTML = textTask.value;
-    createLi.className = 'li-assignment';
-    listTask.appendChild(createLi);
-    textTask.value = '';
+      const createLi = document.createElement('li');
+      createLi.innerHTML = textTask.value;
+      createLi.className = 'li-assignment';
+      listTask.appendChild(createLi);
+      textTask.value = '';
     }
   });
 }
 
 function alterBackColorTask() {
   listTask.addEventListener('click', function(event) {
-    const selectedTask = document.getElementsByClassName('selected');
     if (selectedTask[0]) {
       selectedTask[0].classList.remove('selected');
     }
@@ -30,8 +31,18 @@ function checkTask() {
   });
 }
 
+function delTasks() {
+  btnDelTasks.addEventListener('click', () => {
+    let liAssignment = document.querySelectorAll('.li-assignment');
+    for (let index = 0; index < liAssignment.length; index += 1) {
+      liAssignment[index].remove();
+    }
+  });
+}
+
 window.onload = () => {
   createTask();
   alterBackColorTask();
- checkTask();
+  checkTask();
+  delTasks();
 };
