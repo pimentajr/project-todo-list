66c8192d18c8b4c83cc1dad9ab5ac74a4cba0c72
Edson <edson.exe@outlook.com>
2021-04-12 18:21:20 -0300

    Refatorando as funções

--- script.js
+++ script.js
@@ -1,40 +1,41 @@
+const taskList = document.getElementById('lista-tarefas');
+const insertButtom = document.getElementById('criar-tarefa');
+const tasks = document.querySelector('#lista-tarefas');
+
+function insertTask () {
+  const taskContent = document.getElementById('texto-tarefa').value;
+  const newTask = taskContent;
+  document.getElementById('texto-tarefa').value = '';
+
+  const newTaskToInsert = document.createElement('li');
+  newTaskToInsert.innerHTML = newTask;
+  newTaskToInsert.className = 'task';
+  taskList.appendChild(newTaskToInsert);
+}
+
+function selectTask(e) {
+  for (let index = 0; index < tasks.children.length; index += 1) {
+    tasks.children[index].classList.remove('selectedTask');
+  }
+  e.target.classList.add('selectedTask');
+}
+
+function lineThrough (f) {
+  if (f.target.classList.contains('completed')) {
+    f.target.classList.remove('completed');
+  } else {
+    f.target.className = 'completed';
+  }
+}
+
+
 window.onload = function () {
-  const insertButtom = document.getElementById('criar-tarefa');
-  // const tasks = document.getElementsByClassName('task');
-  const tasks = document.querySelector('#lista-tarefas');
-  let taskList = document.getElementById('lista-tarefas');  
-
   // Desafio 5 e 6
-  insertButtom.addEventListener('click', function(){
-    let taskContent = document.getElementById('texto-tarefa').value;
-    let newTask = taskContent;
-    document.getElementById('texto-tarefa').value = '';
-    
-    let newTaskToInsert = document.createElement('li');
-    newTaskToInsert.innerHTML = newTask;
-    newTaskToInsert.className = 'task';
-    taskList.appendChild(newTaskToInsert);
-
-    console.log(tasks); // Depois apagar!
-  })
+  insertButtom.addEventListener('click', insertTask);
 
   // Desafio 7
-
-  tasks.addEventListener('click', function(e) {
-    for (let index = 0; index < tasks.children.length; index += 1){
-      tasks.children[index].classList.remove('selectedTask');
-    }
-    e.target.classList.add('selectedTask');
-  })
+  tasks.addEventListener('click', selectTask);
 
   // Desafio 9
-  tasks.addEventListener('dblclick', function(f) {
-    if (f.target.classList.contains('completed')) {
-      f.target.classList.remove('completed')
-    } else {
-      f.target.className = 'completed';
-    }
-  })
-
-
+  tasks.addEventListener('dblclick', lineThrough);
 }
