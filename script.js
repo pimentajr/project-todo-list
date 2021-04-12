a3cfa10317fe807123b717b6e3ed08a8be6a7a80
VictorS <czpask@gmail.com>
2021-04-12 17:01:21 -0300

    Exercicio 8 e 9

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 const getInput = document.querySelector('#texto-tarefa');
 const getButton = document.querySelector('#criar-tarefa');
 const getLista = document.querySelector('#lista-tarefas')
+const getLiItems = getLista.childNodes;
 
-getButton.addEventListener('click', function() {
+//funcao botao criar tarefa
+getButton.addEventListener('click', function () {
   const createLi = document.createElement('li');
-  console.log(createLi);
   createLi.innerText = getInput.value;
   getLista.appendChild(createLi);
   getInput.value = '';
 });
-
-getLista.addEventListener('click', function(event) {
-  if (getLista.style.backgroundColor !== 'rgb(128, 128, 128)') {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+//funcao selecionar tarefa e mudar a cor dela
+getLista.addEventListener('click', function (event) {
+  const getLiItems = getLista.childNodes;
+  for (let i = 0; i < getLiItems.length; i += 1) {
+    getLiItems[i].style.backgroundColor = 'white';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+//funcao doubleclick para riscar item
+getLista.addEventListener('dblclick', function (event) {
+  if (event.target.className === 'completed') {
+    event.target.className = '';
+  } else {
+    event.target.className = 'completed';
   }
 })
