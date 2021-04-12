6d968f53ff0ba565f965e55b0426c4b723d15820
Hugo Mouto <hmoutol@gmail.com>
2021-04-12 15:49:01 -0300

    Critério 7, 8 e 9

--- script.js
+++ script.js
@@ -15,3 +15,20 @@ function createNewTask(taskInputValue) {
   taskList.appendChild(newTask);
   taskInput.value = '';
 }
+
+// Select item
+taskList.addEventListener('click',function(event) {
+  let listItem = document.getElementsByTagName('li'); //COMO POSSO NÃO REPETIR?
+  for (key of listItem){
+    key.style.backgroundColor= '';
+  }
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)'})
+
+  // Strike-Unstrike Item
+  taskList.addEventListener('dblclick',function(event){
+    if (!event.target.style.textDecoration) {
+    event.target.style.textDecoration = 'line-through';
+    } else {
+      event.target.style.textDecoration = '';
+    }
+  })
