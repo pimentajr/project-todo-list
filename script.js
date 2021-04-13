ce020e354d5bcdd322439173c310476239ab80a1
Bruno Davi <brunodaviandrade2000@gmail.com>
2021-04-12 22:15:05 -0300

    12.1 add salve button

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 const addTask = document.getElementById('criar-tarefa');
 const clearTasks = document.getElementById('apaga-tudo');
 const endedTasks = document.getElementById('remover-finalizados');
+const salveTasks = document.getElementById('salvar-tarefas');
 const inputTask = document.getElementById('texto-tarefa');
 const listTask = document.getElementById('lista-tarefas');
+
+function loadTasks() {
+  for (let i = 0; i < localStorage.length / 2; i += 1) {
+    const li = document.createElement('li');
+    const val = localStorage[`val_${i}`];
+    const cls = localStorage[`cls_${i}`];
+    li.innerText = val;
+    if (cls === 'completed') {
+      li.className = cls;
+    }
+    listTask.appendChild(li);
+  }
+}
 
 function addTaskEvent() {
   const li = document.createElement('li');
@@ -21,6 +35,17 @@ function endedTasksEvent() {
   const enders = document.getElementsByClassName('completed');
   while (enders.length) {
     enders[0].remove();
+  }
+}
+
+function salveTasksEvent() {
+  localStorage.clear();
+  for (let i = 0; i < listTask.children.length; i += 1) {
+    const task = listTask.children[i];
+    const val = `val_${i}`;
+    const cls = `cls_${i}`;
+    localStorage[val] = task.innerText;
+    localStorage[cls] = task.className;
   }
 }
 
@@ -50,8 +75,12 @@ function doubleTaskEvent(e) {
   }
 }
 
+loadTasks();
+
 addTask.addEventListener('click', addTaskEvent);
 clearTasks.addEventListener('click', clearTasksEvent);
 endedTasks.addEventListener('click', endedTasksEvent);
+salveTasks.addEventListener('click', salveTasksEvent);
+
 listTask.addEventListener('click', clickTaskEvent);
 listTask.addEventListener('dblclick', doubleTaskEvent);
