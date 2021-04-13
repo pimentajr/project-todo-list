8fec25d8a19b5c7a581c8a1287b27b5b596a296c
renan <renan_menezes07@hotmail.com>
2021-04-13 16:48:05 -0300

    Cumpre exigências do requisio 13.

--- script.js
+++ script.js
@@ -16,8 +16,9 @@ button.addEventListener('click', addListItem);
 function removeColor() {
   const toDoList = document.querySelectorAll('li');
   for (let child = 0; child < toDoList.length; child += 1) {
-    if (toDoList[child].style.backgroundColor === 'rgb(128, 128, 128)') {
-      toDoList[child].style.backgroundColor = 'rgb(255, 255, 255)';
+    if (toDoList[child].classList.contains('selected')) {
+      toDoList[child].classList.remove('selected');
+      toDoList[child].style.backgroundColor = '';
     }
   }
 }
@@ -26,7 +27,7 @@ function removeColor() {
 function changeColor(event) {
   removeColor();
   const itemSelected = event.target;
-  itemSelected.style.backgroundColor = 'rgb(128,128,128)';
+  itemSelected.classList.add('selected');
 }
 let toDoList = document.querySelector('#lista-tarefas');
 toDoList.addEventListener('click', changeColor);
@@ -34,7 +35,7 @@ toDoList.addEventListener('click', changeColor);
 // Risca o item ao ser clicado 2 vezes.
 function crossItem(event) {
   const itemSelected = event.target;
-  if (itemSelected.className === 'completed') {
+  if (itemSelected.classList.contains('completed')) {
     itemSelected.classList.remove('completed');
   } else {
     itemSelected.classList.add('completed');
@@ -68,7 +69,7 @@ removeFinishedButton.innerText = 'Limpar Completos';
 function removeFinishedItens() {
   toDoList = document.querySelectorAll('li');
   for (let item = 0; item < toDoList.length; item += 1) {
-    if (toDoList[item].className === 'completed') {
+    if (toDoList[item].classList.contains('completed')) {
       toDoList[item].remove();
     }
   }
@@ -81,7 +82,7 @@ saveListButton.id = 'salvar-tarefas';
 buttonSection.appendChild(saveListButton);
 saveListButton.innerText = 'Salvar lista';
 
-// Salva conteúdo da lista.
+// Salva conteúdo da lista em uma key chamada 'savedList'.
 function saveList() {
   const list = document.querySelector('ol').innerHTML;
   localStorage.setItem('savedList', list);
@@ -92,3 +93,51 @@ saveListButton.addEventListener('click', saveList);
 const savedList = localStorage.getItem('savedList');
 const showedList = document.querySelector('ol');
 showedList.innerHTML = savedList;
+
+// Cria botão "mover-cima".
+const upButton = document.createElement('button');
+upButton.id = 'mover-cima';
+buttonSection.appendChild(upButton);
+upButton.innerText = 'Go up';
+
+// Move item para cima.
+function moveUp() {
+  let itemSelected = document.querySelector('.selected');
+  if (String(itemSelected) !== 'null') {
+    let previousItem = document.querySelector('.selected').previousElementSibling;
+    if (String(previousItem) !== 'null') {
+      const holder = previousItem.innerHTML;
+      previousItem = itemSelected.innerHTML;
+      itemSelected = holder;
+      document.querySelector('.selected').innerText = itemSelected;
+      document.querySelector('.selected').previousElementSibling.innerText = previousItem;
+      document.querySelector('.selected').previousElementSibling.classList.add('selected');
+      document.querySelectorAll('.selected')[1].classList.remove('selected');
+    }
+  }
+}
+upButton.addEventListener('click', moveUp);
+
+// Cria botão "mover-baixo".
+const downButton = document.createElement('button');
+downButton.id = 'mover-baixo';
+buttonSection.appendChild(downButton);
+downButton.innerText = 'Go down';
+
+// Move item para cima.
+function moveDown() {
+  let itemSelected = document.querySelector('.selected');
+  if (String(itemSelected) !== 'null') {
+    let nextItem = document.querySelector('.selected').nextElementSibling;
+    if (String(nextItem) !== 'null') {
+      const holder = nextItem.innerHTML;
+      nextItem = itemSelected.innerHTML;
+      itemSelected = holder;
+      document.querySelector('.selected').innerText = itemSelected;
+      document.querySelector('.selected').nextElementSibling.innerText = nextItem;
+      document.querySelector('.selected').nextElementSibling.classList.add('selected');
+      document.querySelectorAll('.selected')[0].classList.remove('selected');
+    }
+  }
+}
+downButton.addEventListener('click', moveDown);
