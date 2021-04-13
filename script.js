104dd08b912829cd7b8bbb0ffaabfa0403b7b287
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-12 21:24:14 -0300

    Requisito 9 realizado

--- script.js
+++ script.js
@@ -4,17 +4,27 @@ const btnCreateTask = document.getElementById('criar-tarefa');
 
 function changeBackgroundColorTask(event) {
   for (let index = 0; index < jobList.children.length; index += 1) {
-    jobList.children[index].setAttribute('id', ''); 
+    jobList.children[index].setAttribute('id', '');
   }
   event.target.setAttribute('id', 'selectedTask');
 }
 
- //Adiciona a task ao clicar no botao.
+function completedTask(event) {
+  if (event.target.className === 'task') {
+    event.target.setAttribute('class', 'task completed');
+  } else {
+    event.target.setAttribute('class', 'task');
+  }
+}
+
+//Adiciona a task ao clicar no botao.
+
 function addTask() {
   const taskToBeAdded = document.createElement('li');
   taskToBeAdded.innerText = taskNameInput.value;
   taskToBeAdded.setAttribute('class', 'task');
   taskToBeAdded.addEventListener('click', changeBackgroundColorTask);
+  taskToBeAdded.addEventListener('dblclick', completedTask);
   jobList.appendChild(taskToBeAdded);
 
   taskNameInput.value = '';
