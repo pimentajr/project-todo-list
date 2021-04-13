4ecf5c0d910b65a72e22d025b3a06eae494cc36b
Savio Moraes <smmsavio@hotmail.com>
2021-04-12 23:05:08 -0300

    Adiciona correcao do eslint

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+const title = document.createElement('header');
+const titleText = document.createElement('h1');
+const titleParagraph = document.createElement('p');
+
 function insertTitle() {
-  const title = document.createElement('header');
   title.id = 'title';
   document.body.appendChild(title);
-  const titleText = document.createElement('h1');
   titleText.id = 'title-text';
   titleText.textContent = 'Minha Lista de Tarefas';
   title.appendChild(titleText);
 }
 
 function insertParagraphToTitle() {
-  const titleParagraph = document.createElement('p');
   titleParagraph.id = 'funcionamento';
   titleParagraph.textContent = 'Clique duas vezes em um item para marcá-lo como completo';
   title.appendChild(titleParagraph);
