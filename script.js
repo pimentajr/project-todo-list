007eecd074c8c15f45acd0b519a7e1508668cef2
guinhoanjos <iago.anjos@hotmail.com>
2021-04-13 01:08:17 -0300

    resetando valor no event

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ document.getElementById('criar-tarefa').addEventListener('click', function() {
     let liCriada = document.createElement('li');
     liCriada.innerText = document.getElementById('texto-tarefa').value;
     document.getElementById('lista-tarefas').appendChild(liCriada);
+    document.getElementById('texto-tarefa').value = ''
 })
