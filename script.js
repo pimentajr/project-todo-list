7c2d7474f8b3198d8b6f66d2b2ea70ef67542eb3
murilodospacos <murilopacos@gmail.com>
2021-04-13 08:42:04 -0300

    Exercicio 7 e 8 1 tentativa conclusao

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-window.onload = () => {
-  const addTask = document.getElementById('criar-tarefa');
+const addTask = document.getElementById('criar-tarefa');
+const listTasks = document.getElementById('lista-tarefas');
 
-  addTask.addEventListener('click', () => {
-    const listTasks = document.getElementById('lista-tarefas');
-    const textTalks = document.getElementById('texto-tarefa');
-    const itenTasks = document.createElement('li');
-    itenTasks.innerText = textTalks.value;
-    listTasks.appendChild(itenTasks);
-    textTalks.value = '';
-  });
-};
+addTask.addEventListener('click', () => {
+  const textTalks = document.getElementById('texto-tarefa');
+  const itenTasks = document.createElement('li');
+  itenTasks.className = 'myTasks';
+  itenTasks.innerText = textTalks.value;
+  listTasks.appendChild(itenTasks);
+  textTalks.value = '';
+});
+
+listTasks.addEventListener('click', (event) => {
+  const myEvent = event.target;
+  for (let index = 0; index < listTasks.children.length; index += 1) {
+    if (listTasks.children[index] !== myEvent) {
+      listTasks.children[index].style.backgroundColor = 'white';
+      listTasks.children[index].classList.remove('selected');
+    }
+  }
+  myEvent.classList.add('selected');
+  myEvent.style.backgroundColor = 'rgb(128, 128, 128)';
+});
