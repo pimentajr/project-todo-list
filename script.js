0e2f05dca8a3488a39b9c26a2954187415210035
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-13 16:57:36 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 const orderList = document.getElementById('lista-tarefas');
 
 function addToList() {
-const list = document.getElementById('texto-tarefa');
-const listValue = list.value;
-const itemList = document.createElement('li');
-if (listValue === '') {
-alert('Escreva uma Tarefa');
-} else {
-itemList.innerHTML = listValue;
-orderList.appendChild(itemList);
-list.value = '';
-}
+  const list = document.getElementById('texto-tarefa');
+  const listValue = list.value;
+  const itemList = document.createElement('li');
+  if (listValue === '') {
+    alert('Escreva uma Tarefa');
+  }else {
+    itemList.innerHTML = listValue;
+    orderList.appendChild(itemList);
+    list.value = '';
+  }
 }
 addToList();
 
-// desafio 7 e 8 https://www.sitepoint.com/community/t/how-to-make-background-transparent-with-javascript/8129
-
-// desafio 9
-
-
-
-// desafio 10
-
+//const changeListColor = document.getElementsByTagName('li');
+const colorUl = document.getElementById('lista-tarefas');
+function changeColor(evento) {
+  for (let index = 0; index < colorUl.children.length; index +=1) {
+  if (colorUl.children[index] !== evento); {
+      colorUl.children[index].classList.remove('selected');
+} 
+}
+evento.target.className = 'selected';
+evento.classList.add('selected');
+}
+colorUl.addEventListener('click', changeColor);
+changeColor();
