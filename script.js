d84a4785c8f895e981bcf701f1b6e0ea1ca25a7a
Antonio Malato <malatoneto@gmail.com>
2021-04-13 13:42:59 -0300

    Test

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-const boxList = document.getElementById('box-list');
-const createList = document.createElement('ol');
-createList.id = 'lista-tarefas';
-boxList.appendChild(createList);
+const button = document.getElementById('criar-tarefa');
+
+const orderedList = document.getElementById('lista-tarefas');
+
+const createItemList = document.createElement('li');
+createItemList.id = 'item-list';
+orderedList.appendChild(createItemList)
+
+createItemList.appendChild(value);
+
+const input = document.getElementById('texto-tarefa');
+
+function getText() {
+    let valueItem = input.value;
+
+    console.log(valueItem)
+}
+
+button.addEventListener('click', getText);
