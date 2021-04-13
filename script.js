8253f8fbaedcf8da42a34a4211cf7d941da5b64b
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-13 14:16:00 -0300

    testando

--- script.js
+++ script.js
@@ -15,19 +15,6 @@ list.value = '';
 addToList();
 
 // desafio 7 e 8 https://www.sitepoint.com/community/t/how-to-make-background-transparent-with-javascript/8129
-const changeListColor = document.getElementsByTagName('li');
-const colorUl = document.getElementById('lista-tarefas');
-
-function changeItemColor(evento) {
-for (let index = 0; index < colorUl.children.length; index +=1) {
-    if (colorUl.children[index] !== evento.target){
-        colorUl.children[index].style.backgroundColor = 'transparent';
-    }
-}
-   evento.target.style.backgroundColor = 'rgb(128, 128, 128)';
-}
-colorUl.addEventListener('click', changeItemColor);
-changeItemColor ();
 
 // desafio 9
 
