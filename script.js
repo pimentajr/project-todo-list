308b5ba81dd04b448fd7f425f5a6d83b52829c78
Hugo Ferreira <hugonardo@gmail.com>
2021-04-12 22:17:20 -0300

    Requisitos 7 e 8
    7. Clicar em um item da lista deve alterar a cor de fundo do item para
       cinza rgb(128,128,128)
    8. Não deve ser possível selecionar mais de um elemento da lista ao
       mesmo tempo

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+const taskList = document.querySelector('#lista-tarefas');
+
 window.onload = () => {
   function bindListeners() {
-    document.querySelector('#criar-tarefa').onclick = onClickAddButton;
+    document.querySelector('#criar-tarefa')
+      .addEventListener('click', onClickAddButton);
+
+    taskList.addEventListener('click', onClickTask);
   }
 
   bindListeners();
@@ -15,12 +20,28 @@ function onClickAddButton() {
 }
 
 function addTaskToList(task) {
-  document.querySelector('#lista-tarefas')
-    .appendChild(createTaskElement(task));
+  taskList.appendChild(createTaskElement(task));
 }
 
 function createTaskElement(task) {
   const element = document.createElement('li');
   element.innerText = task;
   return element;
+}
+
+function onClickTask(event) {
+  const task = event.target;
+  selectTask(task);
+  unselectAllTasksUnless(task);
+}
+
+function selectTask(task) {
+  task.classList.toggle('selected');
+}
+
+function unselectAllTasksUnless(task) {
+  Array.from(taskList.children).forEach((element) => {
+    if (element === task) return;
+    element.classList.remove('selected');
+  });
 }
