c4e13d1b1c2ad08669a8750d13a0cc21b829dc57
israeltonholo <israeltonholo@hotmail.com>
2021-04-13 16:13:33 -0300

    adiciona ex12

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
+const listaDeTarefas = document.querySelector('#lista-tarefas');
 function elementFromInput() {
   const inputText = document.getElementById('texto-tarefa');
-  const listTask = document.querySelector('#lista-tarefas');
+  const listTask = listaDeTarefas;
   const createdIl = document.createElement('li');
   createdIl.innerText = inputText.value;
   listTask.appendChild(createdIl);
@@ -28,12 +29,12 @@ function markElement(element) {
   }
 }
 function selectListItens() {
-  const listItens = document.getElementById('lista-tarefas');
+  const listItens = listaDeTarefas;
   listItens.addEventListener('click', toColorIten);
   listItens.addEventListener('dblclick', markElement);
 }
 function eraseList() {
-  const parentyList = document.getElementById('lista-tarefas');
+  const parentyList = listaDeTarefas;
   parentyList.innerHTML = '';
 }
 function deleteList() {
@@ -50,11 +51,55 @@ function concludeItens() {
   const removeConcludeBotton = document.querySelector('#remover-finalizados');
   removeConcludeBotton.addEventListener('click', deleteItens);
 }
+function saveItens() {
+  const allItens = listaDeTarefas.children;
+  let allListItens = '';
+  for (let index = 0; index < allItens.length; index += 1) {
+    if (allItens[index].className === 'completed') {
+      allListItens += `${allItens[index].outerText}¢`;
+    } else {
+      allListItens += `${allItens[index].outerText}§`;
+    }
+    console.log(allListItens);
+  }
+  sessionStorage.setItem('liList', `${allListItens}`);
+}
+function endLiNormal(inputElement, auxVariable) {
+  const allItens = listaDeTarefas;
+  let auxVar = auxVariable;
+  if (inputElement === '§') {
+    const getLi = document.createElement('li');
+    getLi.innerText = auxVariable;
+    allItens.appendChild(getLi);
+    auxVar = '';
+  } else if (inputElement === '¢') {
+    const getLi = document.createElement('li');
+    getLi.innerText = auxVariable;
+    getLi.classList.add('completed');
+    allItens.appendChild(getLi);
+    auxVar = '';
+  } else {
+    auxVar += inputElement;
+  }
+  return auxVar;
+}
+function setItens() {
+  const varSession = sessionStorage.liList;
+  if (varSession !== undefined) {
+    let auxVariable = '';
+    for (let index = 0; index < sessionStorage.liList.length; index += 1) {
+      auxVariable = endLiNormal(sessionStorage.liList[index], auxVariable);
+    }
+  }
+  const salvaTarefas = document.getElementById('salvar-tarefas');
+  salvaTarefas.addEventListener('click', saveItens);
+}
 function loadWindow() {
   addElementTable();
   selectListItens();
   deleteList();
   concludeItens();
+  setItens();
 }
 
 window.onload = loadWindow;
