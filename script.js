58f0c45c79d7e85a82cdb488eac432c85d95ccdf
Gustavo Mendes <gusm217@gmail.com>
2021-04-13 11:22:24 -0300

    Adiciona o 10

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let taskBoard = document.getElementById('lista-tarefas');
+let liItems = document.getElementsByTagName('li');
+
 // 5 - 
 document.getElementById('criar-tarefa').addEventListener('click', function(event) {
   let li = document.createElement('li');
@@ -9,10 +12,28 @@ document.getElementById('criar-tarefa').addEventListener('click', function(event
 
 // Insights de alguns PRs dos colegas <3
 // 7, 8 
-document.getElementById('lista-tarefas').addEventListener('click', function(event) {
-	let liItems = document.getElementsByTagName('li');
+taskBoard.addEventListener('click', function(event) {
 	for (let index = 0; index < liItems.length; index += 1) {
 		liItems[index].classList.remove('pickedItem');
 	}
 	event.target.classList.add('pickedItem');
 });
+
+// 9 -
+taskBoard.addEventListener('dblclick', function(event) {			
+	for (let index = 0; index < liItems.length; index += 1) {
+		if (event.target.classList.contains('completed')) {
+			event.target.classList.remove('completed');			
+		} else {
+		event.target.classList.add('completed');
+		}
+		console.log(event.target)
+	}	
+})
+
+// 10 -
+document.getElementById('apaga-tudo').addEventListener('click', (event) => {
+	while (taskBoard.lastChild) {
+		taskBoard.removeChild(taskBoard.lastChild);
+	}
+})
