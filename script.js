a75cb7251ed504070fb6e18f720f83fa2411a9c9
guinhoanjos <iago.anjos@hotmail.com>
2021-04-13 01:14:43 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.getElementById('criar-tarefa').addEventListener('click', function() {
     document.getElementById('lista-tarefas').appendChild(liCriada);
     document.getElementById('texto-tarefa').value = ''
 })
+document.getElementById('lista-tarefas').addEventListener('click', function (event) {
+    event.target.style.backgroundColor = 'rgb(128, 128, 128'
+})
