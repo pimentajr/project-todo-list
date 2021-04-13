f1d8632773034d18ad95d5860058fb2d3346dace
lucasSCsantos <luc.cristovam10@gmail.com>
2021-04-12 23:23:21 -0300

    funçao salvar todos implementada

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const toDoList = document.querySelector('#lista-tarefas');
 const createToDo = document.getElementById('criar-tarefa');
 const removeAll = document.getElementById('apaga-tudo');
 const toDos = document.getElementsByClassName('toDo');
-const removeCompleted = document.getElementById('remover-finalizados')
-const completedToDos = document.getElementsByClassName('completed')
+const removeCompleted = document.getElementById('remover-finalizados');
+const completedToDos = document.getElementsByClassName('completed');
+const saveAll = document.getElementById('salvar-tarefas');
 
 function addToDo() {
   const toDo = document.createElement('li');
@@ -45,6 +46,39 @@ function removeCompletedToDos() {
   }
 }
 
+function saveToDos() {
+  const savedToDos = [];
+  for (let index = 0; index < toDos.length; index += 1) {
+
+    savedToDos[index] = {
+      toDoName: toDos[index].innerHTML,
+      toDoClass: toDos[index].className,
+    }
+
+    localStorage.toDo = JSON.stringify(savedToDos);
+    
+  }
+  for(let todo of JSON.parse(localStorage.toDo)){
+    console.log(todo.toDoClass);
+  }
+}
+
+function reloadToDos() {
+    for(let todo of JSON.parse(localStorage.toDo)){
+      const toDoReload = document.createElement('li');
+      toDoReload.innerText = todo.toDoName;
+      toDoReload.className = todo.toDoClass;
+      console.log(toDoReload);
+      toDoList.appendChild(toDoReload);
+    }
+  
+}
+
+window.onload = function () {
+  reloadToDos();
+}
+
+saveAll.addEventListener('click', saveToDos)
 removeCompleted.addEventListener('click', removeCompletedToDos);
 removeAll.addEventListener('click', removeAllToDos);
 addEventListener('dblclick', toDoCompleted);
