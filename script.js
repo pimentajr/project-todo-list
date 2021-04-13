1e856f1c05ba492192a798604ae1293d4fb4ea4b
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-13 20:28:07 -0300

    Requisito 5 e 6

--- script.js
+++ script.js
@@ -2,3 +2,14 @@
 Project Todo List
 @author Fernando Lasmar
 */
+// Requisito 5
+const adicionarItens = document.getElementById('criar-tarefa');
+
+adicionarItens.addEventListener('click', () => {
+  const tarefa = document.getElementById('texto-tarefa');
+  const lista = document.getElementById('lista-tarefas');
+  const criarItem = document.createElement('li');
+  criarItem.innerText = tarefa.value;
+  lista.appendChild(criarItem);
+  tarefa.value = '';
+});
