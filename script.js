68afd703d1478ca4111dc2146d9d4eeff9b2a5ef
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-13 18:50:24 -0300

    Adiciona requisito 07

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const createTask = document.getElementById('criar-tarefa');
 const listTask = document.getElementById('lista-tarefas');
 const textTask = document.getElementById('texto-tarefa');
+// const createListItem = document.createElement('li');
 
+//Requisitos 05 e 06
 createTask.addEventListener('click', () => {
-    const createListItem = document.createElement('li');
-    createListItem.innerText = textTask.value;
-    listTask.appendChild(createListItem);
-    textTask.value = '';
-})
+  const createListItem = document.createElement('li');
+  createListItem.innerText = textTask.value;
+  listTask.appendChild(createListItem);
+  textTask.value = '';
+});
+
+//Requisito 07
+const getOL = document.getElementById('lista-tarefas');
+getOL.addEventListener('click', (event) => {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128';
+});
