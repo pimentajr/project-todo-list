a56f0360fa1a552513a5789cf0177f3d3ad11cb8
Alexandre <naitwa.alexandre@gmail.com>
2021-04-13 15:48:02 -0300

    teste

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 const btnTasks = document.querySelector('#criar-tarefa');
 const listOfTasks = document.querySelector('#lista-tarefas');
 const inputText = document.querySelector('#texto-tarefa');
+const taskItem = listOfTasks.childNodes;
 btnTasks.addEventListener('click', () => {
   const li = document.createElement('li');
-  const taskItem = listOfTasks.childNodes;
   li.innerHTML = inputText.value;
   inputText.value = '';
   if (li.innerHTML === '') {
@@ -12,17 +12,4 @@ btnTasks.addEventListener('click', () => {
   } else {
     listOfTasks.appendChild(li);
   }
-  li.addEventListener('click', (e) => {
-    for (let i = 0; i < taskItem.length; i += 1) {
-      taskItem[i].classList.remove('selected');
-    }
-    e.target.classList.add('selected');
-  });
-  li.addEventListener('dblclick', (e) => {
-    if (!e.target.classList.contains('completed')) {
-      e.target.classList.add('completed');
-    } else {
-      e.target.classList.remove('completed');
-    }
-  })
 });
