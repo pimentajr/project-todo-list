f3fa29d4343511954cd585ef19467d681239c94c
Bruno Duarte <brunoarduarte@gmail.com>
2021-04-13 10:18:36 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -6,5 +6,17 @@ addListButton.addEventListener('click', () => {
   const elementLi = document.createElement('li');
   elementLi.innerText = inputList.value;
   orderedList.appendChild(elementLi);
+  elementLi.classList.add('list');
   inputList.value = '';
 });
+
+function addBackground() {
+  orderedList.addEventListener('click', (event) => {
+    const selectedLi = document.querySelector('.selected');
+    if (selectedLi != null) {
+      document.querySelector('.selected').classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
+addBackground();
