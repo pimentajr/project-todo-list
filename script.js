8bdb719d51758deb2f9619d287511089f325e239
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-12 21:01:45 -0300

    Requisitos 6 e 7 realizados

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 const jobList = document.getElementById('lista-tarefas');
-let taskNameInput = document.getElementById('texto-tarefa');
+const taskNameInput = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 
-//Adiciona a task ao clicar no botao.
-function addTask(string) {
-  const taskToBeAdded = document.createElement('li');
-  taskToBeAdded.innerText = taskNameInput.value;
-  jobList.appendChild(taskToBeAdded);
-  taskNameInput.value = '';
+function changeBackgroundColorTask(event) {
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
+ //Adiciona a task ao clicar no botao.
+function addTask() {
+  const taskToBeAdded = document.createElement('li');
+  taskToBeAdded.innerText = taskNameInput.value;
+  taskToBeAdded.setAttribute('class', 'task');
+  taskToBeAdded.addEventListener('click', changeBackgroundColorTask);
+  jobList.appendChild(taskToBeAdded);
+
+  taskNameInput.value = '';
+}
 
 btnCreateTask.addEventListener('click', addTask);
