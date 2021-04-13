ec5b98a9af56d34bfee86132eae43c714af578d5
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-13 17:26:16 -0300

    Realizacao do desafio 4 e 5

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-const newHeader = document.getElementsByTagName('header')[0];
+const newDiv = document.querySelector('.new-box');
 
-
+// Desafio 4
 const createOl = document.createElement('ol');
 createOl.id = 'lista-tarefas';
+newDiv.appendChild(createOl);
+console.log();
+
+// Desafio 5
+const addButton = document.createElement('button');
+addButton.id = 'criar-tarefa';
+addButton.innerText = 'Adicionar';
+
+function inputButton() {
+  const inputItem = document.getElementById('texto-tarefa');
+  newDiv.appendChild(addButton);
+
+}
+
+inputButton();
