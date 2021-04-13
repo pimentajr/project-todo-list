92091f9dfb531e86fcf3ec28ed5f39bdf82d53d0
tulio sploradori <tuliospl@gmail.com>
2021-04-13 14:08:03 -0300

    correçao do erro item 3

---
+++ script.js
@@ -0,0 +1,6 @@
+const button = document.getElementById('criar-tarefa');
+const entradaDeTexto = document.getElementById('texto-tarefas');
+const listaOrdenada = document.getElementById('lista-tarefas');
+function criaLista (){
+ 
+}
