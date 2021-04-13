c5c127f3c721c2905364148c4cfddafeac42f25d
MIkaela Braga <bragamikaela@gmail.com>
2021-04-12 22:40:42 -0300

    requisito 3

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const paragrafo = document.createElement('p');
 paragrafo.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
 paragrafo.id = 'funcionamento';
 document.body.appendChild(paragrafo);
+
+function createInputText() {
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const input = document.createElement('input');
+  input.setAttribute('type', 'text');
+  input.id = 'texto-tarefa';
+  div.appendChild(input);
+}
+createInputText();
