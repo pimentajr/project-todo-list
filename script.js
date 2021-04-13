19e3948efd1d1efd13df2dfc1ed0df3bcc83fb81
tulio sploradori <tuliospl@gmail.com>
2021-04-13 18:44:00 -0300

    conclusao do item 6

--- script.js
+++ script.js
@@ -2,12 +2,17 @@ const button = document.getElementById('criar-tarefa');
 const listaOrdenada = document.getElementById('lista-tarefas');
 
 function criaLista() {
- const entradaDeTexto = document.getElementById('texto-tarefa'); 
- button.addEventListener('click', function () {
-  const recebe = document.createElement('li');
-  recebe.innerHTML = entradaDeTexto.value;
-  listaOrdenada.appendChild(recebe);
-  entradaDeTexto.value = null;
- });
+  const entradaDeTexto = document.getElementById('texto-tarefa');
+  button.addEventListener('click', function () {
+    const recebe = document.createElement('li');
+    recebe.className = 'lista-marcador';
+    recebe.innerHTML = entradaDeTexto.value;
+    recebe.addEventListener('click', function (e){
+      e.target.style.backgroundColor = 'rgb(128, 128, 128)';
+      
+    })
+    listaOrdenada.appendChild(recebe);
+    entradaDeTexto.value = null;
+  });
 }
 criaLista();
