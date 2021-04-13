5645b8986df4d568e124fa7315a82529a2c8e016
jhenriquemargrafm <jh.margraf@gmail.com>
2021-04-13 10:17:36 -0300

    Requisito 13: ajuste inicial

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const newTask = document.querySelector('#texto-tarefa');
 const clickCreateTask = document.getElementById('criar-tarefa');
 const allList = document.querySelector('#lista-tarefas');
 const grayColor = 'rgb(128, 128, 128)';
-const saveButton = document.getElementById('salvar-tarefas')
+const saveButton = document.getElementById('salvar-tarefas');
 
 function addingTask() {
   clickCreateTask.addEventListener('click', () => {
@@ -75,12 +75,45 @@ removeCheckedTasks();
 
 // Requisito 12 - Salvar tarefas
 function storageItens() {
-    saveButton.addEventListener('click', () => {
-      localStorage.setItem('Tarefas', allList.innerHTML);
-    });
-    const savedList = localStorage.getItem('Tarefas');
-    if (savedList) allList.innerHTML = savedList;
-    // https://stackoverflow.com/questions/8860654/javascript-single-line-if-statement-best-syntax-this-alternative
-    // Plantão dia 12/04 uso da condicional de existência
-  }
+  saveButton.addEventListener('click', () => {
+    localStorage.setItem('Tarefas', allList.innerHTML);
+  });
+  const savedList = localStorage.getItem('Tarefas');
+  if (savedList) allList.innerHTML = savedList;
+  // https://stackoverflow.com/questions/8860654/javascript-single-line-if-statement-best-syntax-this-alternative
+  // Plantão dia 12/04 uso da condicional de existência
+}
 storageItens();
+
+// Requisito 13 - Botões para mover para cima e para baixo
+function moveUp() {
+  const allItens = document.querySelectorAll('li');
+  const moveUpButton = document.getElementById('mover-cima');
+  moveUpButton.addEventListener('click', () => {
+    for (let index = 1; index < allItens.length; index += 1) {
+      if (allItens[index].style.backgroundColor === grayColor) {
+        allList.insertBefore(allItens[index], allItens[(index-1)]);
+      }
+    }
+  });
+}
+moveUp();
+
+function moveDown() {
+  const allItens = document.querySelectorAll('li');
+  const moveDownButton = document.getElementById('mover-baixo');
+  moveDownButton.addEventListener('click', () => {
+    for (let index = 0; index < allItens.length; index += 1) {
+      if (allItens[index].style.backgroundColor === grayColor) {
+        allList.insertBefore(allItens[index+1], allItens[index]);
+      }
+    }
+  });
+}
+moveDown();
+     
+
+
+
+
+
