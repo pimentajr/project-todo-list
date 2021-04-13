6d285e2eeb686c1b8a8f985c6943e798537f0013
Hugo Ferreira <hugonardo@gmail.com>
2021-04-13 05:08:53 -0300

    Requisito 10
    10. Adicione um botão com id="apaga-tudo" que quando clicado deve apagar
        todos os itens da lista

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ window.onload = () => {
 function bindListeners() {
   document.querySelector('#criar-tarefa')
     .addEventListener('click', onClickButtonAdd);
+  document.querySelector('#apaga-tudo')
+    .addEventListener('click', onClickButtonClearList);
 
   taskList.addEventListener('click', onClickTask);
   taskList.addEventListener('dblclick', onDoubleClickTask);
@@ -19,6 +21,10 @@ function onClickButtonAdd() {
     addTaskToList(taskText.value);
     taskText.value = null;
   }
+}
+
+function onClickButtonClearList() {
+  clearAllTasks();
 }
 
 function addTaskToList(task) {
@@ -50,4 +56,8 @@ function unselectAllTasksUnless(task) {
 
 function onDoubleClickTask(event) {
   event.target.classList.toggle('completed');
+}
+
+function clearAllTasks() {
+  taskList.innerHTML = '';
 }
