94786291c470a237ae8ac7f0baadba219f837401
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-04-13 18:59:21 -0300

    Arruma todos os problemas do lint

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
-window.onload = function () {
+window.onload = function loadPage() {
   const list = document.querySelector('#lista-tarefas');
   list.innerHTML = localStorage.lista;
-}
+};
 
 const inputArea = document.getElementById('texto-tarefa');
 const button = document.querySelector('#criar-tarefa');
 const list = document.querySelector('#lista-tarefas');
+const classColorList = 'list-color';
 
-function onChange(event){
-  let text = event.target.value;
+function onChange(event) {
+  const text = event.target.value;
   inputArea.value = text;
 }
 
 inputArea.addEventListener('change', onChange);
 
-function insertItem () {;
-  const newLi = document.createElement('li')
+function insertItem() {
+  const newLi = document.createElement('li');
   newLi.innerText = inputArea.value;
   list.appendChild(newLi);
   inputArea.value = '';
@@ -23,22 +24,22 @@ function insertItem () {;
 
 button.addEventListener('click', insertItem);
 
-function setColorToList (event) {
-  let listItem = document.querySelectorAll('li');
-  for(let index = 0; index < listItem.length; index +=1) {
+function setColorToList(event) {
+  const listItem = document.querySelectorAll('li');
+  for (let index = 0; index < listItem.length; index += 1) {
     listItem[index].removeAttribute('id');
   }
   const li = event.target;
-  li.setAttribute('id', 'list-color');
+  li.setAttribute('id', classColorList);
 }
 
 list.addEventListener('click', setColorToList);
 
-function setMarkedAsDone (event) {
+function setMarkedAsDone(event) {
   const li = event.target;
-  if(li.classList.contains('completed')) {
+  if (li.classList.contains('completed')) {
     li.classList.remove('completed');
-  } else{
+  } else {
     li.classList.add('completed');
   }
 }
@@ -47,9 +48,9 @@ list.addEventListener('dblclick', setMarkedAsDone);
 
 const buttonErase = document.querySelector('#apaga-tudo');
 
-function eraseList () {
-  let listItem = document.querySelectorAll('li');
-  for(let index =0; index < listItem.length; index+=1) {
+function eraseList() {
+  const listItem = document.querySelectorAll('li');
+  for (let index = 0; index < listItem.length; index += 1) {
     list.removeChild(listItem[index]);
   }
 }
@@ -58,20 +59,20 @@ buttonErase.addEventListener('click', eraseList);
 
 const buttonRemoveDone = document.querySelector('#remover-finalizados');
 
-function eraseCompleted () {
-  let listItem = document.querySelectorAll('li');
-  for(let index =0; index < listItem.length; index+=1) {
-    if(listItem[index].classList.contains('completed')) {
+function eraseCompleted() {
+  const listItem = document.querySelectorAll('li');
+  for (let index = 0; index < listItem.length; index += 1) {
+    if (listItem[index].classList.contains('completed')) {
       list.removeChild(listItem[index]);
     }
-  } 
+  }
 }
 
 buttonRemoveDone.addEventListener('click', eraseCompleted);
 
 const buttonSave = document.querySelector('#salvar-tarefas');
 
-function saveList () {
+function saveList() {
   localStorage.setItem('lista', list.innerHTML);
 }
 
@@ -79,10 +80,10 @@ buttonSave.addEventListener('click', saveList);
 
 const buttonRemoveSelected = document.querySelector('#remover-selecionado');
 
-function eraseSelected () {
-  let listItem = document.querySelectorAll('li');
-  for(let index =0; index < listItem.length; index+=1) {
-    if(listItem[index].id === 'list-color'){
+function eraseSelected() {
+  const listItem = document.querySelectorAll('li');
+  for (let index = 0; index < listItem.length; index += 1) {
+    if (listItem[index].id === classColorList) {
       list.removeChild(listItem[index]);
     }
   }
@@ -92,44 +93,43 @@ buttonRemoveSelected.addEventListener('click', eraseSelected);
 
 const buttonUp = document.querySelector('#mover-cima');
 
-function moveUp () {
+function moveUp() {
   const li = document.querySelectorAll('li');
-  for(let index = 0; index < li.length; index += 1) {
-    if(li[index].id === 'list-color' && index !==0) {
-      let element = li[index].innerHTML;
-      let element2 = li[index -1].innerHTML;
-      let elementClass = li[index].className;
-      let element2Class = li[index -1].className;
-      
+  for (let index = 0; index < li.length; index += 1) {
+    if (li[index].id === classColorList && index !== 0) {
+      const element = li[index].innerHTML;
+      const element2 = li[index - 1].innerHTML;
+      const elementClass = li[index].className;
+      const element2Class = li[index - 1].className;
       li[index].innerHTML = element2;
       li[index - 1].innerHTML = element;
-      li[index].removeAttribute('id', 'list-color');
-      li[index -1].setAttribute('id', 'list-color');
+      li[index].removeAttribute('id', classColorList);
+      li[index - 1].setAttribute('id', classColorList);
       li[index].className = element2Class;
       li[index - 1].className = elementClass;
     }
   }
 }
 
-buttonUp.addEventListener('click', moveUp)
+buttonUp.addEventListener('click', moveUp);
 
 const buttonDown = document.querySelector('#mover-baixo');
 
-function moveDown () {
-  const selected = document.getElementById('list-color');
+function moveDown() {
+  const selected = document.getElementById(classColorList);
   const li = document.querySelectorAll('li');
-  if(selected !== li[li.length-1] && selected){
+  if (selected !== li[li.length - 1] && selected) {
     const selectedInner = selected.innerText;
-  const selectedClass = selected.className;
-  const nextElClass = selected.nextElementSibling.className;
+    const selectedClass = selected.className;
+    const nextElClass = selected.nextElementSibling.className;
 
-  selected.innerText = selected.nextElementSibling.innerHTML;
-  selected.className = nextElClass;
-  selected.nextElementSibling.innerHTML = selectedInner;
-  selected.nextElementSibling.className = selectedClass;
-  selected.nextElementSibling.setAttribute('id', 'list-color');
-  selected.removeAttribute('id');
-    }
+    selected.innerText = selected.nextElementSibling.innerHTML;
+    selected.className = nextElClass;
+    selected.nextElementSibling.innerHTML = selectedInner;
+    selected.nextElementSibling.className = selectedClass;
+    selected.nextElementSibling.setAttribute('id', classColorList);
+    selected.removeAttribute('id');
   }
+}
 
 buttonDown.addEventListener('click', moveDown);
