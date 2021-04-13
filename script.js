4bf72f9a777235b6adc31e3733e4b5c304353f33
Gabriela Nebbia <gabrielanebbia@gmail.com>
2021-04-12 21:06:21 -0300

    Requisito 7 - Adiciona a funcao que muda a cor de fundo ao clicar em um item da lista

--- script.js
+++ script.js
@@ -6,9 +6,22 @@ const newItem = document.getElementById('texto-tarefa'); // caixa de texto
 
 const toDoList = document.getElementById('lista-tarefas'); // lista ordenada
 
+/* const createdItem = document.getElementsByTagName('li').innerHTML; */
+
 buttonAdd.addEventListener('click', function addItemList() {
   const itemList = document.createElement('li');
   toDoList.appendChild(itemList);
   itemList.innerHTML = newItem.value;
   newItem.value = '';
 });
+
+function color() {
+  const toDoListItens = document.querySelectorAll('#lista-tarefas');
+  for (let index = 0; index < toDoListItens.length; index += 1) {
+    toDoListItens[index].addEventListener('click', function backgroundColorItem(event) {
+      event.target.classList.add('backgroundItem');
+    });
+  }
+}
+
+color();
