50fd78120e32544991de66fe7b713599a552cd0d
Renato Silva Bispo <renatoslvbispo@gmail.com>
2021-04-13 02:16:36 -0300

    Define constructor function for task item

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 const addTaskForm = document.getElementById('add-task-form');
-const taskListElement = document.getElementById('lista-tarefas');
+const taskListView = document.getElementById('lista-tarefas');
+const locale = 'pt-BR';
+const prettyDateOptions = {
+  weekday: 'short',
+  day: 'numeric',
+  month: 'short',
+};
+
+function Task() {
+  this.title = '';
+  this.addedOn = Date.now();
+  this.prettyAddedOn = new Date().toLocaleString(locale, prettyDateOptions);
+}
+
+function updateStorage(task) {
+  const retrievedTaskList = localStorage.getItem('taskList');
+
+  if (!retrievedTaskList) {
+    localStorage.setItem('taskList', JSON.stringify([task]));
+  } else {
+    const taskListArray = JSON.parse(retrievedTaskList);
+    taskListArray.push(task);
+    localStorage.setItem('taskList', JSON.stringify(taskListArray));
+  }
+}
 
 function addTask(event) {
-  const dateAdded = Date.now();
-  const prettyDateAdded = new Date().toUTCString();
-  const task = document.getElementById('texto-tarefa').value;
-  const taskObject = { task, dateAdded, prettyDateAdded };
-  let stringifiedTaskList = localStorage.taskList;
-  let taskList;
+  const task = new Task();
 
-  if (!stringifiedTaskList) {
-    taskList = [taskObject];
-  } else {
-    taskList = JSON.parse(stringifiedTaskList);
-    taskList.push(taskObject);
-  }
-  stringifiedTaskList = JSON.stringify(taskList);
-  localStorage.taskList = stringifiedTaskList;
+  task.title = document.getElementById('texto-tarefa').value;
+  updateStorage(task);
   event.preventDefault();
 }
 
