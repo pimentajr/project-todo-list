c75355e87251f840cbd8fb0f495080defbfc5e0f
Cristian Brum Oliveira <cristian.brum2@yahoo.com.br>
2021-04-12 23:56:23 -0300

    requisitos 9 e 10

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const list = document.getElementById('lista-tarefas');
 const inptText = document.querySelector('#texto-tarefa');
 const btnAdd = document.getElementById('criar-tarefa');
 const clear = document.getElementById('apaga-tudo');
+const btnFinalized = document.getElementById('remover-finalizados');
 
 function makingList() {
   const mkLi = document.createElement('li');
@@ -22,8 +23,30 @@ function liBackColor(event) {
 
 list.addEventListener('click', liBackColor);
 
-function clearAll () {
+function clearAll() {
   list.innerHTML = '';
 }
 
-clear.addEventListener('click',clearAll);
+clear.addEventListener('click', clearAll);
+
+function liDone(evento) {
+  if (evento.target.tagName === 'LI') {
+    const completed = document.querySelectorAll('li.completed');
+    if(completed) completed.className= 'completed';
+    evento.target.className= 'completed';
+  }
+}
+
+list.addEventListener('dblclick', liDone);
+
+
+ function clearFinalized () {
+const complet = document.querySelectorAll('li.completed');
+  for (i = 0; i < complet.length; i += 1) {
+    complet[i].remove();
+  }
+}
+
+
+btnFinalized.addEventListener('click', clearFinalized);
+
