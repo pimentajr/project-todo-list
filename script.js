66bd1613a62fa8c5d2bbd6a732611307b5226861
MIkaela Braga <bragamikaela@gmail.com>
2021-04-12 22:28:22 -0300

    requisito 2

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 const title = document.createElement('header');
 title.innerText = 'Minha Lista de Tarefas';
 document.body.appendChild(title);
+
+const paragrafo = document.createElement('p');
+paragrafo.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+paragrafo.id = 'funcionamento';
+document.body.appendChild(paragrafo);
