b0e632ce93b06ff72dbf62d363a57a3d0e1e54ba
anaclarabck <ackyotoku@gmail.com>
2021-04-13 16:46:33 -0300

    [ADD] Adicionado estrutura html

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+
+const title = document.getElementById('title');
+const funcionamento = document.getElementById('funcionamento');
+
+
+title.innerText = 'Minha Lista de Tarefas'
+funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
