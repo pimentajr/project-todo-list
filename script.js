42adddab962c7a12ed6d6848e2f2b6869d98862d
Guilherme Nunes <guiicnunes@gmail.com>
2021-04-12 23:58:51 -0300

    Req 13

--- script.js
+++ script.js
@@ -44,6 +44,30 @@ document.querySelector('#salvar-tarefas').addEventListener('click', () => {
   localStorage.taskList = listOfTasks.innerHTML;
 });
 
+// Font: https://developer.mozilla.org/pt-BR/docs/Web/API/Node/replaceChild
+// Font: https://developer.mozilla.org/en-US/docs/Web/API/Node/isSameNode
+// Font: https://stackoverflow.com/questions/21422337/append-element-as-sibling-after-element
+// Font: https://developer.mozilla.org/en-US/docs/Web/API/Element/insertAdjacentElement
+document.querySelector('#mover-cima').addEventListener('click', () => {
+  if (document.querySelector('.selected')) {
+    const selected = document.querySelector('.selected');
+    if (!selected.isSameNode(listOfTasks.firstChild)) {
+      const oldPosition = listOfTasks.replaceChild(selected, selected.previousElementSibling);
+      selected.insertAdjacentElement('afterend', oldPosition);
+    }
+  }
+});
+
+document.querySelector('#mover-baixo').addEventListener('click', () => {
+  if (document.querySelector('.selected')) {
+    const selected = document.querySelector('.selected');
+    if (!selected.isSameNode(listOfTasks.lastChild)) {
+      const oldPosition = listOfTasks.replaceChild(selected, selected.nextElementSibling);
+      selected.insertAdjacentElement('beforebegin', oldPosition);
+    }
+  }
+});
+
 listOfTasks.addEventListener('click', (event) => {
   if (event.target.classList.contains('task')) {
     if (document.querySelector('.selected')) {
