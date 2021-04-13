6e71c8e4dbf88169d41a09e60f73a5e4ad89499c
Matheus Gaspar <msouzagaspar@gmail.com>
2021-04-13 18:37:46 -0300

    requisito 13: adiciona botões para mover tarefas na lista

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const clearAllButton = document.getElementById('apaga-tudo');
 const clearCompletedButton = document.getElementById('remover-finalizados');
 const saveListButton = document.getElementById('salvar-tarefas');
 const removeTaskButton = document.getElementById('remover-selecionado');
+const moveUpButton = document.getElementById('mover-cima');
+const moveDownButton = document.getElementById('mover-baixo');
 
 function newTask() {
   const task = document.createElement('li');
@@ -44,6 +46,20 @@ function removeTask() {
   if (selectedTask) selectedTask.remove();
 }
 
+function moveTaskUp() {
+  const selectedTask = document.getElementsByClassName('selected')[0];
+  if (selectedTask && toDoList.firstChild !== selectedTask) {
+    toDoList.insertBefore(selectedTask, selectedTask.previousSibling);
+  }
+}
+
+function moveTaskDown() {
+  const selectedTask = document.getElementsByClassName('selected')[0];
+  if (selectedTask && toDoList.lastChild !== selectedTask) {
+    toDoList.insertBefore(selectedTask.nextSibling, selectedTask);
+  }
+}
+
 newTaskButton.addEventListener('click', newTask);
 inputElement.addEventListener('keydown', (event) => {
   if (event.key === 'Enter') newTaskButton.click();
@@ -54,5 +70,7 @@ clearAllButton.addEventListener('click', deleteAllTasks);
 clearCompletedButton.addEventListener('click', deleteCompletedTasks);
 saveListButton.addEventListener('click', saveList);
 removeTaskButton.addEventListener('click', removeTask);
+moveUpButton.addEventListener('click', moveTaskUp);
+moveDownButton.addEventListener('click', moveTaskDown);
 
 toDoList.innerHTML = localStorage.getItem('todo-list');
