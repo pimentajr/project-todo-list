683810bbb7e848a76276d9921f2dc1d6abaa0958
murilodospacos <murilopacos@gmail.com>
2021-04-13 07:37:32 -0300

    Exercicio 5 e 6 2 tentativa

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const addTask = document.getElementById("criar-tarefa");
-addTask.addEventListener("click", addNewTasks);
+window.onload = () => {
+  const addTask = document.getElementById('criar-tarefa');
 
-function addNewTasks() {
-  const listTasks = document.getElementById("lista-tarefas");
-  const textTalks  = document.getElementById("texto-tarefas");
-  let itenTasks = document.createElement('li');
-  listTasks.appendChild(itenTasks);
-}
-addNewTasks();
+  addTask.addEventListener('click', () => {
+    const listTasks = document.getElementById('lista-tarefas');
+    const textTalks = document.getElementById('texto-tarefa');
+    const itenTasks = document.createElement('li');
+    itenTasks.innerText = textTalks.value;
+    listTasks.appendChild(itenTasks);
+    textTalks.value = '';
+  });
+};
