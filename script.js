ebc7ee1f0bb71a4d4973fa74e4d23c45be032548
Ghislaine <glenox79@gmail.com>
2021-04-13 18:07:43 -0300

    Exer 07 08

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const addTask = document.querySelector('#texto-tarefa');
 const buttonTask = document.querySelector('#criar-tarefa');
+const olList = document.querySelector('#lista-tarefas');
+const buttonDellAllTasks = document.querySelector('#apaga-tudo');
 
 function addNewTask() {
   const task = addTask.value;
@@ -13,6 +15,20 @@ function addNewTask() {
 }
 buttonTask.addEventListener('click', addNewTask);
 
+// excluit todas as tarefas inseridas
+function deleteTasks() {
+  olList.innerText = '';
+}
+buttonDellAllTasks.addEventListener('click', deleteTasks);
 
-
-
+// selecionar item - alterando fundo para cinza
+function SelectedTask(event) {
+  const itemSelected = olList.childNodes;
+  for (let i = 1; i < itemSelected.length; i += 1) {
+    if (itemSelected[i].classList.contains('selected')) {
+      itemSelected[i].classList.remove('selected')
+    }
+    event.target.classList.add('selected');
+  }
+}
+olList.addEventListener('click', SelectedTask);
