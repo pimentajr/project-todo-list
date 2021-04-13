3c3fb9e5ab60110659555df84f810a7afd196f4a
Hugo Ferreira <hugonardo@gmail.com>
2021-04-12 21:07:07 -0300

    Requisitos do 1 ao 6
    1. Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
    2. Adicione abaixo do título um pequeno e discreto parágrafo com
       id="funcionamento" e com o texto "Clique duas vezes em um item para
       marcá-lo como completo"
    3. Adicione um input com o id="texto-tarefa" onde a pessoa usuária
       poderá digitar o nome do item que deseja adicionar à lista
    4. Adicione uma lista ordenada de tarefas com o id="lista-tarefas"
    5. Adicione um botão com id="criar-tarefa" e, ao clicar nesse botão, um
       novo item deverá ser criado ao final da lista e o texto do input deve
       ser limpo
    6. Ordene os itens da lista de tarefas por ordem de criação

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('xulambs');
+window.onload = () => {
+  function bindListeners() {
+    document.querySelector('#criar-tarefa').onclick = onClickAddButton;
+  }
+
+  bindListeners();
+};
+
+function onClickAddButton() {
+  const taskText = document.querySelector('#texto-tarefa');
+  if (taskText.value) {
+    addTaskToList(taskText.value);
+    taskText.value = null;
+  }
+}
+
+function addTaskToList(task) {
+  document.querySelector('#lista-tarefas')
+    .appendChild(createTaskElement(task));
+}
+
+function createTaskElement(task) {
+  const element = document.createElement('li');
+  element.innerText = task;
+  return element;
+}
