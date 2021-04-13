8d5430041eacf20903dacd51c203ab47695de5df
Nalu <aluziapantoja@gmail.com>
2021-04-13 15:42:35 -0300

    Req 7

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//5
+//5 e 6
 const criarTarefa = document.getElementById('criar-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
 const tarefa = document.getElementById('texto-tarefa')
@@ -9,3 +9,12 @@ criarTarefa.addEventListener('click', () => {
    listaTarefa.appendChild(itemLista);
    tarefa.value = '';
 })
+
+//7
+function selectedItem() {
+    listaTarefa.addEventListener('click', (event) => {
+        event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    })
+}
+
+selectedItem();
