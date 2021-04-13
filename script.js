40c3c12b5f213653ee4f6993735a79dcaa86c6be
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-12 21:43:59 -0300

    Requisito 10 realizado

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const jobList = document.getElementById('lista-tarefas');
 const taskNameInput = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
+const btnDeleteTasks = document.getElementById('apaga-tudo');
 
 function changeBackgroundColorTask(event) {
-  for (let index = 0; index < jobList.children.length; index += 1) {
-    jobList.children[index].setAttribute('id', '');
+  for (let tarefa = 0; tarefa < jobList.children.length; tarefa += 1) {
+    jobList.children[tarefa].setAttribute('id', '');
   }
   event.target.setAttribute('id', 'selectedTask');
 }
 
+
+// Faz o risco em tarefas completas, ou tira.
 function completedTask(event) {
   if (event.target.className === 'task') {
-    event.target.setAttribute('class', 'task completed');
-  } else {
-    event.target.setAttribute('class', 'task');
-  }
+    event.target.classList.toggle('completed');
+  } 
 }
 
-//Adiciona a task ao clicar no botao.
-
+// Adiciona a task ao clicar no botao.
 function addTask() {
   const taskToBeAdded = document.createElement('li');
   taskToBeAdded.innerText = taskNameInput.value;
@@ -31,3 +31,11 @@ function addTask() {
 }
 
 btnCreateTask.addEventListener('click', addTask);
+
+function deleAllTasks() {
+  while (jobList.children.length > 0) {
+    jobList.children[0].remove();
+  }
+}
+
+btnDeleteTasks.addEventListener('click', deleAllTasks);
