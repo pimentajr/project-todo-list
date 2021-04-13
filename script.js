b9ecfe996093638cbd3bdb1bdd9259e3c1c5b3f1
Ghislaine <glenox79@gmail.com>
2021-04-13 08:39:41 -0300

    Corrigindo 05

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonTask = document.querySelector('#criar-tarefa');
 
 function addNewTask() {
   const task = addTask.value;
-  const olList = document.querySelector('.lista-tarefas');
+  const olList = document.querySelector('#lista-tarefas');
   const listLiTask = document.createElement('li');
   const list = document.createTextNode(task);
   listLiTask.appendChild(list);
@@ -13,9 +13,6 @@ function addNewTask() {
 }
 buttonTask.addEventListener('click', addNewTask);
 
-function activityCompleted(event){
-  const actComplete = event.taget;
 
-}
 
 
