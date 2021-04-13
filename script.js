0f75bf31fe319565ea5a8d32a254c42ca66e49f0
Laura Gusmao <laurargus@gmail.com>
2021-04-13 19:21:45 -0300

    Adiciona botoes de deletar lista e apagar completos

--- script.js
+++ script.js
@@ -11,18 +11,18 @@ function createTask(text) {
  * Adiciona valor do input (tarefa) à lista de tarefas
  */
 function addTask() {
-  const list = document.getElementById('lista-tarefas');
+  const listItems = document.getElementById('lista-tarefas');
   const input = document.getElementById('texto-tarefa');
   if (input.value === '') {
     alert('Digite uma tarefa.');
   } else {
     const task = createTask(input.value);
-    list.appendChild(task);
+    listItems.appendChild(task);
     input.value = '';
   }
 }
 
-function addClassSelected(event) {
+function taskClassSelected(event) {
   const selected = document.querySelectorAll('.selected');
   for (let index = 0; index < selected.length; index += 1) {
     selected[index].classList.remove('selected');
@@ -30,18 +30,33 @@ function addClassSelected(event) {
   event.target.classList.add('selected');
 }
 
-function addClassCompleted(event) {
-  if (event.target.classList.contains('completed')) {
-    event.target.classList.remove('completed');
-  } else {
-    event.target.classList.add('completed');
+function taskClassCompleted(event) {
+  const task = event.target;
+  task.classList.toggle('completed');
+}
+
+function deleteList() {
+  const items = document.querySelectorAll('.task');
+  for (let index = 0; index < items.length; index += 1) {
+    items[index].remove();
+  }
+}
+
+function removeCompleted() {
+  const completed = document.querySelectorAll('.completed');
+  for (let index = 0; index < completed.length; index += 1) {
+    completed[index].remove();
   }
 }
 
 window.onload = function init() {
-  const button = document.querySelector('#criar-tarefa');
-  button.addEventListener('click', addTask);
+  const addButton = document.querySelector('#criar-tarefa');
+  addButton.addEventListener('click', addTask);
   const listItems = document.getElementById('lista-tarefas');
-  listItems.addEventListener('click', addClassSelected);
-  listItems.addEventListener('dblclick', addClassCompleted);
+  listItems.addEventListener('click', taskClassSelected);
+  listItems.addEventListener('dblclick', taskClassCompleted);
+  const deleteButton = document.querySelector('#apaga-tudo');
+  deleteButton.addEventListener('click', deleteList);
+  const removeCompletedButton = document.querySelector('#remover-finalizados');
+  removeCompletedButton.addEventListener('click', removeCompleted);
 };
