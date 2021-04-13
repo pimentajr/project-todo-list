0fad6140e9eda5e32fce57e9f1284de7b60b5dc8
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-12 21:13:33 -0300

    Requisito 8 realizado

--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const taskNameInput = document.getElementById('texto-tarefa');
 const btnCreateTask = document.getElementById('criar-tarefa');
 
 function changeBackgroundColorTask(event) {
-  event.target.style.backgroundColor = 'rgb(128,128,128)';
+  for (let index = 0; index < jobList.children.length; index += 1) {
+    jobList.children[index].setAttribute('id', ''); 
+  }
+  event.target.setAttribute('id', 'selectedTask');
 }
 
  //Adiciona a task ao clicar no botao.
