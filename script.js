2e40d0b1651f85183e71b2d49b6cf05d1fdb2382
Alexandre <naitwa.alexandre@gmail.com>
2021-04-13 18:00:11 -0300

    08

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const btnTasks = document.querySelector('#criar-tarefa');
 const ol = document.querySelector('#lista-tarefas');
 const inputText = document.querySelector('#texto-tarefa');
 
+// give selected to a testk
+function selectedTask(event) {  
+  const li = event.target;
+  const list = ol.childNodes;
+  for (let i = 0; i < list.length; i += 1) {
+    list[i].classList.remove('selected');
+  }
+  li.classList.add('selected');
+}
+
+// give completed to a task
+function completedTask(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+
 // create Lists;
 btnTasks.addEventListener('click', () => {
   const li = document.createElement('li');
@@ -12,12 +31,7 @@ btnTasks.addEventListener('click', () => {
     alert('Adicone um item');
   } else {
     li.addEventListener('click', selectedTask);
+    li.addEventListener('dblclick', completedTask);
     ol.appendChild(li);
   }
 });
-
-
-//give selected to a testk
-function selectedTask(event) {
-  console.log(event.target);
-}
