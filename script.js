b07b94f969107ea6aebd14d15793182b3c479cf5
Henrique_Pozzolini <hpmoreira05@gmail.com>
2021-04-13 18:27:42 -0300

    Separa funcoes

--- script.js
+++ script.js
@@ -1,119 +1,135 @@
-window.onload = function(){
-  const list = document.querySelector('#lista-tarefas')
-  list.innerHTML = localStorage.lista
+window.onload = function () {
+  const list = document.querySelector('#lista-tarefas');
+  list.innerHTML = localStorage.lista;
 }
 
-const inputArea = document.getElementById('texto-tarefa')
-const button = document.querySelector('#criar-tarefa')
-const list = document.querySelector('#lista-tarefas')
+const inputArea = document.getElementById('texto-tarefa');
+const button = document.querySelector('#criar-tarefa');
+const list = document.querySelector('#lista-tarefas');
 
+function onChange(event){
+  let text = event.target.value;
+  inputArea.value = text;
+}
 
-inputArea.addEventListener('change', function(event){
-  let text = event.target.value
-  inputArea.value = text
-})
+inputArea.addEventListener('change', onChange);
 
-button.addEventListener('click', function(){
+function insertItem () {;
   const newLi = document.createElement('li')
-  newLi.innerText = inputArea.value
-  list.appendChild(newLi)
-  inputArea.value = ''
-})
+  newLi.innerText = inputArea.value;
+  list.appendChild(newLi);
+  inputArea.value = '';
+}
 
-list.addEventListener('click', function(event){
-  let listItem = document.querySelectorAll('li')
-  for(let index = 0; index < listItem.length; index +=1){
-    listItem[index].removeAttribute('id')
+button.addEventListener('click', insertItem);
+
+function setColorToList (event) {
+  let listItem = document.querySelectorAll('li');
+  for(let index = 0; index < listItem.length; index +=1) {
+    listItem[index].removeAttribute('id');
   }
-  const li = event.target
-  li.setAttribute('id', 'list-color')
-})
+  const li = event.target;
+  li.setAttribute('id', 'list-color');
+}
 
-list.addEventListener('dblclick', function(event){
-  const li = event.target
-  if(li.classList.contains('completed')){
-    li.classList.remove('completed')
+list.addEventListener('click', setColorToList);
+
+function setMarkedAsDone (event) {
+  const li = event.target;
+  if(li.classList.contains('completed')) {
+    li.classList.remove('completed');
   } else{
-    li.classList.add('completed')
+    li.classList.add('completed');
   }
-})
+}
 
-const buttonErase = document.querySelector('#apaga-tudo')
+list.addEventListener('dblclick', setMarkedAsDone);
 
-buttonErase.addEventListener('click', function(){
-  let listItem = document.querySelectorAll('li')
-  for(let index =0; index < listItem.length; index+=1){
-    list.removeChild(listItem[index])
+const buttonErase = document.querySelector('#apaga-tudo');
+
+function eraseList () {
+  let listItem = document.querySelectorAll('li');
+  for(let index =0; index < listItem.length; index+=1) {
+    list.removeChild(listItem[index]);
   }
-})
+}
 
-const buttonRemoveDone = document.querySelector('#remover-finalizados')
+buttonErase.addEventListener('click', eraseList);
 
-buttonRemoveDone.addEventListener('click', function(){
-  let listItem = document.querySelectorAll('li')
-  for(let index =0; index < listItem.length; index+=1){
-    if(listItem[index].classList.contains('completed')){
-      list.removeChild(listItem[index])
+const buttonRemoveDone = document.querySelector('#remover-finalizados');
+
+function eraseCompleted () {
+  let listItem = document.querySelectorAll('li');
+  for(let index =0; index < listItem.length; index+=1) {
+    if(listItem[index].classList.contains('completed')) {
+      list.removeChild(listItem[index]);
     }
   } 
-})
+}
 
-const buttonSave = document.querySelector('#salvar-tarefas')
+buttonRemoveDone.addEventListener('click', eraseCompleted);
 
-buttonSave.addEventListener('click', function(){
-  localStorage.setItem('lista', list.innerHTML)
-})
+const buttonSave = document.querySelector('#salvar-tarefas');
 
-const buttonRemoveSelected = document.querySelector('#remover-selecionado')
+function saveList () {
+  localStorage.setItem('lista', list.innerHTML);
+}
 
-buttonRemoveSelected.addEventListener('click', function(){
-  let listItem = document.querySelectorAll('li')
-  for(let index =0; index < listItem.length; index+=1){
+buttonSave.addEventListener('click', saveList);
+
+const buttonRemoveSelected = document.querySelector('#remover-selecionado');
+
+function eraseSelected () {
+  let listItem = document.querySelectorAll('li');
+  for(let index =0; index < listItem.length; index+=1) {
     if(listItem[index].id === 'list-color'){
-      list.removeChild(listItem[index])
+      list.removeChild(listItem[index]);
     }
   }
-})
+}
 
-const buttonUp = document.querySelector('#mover-cima')
+buttonRemoveSelected.addEventListener('click', eraseSelected);
 
-buttonUp.addEventListener('click', function(){
-  const li = document.querySelectorAll('li')
-  for(let index = 0; index < li.length; index += 1){
-    if(li[index].id === 'list-color' && index !==0){
-      let element = li[index].innerHTML
-      let element2 = li[index -1].innerHTML
-      let elementClass = li[index].className
-      let element2Class = li[index -1].className
+const buttonUp = document.querySelector('#mover-cima');
+
+function moveUp () {
+  const li = document.querySelectorAll('li');
+  for(let index = 0; index < li.length; index += 1) {
+    if(li[index].id === 'list-color' && index !==0) {
+      let element = li[index].innerHTML;
+      let element2 = li[index -1].innerHTML;
+      let elementClass = li[index].className;
+      let element2Class = li[index -1].className;
       
-      li[index].innerHTML = element2
-      li[index - 1].innerHTML = element
-      li[index].removeAttribute('id', 'list-color')
-      li[index -1].setAttribute('id', 'list-color')
-      li[index].className = element2Class
-      li[index - 1].className = elementClass
+      li[index].innerHTML = element2;
+      li[index - 1].innerHTML = element;
+      li[index].removeAttribute('id', 'list-color');
+      li[index -1].setAttribute('id', 'list-color');
+      li[index].className = element2Class;
+      li[index - 1].className = elementClass;
     }
   }
-})
+}
 
-const buttonDown = document.querySelector('#mover-baixo')
+buttonUp.addEventListener('click', moveUp)
 
-buttonDown.addEventListener('click', function(){
-  const selected = document.getElementById('list-color')
-  const li = document.querySelectorAll('li')
+const buttonDown = document.querySelector('#mover-baixo');
+
+function moveDown () {
+  const selected = document.getElementById('list-color');
+  const li = document.querySelectorAll('li');
   if(selected !== li[li.length-1] && selected){
-    const selectedInner = selected.innerText
-  const selectedClass = selected.className
-  const nextElClass = selected.nextElementSibling.className
+    const selectedInner = selected.innerText;
+  const selectedClass = selected.className;
+  const nextElClass = selected.nextElementSibling.className;
 
-  selected.innerText = selected.nextElementSibling.innerHTML
-  selected.className = nextElClass
-  selected.nextElementSibling.innerHTML = selectedInner
-  selected.nextElementSibling.className = selectedClass
-  selected.nextElementSibling.setAttribute('id', 'list-color')
-  selected.removeAttribute('id')
+  selected.innerText = selected.nextElementSibling.innerHTML;
+  selected.className = nextElClass;
+  selected.nextElementSibling.innerHTML = selectedInner;
+  selected.nextElementSibling.className = selectedClass;
+  selected.nextElementSibling.setAttribute('id', 'list-color');
+  selected.removeAttribute('id');
     }
   }
-)
 
-
+buttonDown.addEventListener('click', moveDown);
