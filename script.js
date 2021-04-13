ecec2755c7073a2e2527cddc05ed09dac9bde156
Wilquer82 <wilquer.fig@gmail.com>
2021-04-13 19:59:45 -0300

    13-04 19:59

---
+++ script.js
@@ -0,0 +1,13 @@
+function addItem() {
+    let newTask = document.getElementById("texto-tarefa").value;
+    let list  = document.getElementById("lista-tarefas").innerHTML;
+    list = list + "<li>" + newTask + "</li>";
+    
+    document.getElementById("lista-tarefas").innerHTML = list;
+    document.getElementById("texto-tarefa").value = " ";
+}
+function Start() {
+    const button = document.querySelector('#criar-tarefa');
+    button.addEventListener('click', addItem);    
+}
+  window.onload = Start;
