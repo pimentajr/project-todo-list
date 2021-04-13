fc233e148770532f00aa51eb4a220de4f353e0dd
Gabriela Feijo <feijo.gabriela@gmail.com>
2021-04-13 11:48:10 -0300

    Adiciona requisitos iniciais ao HTML, simplificando o JS

--- script.js
+++ script.js
@@ -1,26 +1,3 @@
-const header = document.createElement('header');
-header.innerHTML = '<strong>Minha Lista de Tarefas</strong>';
-document.body.appendChild(header);
-
-const paragraph = document.createElement('p');
-paragraph.id = 'funcionamento';
-paragraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-document.body.appendChild(paragraph);
-
-const input = document.createElement('input');
-input.id = 'texto-tarefa';
-input.type = 'text';
-document.body.appendChild(input);
-
-const lista = document.createElement('ol');
-lista.id = 'lista-tarefas';
-document.body.appendChild(lista);
-
-const button = document.createElement('button');
-button.id = 'criar-tarefa';
-button.innerHTML = 'To Do';
-document.body.appendChild(button);
-
 const selectList = document.querySelector('#lista-tarefas');
 const selectButton = document.querySelector('#criar-tarefa');
 const selectInput = document.getElementById('texto-tarefa');
