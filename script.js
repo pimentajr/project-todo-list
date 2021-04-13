db91e6422dde073d1a4f5d06af5caeefd36abdee
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-04-12 22:08:53 -0300

    Requisito 9 finalizado

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Adiciona itens à lista
-// Para adição de enter na lista, foi consultada a documentação em W3Schools sobre ações usando o "Enter"
-// Source: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 const input = document.getElementById('texto-tarefa');
 const addButton = document.getElementById('criar-tarefa');
 const list = document.getElementById('lista-tarefas');
 const itemsOfList = document.getElementsByTagName('li');
 
+// Adiciona itens à lista
+// Para adição de enter na lista, foi consultada a documentação em W3Schools sobre ações usando o "Enter"
+// Source: https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
 function addToList() {
   addButton.addEventListener('click', () => {
     const li = document.createElement('li');
@@ -25,7 +25,7 @@ function addToList() {
 }
 addToList();
 
-// Muda cor de fundo ao clicar em um item da lista, mas somente um fica selecionado (requisito 9).
+// Muda cor de fundo ao clicar em um item da lista, mas somente um fica selecionado (requisito 8).
 function changeBackgroundColor() {
   list.addEventListener('click', (event) => {
     for (let index = 0; index < itemsOfList.length; index += 1) {
@@ -35,3 +35,15 @@ function changeBackgroundColor() {
   });
 }
 changeBackgroundColor();
+
+// Risca o texto
+function addSerrated() {
+  list.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  });
+}
+addSerrated();
