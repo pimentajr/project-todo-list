2b6eda4b0f012e1bbaf6466601eaa69c9f305926
Savio Moraes <smmsavio@hotmail.com>
2021-04-12 22:48:43 -0300

    Adiciona correcoes do eslint e do stylelint

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 function insertTitle() {
-  const body = document.body;
   const title = document.createElement('header');
   title.id = 'title';
-  body.appendChild(title);
+  document.body.appendChild(title);
   const titleText = document.createElement('h1');
   titleText.id = 'title-text';
   titleText.textContent = 'Minha Lista de Tarefas';
