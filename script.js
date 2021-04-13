1eb52ebd561f3883bce3cb0cc0f1cbc173c2173d
josanjohnata <josanjohnata@gmail.com>
2021-04-12 23:01:56 -0300

    sixth & seventh step, sort list & add completed task marker

--- script.js
+++ script.js
@@ -9,3 +9,19 @@ function newListItem() {
     }
     document.getElementById('texto-tarefa').value = '';
 }
+let toDoList = document.getElementById('lista-tarefas');
+toDoList.addEventListener('click', (event) => {
+  if (event.target.tagName === 'LI') {
+    const countLine = document.querySelector('.selected');
+    if (countLine != null) {
+      countLine.classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
+});
+
+toDoList.addEventListener('dblclick', (event) => {
+  if (event.target.tagName === 'LI') {
+    event.target.classList.toggle('completed');
+  }
+});
