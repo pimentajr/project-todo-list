d87735acb4b33c714854fc514d08008b08e911e1
anaclarabck <ackyotoku@gmail.com>
2021-04-13 17:27:34 -0300

    [ADD] Funçao para adicionar lista de tarefas ordenados por ordem de criaçao

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
-
-
-
 const title = document.getElementById('title');
 const funcionamento = document.getElementById('funcionamento');
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+const inputTextoTarefa = document.getElementById('texto-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
 
+title.innerText = 'Minha Lista de Tarefas';
+funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+buttonCriarTarefa.innerText = 'Adicionar';
 
-title.innerText = 'Minha Lista de Tarefas'
-funcionamento.innerText = 'Clique duas vezes em um item para marcá-lo como completo'
+function adicionarTarefa() {
+  var itemListaTarefa = document.createElement('li');
+
+  itemListaTarefa.classList.add('tarefa');
+  listaTarefas.appendChild(itemListaTarefa);
+  itemListaTarefa.innerText = inputTextoTarefa.value;
+  inputTextoTarefa.value = '';
+}
+
+buttonCriarTarefa.addEventListener('click', adicionarTarefa);
