fb8f69c5cea80ef0a1d2f2bfe6e8abe81a510017
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-13 20:44:45 -0300

    Add option to reset task field

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const inputField = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const addButton = document.querySelector('#criar-tarefa');
+const taskForm = document.querySelector('#input-task-container');
+
+function clearTask() {
+  listItem.innerText = '';
+}
 
 function addTask() {
   const task = inputField.value;
@@ -8,6 +13,7 @@ function addTask() {
 
   listItem.innerText = task;
   taskList.appendChild(listItem);
+  taskForm.reset();
 }
 
 addButton.addEventListener('click', addTask);
