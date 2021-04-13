2967a87b2733751975f894f05264dd252ca57de5
Antonio Malato <malatoneto@gmail.com>
2021-04-13 15:55:32 -0300

    Refatorando codigo

--- script.js
+++ script.js
@@ -1,19 +1,37 @@
-const button = document.getElementById('criar-tarefa');
+const body = document.getElementById('body');
+const header = document.createElement('header');
+header.id = 'header';
+header.innerText = 'Minha Lista de Tarefas';
+body.appendChild(header);
 
-const orderedList = document.getElementById('lista-tarefas');
+const p = document.createElement('p');
+p.id = 'funcionamento';
+p.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+body.appendChild(p);
 
-const createItemList = document.createElement('li');
-createItemList.id = 'item-list';
-orderedList.appendChild(createItemList)
+const input = document.createElement('input');
+input.id = 'texto-tarefa';
+input.type = 'text';
+body.appendChild(input);
 
-createItemList.appendChild(value);
+const btn = document.createElement('button');
+btn.id = 'criar-tarefa';
+btn.innerText = 'Adicionar';
+btn.type = 'submit';
+body.appendChild(btn);
 
-const input = document.getElementById('texto-tarefa');
+const list = document.createElement('ol');
+list.id = 'lista-tarefas';
+body.appendChild(list);
 
 function getText() {
-    let valueItem = input.value;
+    const itemList = document.createElement('li');
+    itemList.className = 'item-list';
 
-    console.log(valueItem)
+    let getInput = input.value;
+    itemList.innerHTML = getInput;
+    list.appendChild(itemList);
+    input.value = '';
 }
 
-button.addEventListener('click', getText);
+btn.addEventListener('click', getText);
