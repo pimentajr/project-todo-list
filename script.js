d3d57f20f695ce4313be0c681f1d6485b84e052f
Alexandre <naitwa.alexandre@gmail.com>
2021-04-13 12:15:39 -0300

    verificacao de nota

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+// cria lista de elementos;
 const btnTasks = document.querySelector('#criar-tarefa');
 const listOfTasks = document.querySelector('#lista-tarefas');
 const inputText = document.querySelector('#texto-tarefa');
-
 btnTasks.addEventListener('click', () => {
   const li = document.createElement('li');
+  li.className = 'lista';
   li.innerHTML = inputText.value;
   inputText.value = '';
-  listOfTasks.appendChild(li);
-  console.log(inputText.value);
+  if (li.innerHTML === '') {
+    alert('Adicone um item');
+  } else {
+    listOfTasks.appendChild(li);
+  }
 });
+
+
+
+
+
