7f28fc119334a4c7a45cb48700cae341cdbb8c2a
DADA <davimarque2000@gmail.com>
2021-04-13 16:20:36 -0300

    adiciona botao que apaga yasks completadas

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const addButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const clearButton = document.querySelector('#apaga-tudo');
-
+const completedClear = document.querySelector('#remover-finalizados')
 addButton.addEventListener('click', () => {
   const inputText = document.querySelector('#texto-tarefa').value;
   const listElement = document.createElement('li');
@@ -10,26 +10,29 @@ addButton.addEventListener('click', () => {
   taskList.appendChild(listElement);
   document.querySelector('#texto-tarefa').value = '';
 });
-const tasksList = document.querySelector('#lista-tarefas');
-tasksList.addEventListener('click', (event) => {
+taskList.addEventListener('click', (event) => {
   event.target.classList.add('selectedTask');
   const selected = document.querySelectorAll('.selectedTask');
-  console.log(selected);
   for (let index = 0; index < selected.length; index += 1) {
     selected[index].classList.remove('selectedTask');
   }
   event.target.classList.add('selectedTask');
 });
-tasksList.addEventListener('dblclick', (event) => {
+taskList.addEventListener('dblclick', (event) => {
   if (event.target.classList.contains('completed')) {
     event.target.classList.remove('completed');
   } else {
     event.target.classList.add('completed');
   }
 });
-
 clearButton.addEventListener('click', () => {
   while (taskList.firstChild) {
     taskList.removeChild(taskList.lastChild);
+  }
+});
+completedClear.addEventListener('click', () => {
+  const completedTasks = document.querySelectorAll('.completed');
+  for (let index = 0; index < completedTasks.length; index += 1) {
+    completedTasks[index].remove();
   }
 });
