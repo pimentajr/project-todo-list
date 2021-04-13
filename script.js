fff9f603d43bb883285024056b0f476b33e8fdb8
Antonio Malato <malatoneto@gmail.com>
2021-04-13 11:16:16 -0300

    Correçao

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const boxList = document.getElementById('box-list');
 const createList = document.createElement('ol');
-createList.id = 'lista-tarefas'
+createList.id = 'lista-tarefas';
 boxList.appendChild(createList);
