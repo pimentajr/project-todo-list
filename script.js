a9f233f93e77c992e665a01fe034f87000ea5c26
IgorMendez <igor.emp@hotmail.com>
2021-04-13 12:52:42 -0300

    Exercício 6, 7 e 8

--- script.js
+++ script.js
@@ -3,14 +3,31 @@ const textoTarefa = document.getElementById('texto-tarefa')
 const listTarefas = document.getElementById('lista-tarefas')
 
 function getText() {
+  const criaLista = document.createElement('li')
+  listTarefas.appendChild(criaLista)
   if(textoTarefa.value !== '') {
-    const criaLista = document.createElement('li')
-    listTarefas.appendChild(criaLista)
-    criaLista.innerText += textoTarefa.value
+    listTarefas.lastChild.innerText = textoTarefa.value
     textoTarefa.value = ''
+  } else {
+    listTarefas.lastChild.remove()
   }
 }
 
+function removeColor(e) {
+  for (let index = 0; index < listTarefas.childNodes.length; index++) {
+    listTarefas.childNodes[index].classList.remove('back-ground')
+    
+  }
+}
+
+function setListColor(e) {
+  removeColor()
+  if(e.target.innerText) {
+    e.target.classList.add('back-ground')
+  }
+}
+
+listTarefas.addEventListener('click', setListColor)
 criarTarefa.addEventListener('click', getText)
 
 // console.log(criarTarefa)
