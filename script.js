6490effc6847159c2cc957f98e43519bca22d3ef
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-04-13 21:06:22 -0300

    Realizacao do desafio 8

--- script.js
+++ script.js
@@ -11,17 +11,29 @@ const createOl = document.createElement('ol');
 createOl.id = 'lista-tarefas';
 newDiv.appendChild(createOl);
 
-// Desafio 5
+// Desafio 5 e 6: para realizacao do desafio, tive auxilio dos colegas de turma Julio Filizzola e Rodolfo Ramos;
 function createTask() {
   const inputItem = document.getElementById('texto-tarefa');
 
-  addButton.addEventListener('click', (event) => {
+  addButton.addEventListener('click', () => {
     const newList = document.createElement('li');
     createOl.appendChild(newList);
     newList.innerHTML = inputItem.value;
-    inputItem.value = ''
-    console.log(newList)
-  })
+    inputItem.value = '';
+  });
 }
 
 createTask();
+
+function changeColor(event) {
+  const getLi = document.querySelectorAll('li');
+  const targetElement = event.target;
+
+  for (let index = 0; index < getLi.length; index += 1) {
+    getLi[index].classList.remove('selected');
+  }
+  createOl.classList.remove('selected');
+  targetElement.classList.add('selected');
+}
+
+createOl.addEventListener('click', changeColor);
