647fecf9ef639c7edcaa13ece4d25fe5a731f9ab
Flavio_Cordeiro <flavio_ribeirorc@hotmail.com>
2021-04-14 06:03:02 -0300

    Projeto To Do List - v2

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
+//Declaração e link com document HTML
 const addTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const taskText = document.getElementById('texto-tarefa');
 const eraseAll = document.getElementById('apaga-tudo');
 const eraseCompleted = document.getElementById('remover-finalizados');
+const completed = document.getElementsByClassName('completed');
 
-
+// Adicionando Tarefas
 addTask.addEventListener('click', () => {
-  const task = document.createElement('li');
-  task.innerText = taskText.value;
-  taskList.appendChild(task);
-  taskText.value = '';
-})
+    const task = document.createElement('li');
+    task.innerText = taskText.value;
+    taskList.appendChild(task);
+    taskText.value = '';
+});
 
+//Selecionando uma tarefa da Lista
 function selectTask() {
   taskList.addEventListener('click', (event) => {
     const eTarget = event.target;
     const selectedTask = document.querySelector('.selected');
     if (selectedTask) {
       selectedTask.classList.remove('selected');
-    } else {
-        eTarget.classList.add('selected');
-    }
-  })
+    } 
+    eTarget.classList.add('selected');
+  });
 }
 
-selectTask();
-
+// Marcando uma tarefa da lista como feita
 function completedTask() {
   taskList.addEventListener('dblclick', (event) => {
   const etcList = event.target.classList;
@@ -34,26 +35,26 @@ function completedTask() {
   } else {
     etcList.add('completed');
     }
-  })
+  });
 }
 
-completedTask();
-
+// Apagando todas as tarefas da Lista
 function eraseTasks() {
-    eraseAll.addEventListener('click', () => {
-        taskList.innerHTML = '';
-    })
+  eraseAll.addEventListener('click', () => {
+    taskList.innerHTML = '';
+  });
 }
 
-eraseTasks();
-
-const completed = document.getElementsByClassName('completed');
+// Apagando somente as tarefas feitas
 function erasedoneTasks() {
   eraseCompleted.addEventListener('click', () => {
-    for (let index = completed.length - 1; index >= 0; index -=1) {
+    for (let index = completed.length - 1; index >= 0; index -= 1) {
       completed[index].remove();
     }
-  })
+  });
 }
 
+selectTask();
+completedTask();
+eraseTasks();
 erasedoneTasks();
