c427125d8ffc1379a65588a84656b770561617a1
Raphael Pereira Silva <raphaelprsilva@gmail.com>
2021-04-14 11:27:33 -0300

    Add comments about new functions created

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 // To set a new event listener, I've used the example at link below
 // https://www.codegrepper.com/code-examples/javascript/Adding+an+event+listener+to+an+element+that+doesn%27t+exist+yet
+// To separetaly create functions changeBackgroundColor and strikeListItem, I've used that logics
+// https://dev.to/akhil_001/adding-event-listeners-to-the-future-dom-elements-using-event-bubbling-3cp1
 
 const inputField = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const addButton = document.querySelector('#criar-tarefa');
 const taskForm = document.querySelector('#input-task-container');
+const rootElement = document.querySelector('body');
 
 function addTaskAndChangeBackgroundColor() {
   const task = inputField.value;
@@ -14,32 +17,8 @@ function addTaskAndChangeBackgroundColor() {
   listItem.innerText = task;
   taskList.appendChild(listItem);
 
-  // Add an event listener to new li element created
-  // if (listItem) {
-  //   listItem.addEventListener('click', (event) => {
-  //     // Set new background onclick
-  //     const taskListChildren = taskList.children;
-  //     const background = 'rgb(128, 128, 128)';
-  //     const clickedItem = event.target;
-  //     // Set option to select a unique item with new background color
-  //     for (let index = 0; index < taskListChildren.length; index += 1) {
-  //       taskListChildren[index].style.backgroundColor = '';
-  //     }
-  //     clickedItem.style.backgroundColor = background;
-  //   });
-
-  //   listItem.addEventListener('dblclick', (event) => {
-  //     const clickedItem = event.target;
-
-  //     clickedItem.classList.toggle('completed');
-  //   });
-  // } else {
-  //   console.log(Error('fail'));
-  // }
   taskForm.reset();
 }
-
-const rootElement = document.querySelector('body');
 
 function changeBackgroundColor(event) {
   const clickedItem = event.target;
@@ -62,7 +41,6 @@ function strikeListItem(event) {
     clickedItem.classList.toggle('completed');
   }
 }
-
 
 rootElement.addEventListener('click', changeBackgroundColor);
 rootElement.addEventListener('dblclick', strikeListItem);
