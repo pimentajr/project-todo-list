7e7945469485f2321db3fcfb54b3dfb66ad4a8e3
Ruben Lopes <rubenssouss@hotmail.com>
2021-04-14 10:52:22 -0300

    Add changing-bccolor functionality event on ol's items

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
 let addButton = document.querySelector('#criar-tarefa');
 
+let olTaskList = document.querySelector('#lista-tarefas');
+
 let input1 = document.querySelector('#texto-tarefa');
 
 addButton.addEventListener('click', function () {
-  let inputValue = document.querySelector('#texto-tarefa').value;
-  let olTaskList = document.querySelector('#lista-tarefas');
+  let inputValue = input1.value;
   let newTask = document.createElement('li');
   newTask.innerHTML = inputValue;
+  newTask.className = 'task';
   olTaskList.appendChild(newTask);
-  document.querySelector('#texto-tarefa').value = '';
+  input1.value = '';
 })
+
+let tasks = document.querySelector('ol');
+
+function selectTask(event) {
+  for (let index = 0; index < tasks.children.length; index += 1) {
+      tasks.children[index].style.backgroundColor = '';
+    }
+  event.target.style.backgroundColor = 'rgb(128,128,128)';
+}
+tasks.addEventListener('click', selectTask);
+
+
