25cdd8674def02abb6e27712d9c357e772ccb93b
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-14 03:45:01 -0300

    Adicionadno requisitos de 5-1

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const textoTarefa = document.getElementById('texto-tarefa');
 
 function gerar() {
-  const lista = document.getElementById('lista-tarefa');
+  const lista = document.querySelector('ol#lista-tarefas');
   const item = document.createElement('li');
   item.appendChild(document.createTextNode(textoTarefa.value));
   lista.appendChild(item);
