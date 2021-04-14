4b32293864ee1f98e24a76f0e3433015a4aadac2
Tarcisio-Menezes <tarcisioquim@gmail.com>
2021-04-14 12:14:20 -0300

    refatorando - a saga...

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ const upButton = document.getElementById('mover-cima');
 
 function up() {
   const lisSelected = document.getElementsByClassName('selected')[0];
-  if (lisSelected !== null && lisSelected.previousSibling !== null) {
+  if (lisSelected !== undefined && lisSelected.previousElementSibling !== null) {
     taskList.insertBefore(lisSelected, lisSelected.previousElementSibling);
   }
 }
@@ -80,7 +80,7 @@ const downButtonm = document.getElementById('mover-baixo');
 
 function down() {
   const lisSelected = document.getElementsByClassName('selected')[0];
-  if (lisSelected !== null && lisSelected.nextSibling !== null) {
+  if (lisSelected !== undefined && lisSelected.nextElementSibling !== null) {
     taskList.insertBefore(lisSelected.nextElementSibling, lisSelected);
   }
 }
