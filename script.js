d32323243461dbd7f1961007d42d0fba3f534ea0
Maran <maranmorimoto@gmail.com>
2021-04-13 23:08:18 -0300

    item 12, done

--- script.js
+++ script.js
@@ -1,48 +1,57 @@
 // http://www.learningaboutelectronics.com/Articles/How-to-retrieve-data-from-a-text-box-in-javascript.php#:~:text=To%20extract%20the%20information%20which,entered%20into%20this%20text%20box.
-function addToList () {
-  var task = document.querySelector('#texto-tarefa');
-  var addTask = task.value;
-  var orderList = document.getElementById('lista-tarefas');
-  var node = document.createElement('li')
+function addToList() {
+  const task = document.querySelector('#texto-tarefa');
+  const addTask = task.value;
+  const orderList = document.getElementById('lista-tarefas');
+  const node = document.createElement('li');
   node.innerHTML = addTask;
   orderList.appendChild(node);
   task.value = '';
 }
 
-const findList = document.querySelector('#lista-tarefas')
+const findList = document.querySelector('#lista-tarefas');
 const countLines = document.getElementsByTagName('li');
 
 // https://stackoverflow.com/questions/8739665/is-background-colornone-valid-css
 
-function colorLine (event) {
-  for (index = 0; index < countLines.length; index += 1) {
+function colorLine(event) {
+  for (let index = 0; index < countLines.length; index += 1) {
     countLines[index].style.backgroundColor = 'transparent';
   }
-    event.target.style.backgroundColor = 'grey';
+  event.target.style.backgroundColor = 'grey';
 }
 findList.addEventListener('click', colorLine);
 
-findList.addEventListener('dblclick', completedTask);
-
-function completedTask (event) {
+function completedTask(event) {
   event.target.classList.toggle('completed');
 }
 
 findList.addEventListener('dblclick', completedTask);
 
 // https://www.w3schools.com/jsref/met_node_removechild.asp
-function deleteAll () {
+function deleteAll() {
   while (countLines.length > 0) {
-  findList.removeChild(findList.childNodes[0]);
+    findList.removeChild(findList.childNodes[0]);
   }
+  localStorage.clear();
 }
 
-function deleteDoneTask () {
+function deleteDoneTask() {
   const deleteDoneTask = document.getElementsByClassName('completed');
   while (deleteDoneTask.length > 0) {
-   findList.removeChild(deleteDoneTask[0]);
+    findList.removeChild(deleteDoneTask[0]);
   }
 }
+// https://www.w3schools.com/jsref/prop_win_localstorage.asp
+// https://developer.mozilla.org/en-US/docs/Web/API/GlobalEventHandlers/onload
+function saveTasks() {
+  localStorage.setItem('tasks', findList.innerHTML);
+}
+
+window.onload = function () {
+  const savedList = localStorage.getItem('tasks');
+  findList.innerHTML = savedList;
+};
 
 // function completedTask (event) {
 //   const trace = event.target;
