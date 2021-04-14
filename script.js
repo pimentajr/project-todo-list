db0db93dc08ee1b95b83c7d9922a63332849f05d
Bernardo Coelho <bernardocezario21@gmail.com>
2021-04-14 20:32:00 -0300

    Complete exercise 10

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const createTaskButton = document.getElementById('criar-tarefa');
 const listTask = document.getElementById('lista-tarefas');
 const textTask = document.getElementById('texto-tarefa');
+const eraseTasksButton = document.getElementById('apaga-tudo');
 
 createTaskButton.addEventListener ('click', function addTask() {
   const tagP = document.createElement('li');
@@ -9,9 +10,9 @@ createTaskButton.addEventListener ('click', function addTask() {
   textTask.value = '';
   tagP.addEventListener('click', function addClass() {
     tagP.classList.add('tasks-background-color');
-    tagP.addEventListener('click', function rmvClass() {
-      tagP.classList.remove('tasks-background-color');
-    });
+  });
+  eraseTasksButton.addEventListener('click', function rmvTasks() {
+    listTask.removeChild(tagP);
   });
   tagP.addEventListener('dblclick', function addRisk() {
     tagP.classList.add('completed');
@@ -20,3 +21,12 @@ createTaskButton.addEventListener ('click', function addTask() {
     });
   });
 });
+
+//eraseTasksButton.addEventListener ('click', function rmvTask() {
+//  const rmvtagP = document.removeEventListener
+//  tagP.innerText = textTask.value;
+//  listTask.appendChild(tagP);
+//  textTask.value = '';
+//  tagP.addEventListener('click', function addClass() {
+//    tagP.classList.add('tasks-background-color');
+//  });
