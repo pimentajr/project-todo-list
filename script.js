b0503e28bfec525f09082729d66ed5c623aba614
Bernardo Coelho <bernardocezario21@gmail.com>
2021-04-14 17:36:35 -0300

    Complete exercise 7

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const createTaskButton = document.getElementById('criar-tarefa');
 const listTask = document.getElementById('lista-tarefas');
 const textTask = document.getElementById('texto-tarefa');
 
-createTaskButton.addEventListener('click', function() {
-  let tagP = document.createElement('li');
+createTaskButton.addEventListener ('click', function addTask() {
+  const tagP = document.createElement('li');
   tagP.innerText = textTask.value;
   listTask.appendChild(tagP);
   textTask.value = '';
+  tagP.addEventListener('click', function backgroundColor() {
+    tagP.style.backgroundColor = 'rgb(128,128,128)';
+  });
 });
