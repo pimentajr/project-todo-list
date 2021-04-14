3816bf0580da5aa8dea944c6b2fb438cc8ebdfe5
nandovbr <nandovbr15@hotmail.com>
2021-04-13 21:30:14 -0300

    requisito 5

---
+++ script.js
@@ -0,0 +1,10 @@
+const textoTarefa = document.getElementById('texto-tarefa');
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+const listaDeTarefas = document.getElementById('lista-tarefas');
+
+buttonCriarTarefa.addEventListener('click', () => {
+  const novaTarefa = document.createElement('li');
+  novaTarefa.innerText = textoTarefa.value;
+  listaDeTarefas.appendChild(novaTarefa);
+  textoTarefa.value = '';
+})
