d1933babb431b122a20b299fcf0c77c24fdb9921
Genivaldo <cgenivaldo46@gmail.com>
2021-04-15 16:21:29 -0300

    Exercicio 04

---
+++ script.js
@@ -0,0 +1,9 @@
+const createTask = document.getElementById('criar-tarefa');
+createTask.addEventListener('click', () => {
+  const task = document.getElementById('texto-tarefa');
+  const tasklist = document.getElementById('lista-tarefas');
+  const itemTask = document.createElement('li');
+  itemTask.innerText = task.value;
+  tasklist.appendChild(itemTask);
+  task.value = '';
+});
