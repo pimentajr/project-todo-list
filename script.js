f28ddd17e7c7e230d3209d57bf19c30fe6f7ce9a
Michel Pereira <michelbecauro@gmail.com>
2021-04-15 00:30:33 -0300

    JS: Parte do código

---
+++ script.js
@@ -0,0 +1,19 @@
+let ol = document.getElementById('lista-tarefas');
+
+
+// function start () {
+//     criar();
+   
+// }
+
+
+
+function criar () {
+    let bt = document.getElementById('criar-tarefa');
+    bt.addEventListener('click', function (){
+
+    })
+}
+
+window.onload = start();
+
