710fae02e69eaf102f3b97210cf26ebdd18d3524
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-15 19:34:44 -0300

    Ajustes Lint

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const createTask = document.getElementById('criar-tarefa');
 const listTask = document.getElementById('lista-tarefas');
 const textTask = document.getElementById('texto-tarefa');
-// const createListItem = document.createElement('li');
 
 //Requisitos 05 e 06
 createTask.addEventListener('click', () => {
@@ -15,23 +14,22 @@ createTask.addEventListener('click', () => {
 const OL = document.getElementById('lista-tarefas');
 OL.addEventListener('click', (event) => {
   const selectedLI = document.getElementsByClassName('selected');
-  if (selectedLI.length !== 0){
-    console.log(selectedLI);
+  if (selectedLI.length !== 0) {
     selectedLI[0].classList.remove('selected');
     }
     event.target.classList.add('selected');
-  });
+});
 
-//Requisito 10
-function deleteSelectedItem () {
+//Requisito 09
+function deleteSelectedItem() {
   listTask.addEventListener('dblclick', (event) => {
     const eventTarget = event.target;
     console.log(eventTarget);
     const riskedItem = document.querySelectorAll('.completed');
-    if (eventTarget.classList.contains('completed')) {
-      eventTarget.classList.remove('completed');
+    if (riskedItem.length !== 0) {
+      riskedItem[0].classList.remove('completed');
     }
       eventTarget.classList.add('completed');
-  })
+  });
 }
 deleteSelectedItem();
