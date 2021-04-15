53570a5171f9eae9a64591d3072ad8b8de819bac
Ruben Lopes <rubenssouss@hotmail.com>
2021-04-15 07:39:13 -0300

    Linter corrections 1

--- script.js
+++ script.js
@@ -1,47 +1,48 @@
-let addButton = document.querySelector('#criar-tarefa');
-let delButton = document.querySelector('#apaga-tudo');
-let removeCheckButton = document.querySelector('#remover-finalizados');
+const addButton = document.querySelector('#criar-tarefa');
+const delButton = document.querySelector('#apaga-tudo');
+const removeCheckButton = document.querySelector('#remover-finalizados');
 
-let olTaskList = document.querySelector('#lista-tarefas');
+const olTaskList = document.querySelector('#lista-tarefas');
 
-let input1 = document.querySelector('#texto-tarefa');
+const input1 = document.querySelector('#texto-tarefa');
 
 addButton.addEventListener('click', function () {
-  let inputValue = input1.value;
-  let newTask = document.createElement('li');
+  const inputValue = input1.value;
+  const newTask = document.createElement('li');
   newTask.innerHTML = inputValue;
   newTask.className = 'task';
   olTaskList.appendChild(newTask);
   input1.value = '';
-})
+});
 
-let tasks = document.querySelector('ol');
+const tasks = document.querySelector('ol');
 
 function selectTask(event) {
   for (let index = 0; index < tasks.children.length; index += 1) {
-      tasks.children[index].style.backgroundColor = '';
-    }
+    tasks.children[index].style.backgroundColor = '';
+  }
   event.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 tasks.addEventListener('click', selectTask);
 
 let click = 0;
 function completed(event) {
-  if (click == 0) {
+  if (click === 0) {
     event.target.classList.add('completed');
     click = 1;
   } else {
     event.target.classList.remove('completed');
     click = 0;
   }
-
 }
+
 tasks.addEventListener('dblclick', completed);
 
 function deleteAll() {
   tasks.innerHTML = '';
 }
-delButton.addEventListener('click', deleteAll)
+
+delButton.addEventListener('click', deleteAll);
 
 //let complete = document.querySelectorAll('.completed');
 function removeChecked() {
@@ -49,7 +50,7 @@ function removeChecked() {
     if (tasks.children[index].classList.contains('completed')) {
       tasks.removeChild(tasks.children[index]);
       index = 0;
-    };
+    }
   }
 }
 
