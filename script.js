10c103102bfce58c0b3caf6ee7605e1e587c85f3
Matheus Araújo <matheusaraujo.dev@gmail.com>
2021-04-15 19:07:42 -0300

    Adiciona requisito 08

--- script.js
+++ script.js
@@ -11,8 +11,31 @@ createTask.addEventListener('click', () => {
   textTask.value = '';
 });
 
-//Requisito 07
-const getOL = document.getElementById('lista-tarefas');
-getOL.addEventListener('click', (event) => {
-  event.target.style.backgroundColor = 'rgb(128, 128, 128';
-});
+//Requisito 07 e 08 
+const OL = document.getElementById('lista-tarefas');
+OL.addEventListener('click', (event) => {
+  const selectedLI = document.getElementsByClassName('selected');
+  if (selectedLI.length !== 0){
+    console.log(selectedLI);
+    selectedLI[0].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+
+
+  //Requisito 08 teste
+  // const getLI = getOL.appendChild;
+  // getLI.addEventListener('click', (event) => {
+  //   event.target.style.backgroundColor = '';
+  //   event.target.style.backgroundColor = 'rgb(128, 128, 128';
+  // });
+ 
+
+
+
+
+// Requisito 07 Modo que deu certo
+// const getOL = document.getElementById('lista-tarefas');
+// getOL.addEventListener('click', (event) => {
+//   event.target.style.backgroundColor = 'rgb(128, 128, 128';
+//   });
