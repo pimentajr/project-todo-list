521c7a06220b343fee15f54feb0669dea0116a79
DADA <davimarque2000@gmail.com>
2021-04-14 22:44:11 -0300

    completa 100% dos requisitos e conserta todos erros de lint, se deus quiser

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-
 const addButton = document.querySelector('#criar-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const clearButton = document.querySelector('#apaga-tudo');
 const completedClear = document.querySelector('#remover-finalizados');
 const saveListButton = document.querySelector('#salvar-tarefas');
+const moveUp = document.querySelector('#mover-cima');
+const moveDown = document.querySelector('#mover-baixo');
+const selectedRemoveButton = document.querySelector('#remover-selecionado');
+const selectedClass = '.selectedTask';
 function retrieveState() {
   taskList.innerHTML = localStorage.getItem('tasks');
 }
@@ -17,9 +20,9 @@ addButton.addEventListener('click', () => {
   document.querySelector('#texto-tarefa').value = '';
 });
 taskList.addEventListener('click', (event) => {
-  const selected = document.querySelectorAll('.selectedTask');
-  for (let index = 0; index < selected.length; index += 1) {
-    selected[index].classList.remove('selectedTask');
+  const selectedTask = document.querySelectorAll(selectedClass);
+  for (let index = 0; index < selectedTask.length; index += 1) {
+    selectedTask[index].classList.remove('selectedTask');
   }
   event.target.classList.add('selectedTask');
 });
@@ -41,9 +44,26 @@ completedClear.addEventListener('click', () => {
     completedTasks[index].remove();
   }
 });
-function saveList() {
-  localStorage.setItem('tasks', taskList.innerHTML);
-  return;
-}
 
-saveListButton.addEventListener('click', saveList);
+saveListButton.addEventListener('click', () => {
+  localStorage.setItem('tasks', taskList.innerHTML);
+});
+
+moveUp.addEventListener('click', () => {
+  const selectedTask = document.querySelector(selectedClass);
+  const lista = selectedTask.parentElement;
+  if (selectedTask !== null && selectedTask.previousSibling !== null) {
+    lista.insertBefore(selectedTask, selectedTask.previousSibling);
+  }
+});
+moveDown.addEventListener('click', () => {
+  const selectedTask = document.querySelector(selectedClass);
+  const lista = selectedTask.parentElement;
+  if (selectedTask !== null && selectedTask.nextSibling !== null) {
+    lista.insertBefore(selectedTask, selectedTask.nextSibling.nextSibling);
+  }
+});
+selectedRemoveButton.addEventListener('click', () => {
+  const selectedTask = document.querySelector(selectedClass);
+  selectedTask.remove();
+});
