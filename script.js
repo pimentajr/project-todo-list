8de6dc8f2bdb829e886c470b6cc749ae07b3a2cd
Luís Seixas <luiseixas2@gmail.com>
2021-04-15 19:40:49 -0300

    requisito 7

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ function addTask() {
 
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addTask);
+
+function changeBackgroundLi(event) {
+  const getListItem = document.querySelectorAll('li');
+  const targetItem = event.target;
+  for (let index = 0; index < getListItem.length; index += 1) {
+    getListItem[index].classList.remove('selected');
+  }
+  targetItem.classList.add('selected');
+}
+
+const taskList = document.querySelector('#lista-tarefas');
+taskList.addEventListener('click', changeBackgroundLi);
