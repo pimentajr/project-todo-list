582d7762b224a133e79d90b9147c8be18defd313
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-16 07:07:49 -0300

    Adicionadno requisitos de 9

--- script.js
+++ script.js
@@ -1,21 +1,32 @@
 const textoTarefa = document.querySelector('input#texto-tarefa');
 const criarTarefa = document.getElementById('criar-tarefa');
 const lista = document.querySelector('ol#lista-tarefas');
-lista.innerHTML = localStorage.getItem('list');
 
+// função criar tarefa
 criarTarefa.addEventListener('click', () => {
   const item = document.createElement('li');
   item.classList.add('lista-itens');
   item.appendChild(document.createTextNode(textoTarefa.value));
   lista.appendChild(item);
   textoTarefa.value = '';
-  
-  lista.addEventListener('click', (event) => {
-    const evento = event.target;
-    const selTarefa = document.querySelector('.selected');
-    if (selTarefa) {
-      selTarefa.classList.remove('selected');
-    }
-    evento.classList.add('selected');
-  });
+});
+
+// função para selecionar item
+lista.addEventListener('click', (event) => {
+  const evento = event.target;
+  const selTarefa = document.querySelector('.selected');
+  if (selTarefa) {
+    selTarefa.classList.remove('selected');
+  }
+  evento.classList.add('selected');
+});
+
+// função par riscar item
+lista.addEventListener('dblclick', (event) => {
+  const evt = event.target.classList;
+  if (evt.contains('completed')) {
+    evt.remove('completed');
+  } else {
+    evt.add('completed');
+  }
 });
