de0fa5baf539bd24f426899728a92bd913f037ca
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-04-16 02:03:01 -0300

    adiciona botao para adicionar tarefa, quando clicado adiciona um item embaixo do outro e limpa o campo do input

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+const btnCriar = document.querySelector('#criar-tarefa');
+btnCriar.addEventListener('click', () => {
+  const valor = document.querySelector('#texto-tarefa');
+  const listaOl = document.querySelector('#lista-tarefas');
+  const newLi = document.createElement('li');
+  listaOl.appendChild(newLi);
+  newLi.classList.add = 'value-list';
+  newLi.innerText = valor.value;
+  valor.value = '';
+});
+
 // npm run lint
 // npm run lint:styles
 // npm run cypress:open
