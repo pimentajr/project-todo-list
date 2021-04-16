bff8f9c99d5505898711b76adbebf463f797b037
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-16 04:26:59 -0300

    Adicionadno requisitos de 8

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
-const textoTarefa = document.querySelector('input#texto-tarefa'); //.value 
+const textoTarefa = document.querySelector('input#texto-tarefa');
 const criarTarefa = document.getElementById('criar-tarefa');
 const lista = document.querySelector('ol#lista-tarefas');
 lista.innerHTML = localStorage.getItem('list');
 
-criarTarefa.addEventListener('click', function gerar() {
-  let item = document.createElement('li');
+criarTarefa.addEventListener('click', () => {
+  const item = document.createElement('li');
   item.classList.add('lista-itens');
   item.appendChild(document.createTextNode(textoTarefa.value));
   lista.appendChild(item);
   textoTarefa.value = '';
   
-  item.addEventListener('click', function(event) {
-    item.style.backgroundColor = 'gray';
+  lista.addEventListener('click', (event) => {
+    const evento = event.target;
+    const selTarefa = document.querySelector('.selected');
+    if (selTarefa) {
+      selTarefa.classList.remove('selected');
+    }
+    evento.classList.add('selected');
   });
 });
