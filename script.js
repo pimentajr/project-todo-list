d59a743b02599f3dcff370c8dd05ffcd75ff4c4e
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-04-16 19:09:06 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const listaTarefas = document.querySelector('#lista-tarefas');
-const textoTarefa = document.getElementById('texto-tarefa');
-const classSelected = document.querySelector('.selected');
-const liCriada = document.createElement('li');
-const liList = listaTarefas.innerHTML;
+let listaTarefas = document.querySelector('#lista-tarefas');
+let textoTarefa = document.getElementById('texto-tarefa');
+let classSelected = document.querySelector('.selected');
+let liCriada = document.createElement('li');
+let liList = listaTarefas.innerHTML;
 
 // Função carregar itens salvos
 window.onload = function() {
@@ -14,37 +14,37 @@ document.getElementById('criar-tarefa').addEventListener('click', function() {
   liCriada.innerText = textoTarefa.value;
   listaTarefas.appendChild(liCriada);
   textoTarefa.value = ''
-})
+});
 
 // Evento com função pra trocar a classe .selected nas li
 listaTarefas.addEventListener('click', function (event) {
-    if (classSelected.length > 0) {
-        classSelected[0].classList.remove('selected');
-        event.target.classList.toggle('selected');
-    } else {
-        event.target.classList.toggle('selected');
-    }
-})
+  if (classSelected.length > 0) {
+    classSelected[0].classList.remove('selected');
+    event.target.classList.toggle('selected');
+  } else {
+    event.target.classList.toggle('selected');
+  }
+});
 
 // Evento com função para marcar .completed nas li com doubleclick
 listaTarefas.addEventListener('dblclick', function (event) {
-    event.target.classList.toggle('completed');
-})
+  event.target.classList.toggle('completed');
+});
 
 // Criar butão para limpar as li
 document.getElementById('apaga-tudo').addEventListener('click', function() {
-    while (listaTarefas.firstChild) {
-        listaTarefas.removeChild(listaTarefas.firstChild);
-    }
-})
+  while (listaTarefas.firstChild) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
+});
 
 // Criar butão para apagar todas as li com a classe .completed
 function deleteCompleted() {
-    for (let index = document.querySelectorAll('li').length -1; index >= 0; index -= 1) {
-        if (document.querySelectorAll('li')[index].className.includes('completed')) {
-            document.querySelectorAll('li')[index].remove();
-        }
+  for (let index = document.querySelectorAll('li').length - 1; index >= 0; index -= 1) {
+    if (document.querySelectorAll('li')[index].className.includes('completed')) {
+      document.querySelectorAll('li')[index].remove();
     }
+  }
 }
 
 document.getElementById('remover-finalizados').addEventListener('click', deleteCompleted);
