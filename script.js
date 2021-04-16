87d013d58e9398d88e0bd1a14da8016a6dab4a2d
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-04-16 18:35:20 -0300

    arrumando código

--- script.js
+++ script.js
@@ -1,32 +1,43 @@
+const listaTarefas = document.querySelector('#lista-tarefas');
+const textoTarefa = document.getElementById('texto-tarefa');
+const classSelected = document.getElementsByClassName('selected');
+
+// Função carregar itens salvos
 window.onload = function() {
-    document.querySelector('#lista-tarefas').innerHTML = localStorage.savedList;
-    
+    listaTarefas.innerHTML = localStorage.savedList; 
 }
+
+// Evento com função pra criar e inserir elementos li
 document.getElementById('criar-tarefa').addEventListener('click', function() {
     let liCriada = document.createElement('li');
-    liCriada.innerText = document.getElementById('texto-tarefa').value;
-    document.getElementById('lista-tarefas').appendChild(liCriada);
-    document.getElementById('texto-tarefa').value = ''
+    liCriada.innerText = textoTarefa.value;
+    listaTarefas.appendChild(liCriada);
+    textoTarefa.value = ''
 })
-document.getElementById('lista-tarefas').addEventListener('click', function (event) {
-    if (document.getElementsByClassName('selected').length > 0) {
-        document.getElementsByClassName('selected')[0].classList.remove('selected');
+
+// Evento com função pra trocar a classe .selected nas li
+listaTarefas.addEventListener('click', function (event) {
+    if (classSelected.length > 0) {
+        classSelected[0].classList.remove('selected');
         event.target.classList.toggle('selected');
     } else {
         event.target.classList.toggle('selected');
     }
 })
 
-document.getElementById('lista-tarefas').addEventListener('dblclick', function (event) {
+// Evento com função para marcar .completed nas li com doubleclick
+listaTarefas.addEventListener('dblclick', function (event) {
     event.target.classList.toggle('completed');
 })
 
+// Criar butão para limpar as li
 document.getElementById('apaga-tudo').addEventListener('click', function() {
-    while (document.getElementById('lista-tarefas').firstChild) {
-        document.getElementById('lista-tarefas').removeChild(document.getElementById('lista-tarefas').firstChild);
+    while (listaTarefas.firstChild) {
+        listaTarefas.removeChild(listaTarefas.firstChild);
     }
 })
 
+// Criar butão para apagar todas as li com a classe .completed
 function deleteCompleted() {
     for (let index = document.querySelectorAll('li').length -1; index >= 0; index -= 1) {
         if (document.querySelectorAll('li')[index].className.includes('completed')) {
@@ -37,17 +48,19 @@ function deleteCompleted() {
 
 document.getElementById('remover-finalizados').addEventListener('click', deleteCompleted)
 
+// Função pra deletar o elemento com a classe .selected
 function deleteSelected() {
-    const selected = document.querySelector('.selected');
-    selected.remove();
+    classSelected.remove();
 }
 
 document.querySelector('#remover-selecionado').addEventListener('click', deleteSelected);
+
+// Função pra salvar o conteudo do ol como html no localstorage
 // Referência: Luan Alexandre
 // https://github.com/tryber/sd-011-project-todo-list/pull/138/
 function saveList() {
     localStorage.clear();
-    let liList = document.querySelector('#lista-tarefas').innerHTML;
+    let liList = listaTarefas.innerHTML;
     localStorage.savedList = liList;
 }
 
