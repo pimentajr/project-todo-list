dab8c76f40af77cf537d23c0785df8c474bff5a8
Tulio Sploradori <tuliospl@gmail.com>
2021-04-16 11:38:59 -0300

    conclusao do itme 9

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ const listaOrdenada = document.getElementById('lista-tarefas');
 function criaLista() {
   const entradaDeTexto = document.getElementById('texto-tarefa');
   button.addEventListener('click', function () {
-    const recebe = document.createElement('li');    
-    recebe.innerHTML = entradaDeTexto.value;   
+    const recebe = document.createElement('li');
+    recebe.innerHTML = entradaDeTexto.value;
     listaOrdenada.appendChild(recebe);
     entradaDeTexto.value = null;
   });
@@ -18,8 +18,20 @@ function selecionaTarefa() {
     const tarefaSelecionada = document.querySelector('.selecionado');
     if (tarefaSelecionada) {
       tarefaSelecionada.classList.remove('selecionado');
-    }    
-    eTarget.classList.add('selecionado');    
+    }
+    eTarget.classList.add('selecionado');
   });
 }
 selecionaTarefa();
+
+function tarefaCompletada() {
+  listaOrdenada.addEventListener('dblclick', function (e) {
+    const eTarget = e.target;
+    if (eTarget.classList === 'completed') {
+      eTarget.classList.remove('completed');
+    } else {
+      eTarget.classList.add('completed');
+    }
+  });
+}
+tarefaCompletada();
