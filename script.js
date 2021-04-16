dcef31b2ecd318a49162799c4fbbb43203fe8d5b
Julio Cesar <juliocesar@pop-os.localdomain>
2021-04-16 12:06:49 -0300

    requisitos 9 e 11 funcionando

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const buttonTaskCreator = document.querySelector('#criar-tarefa');
 const listOfTasks = document.querySelector('#lista-tarefas');
 const cleanButton = document.querySelector('#apaga-tudo');
 const cleanCompletedButton = document.querySelector('#remover-finalizados');
-const completedTask = document.querySelectorAll('.completed');
 const itemListSelected = document.querySelector('.selected');
 
 let returnClassCompleted;
@@ -19,7 +18,6 @@ function createTaskItem() {
   if (createTask.value !== '') {
     taskAdd.innerText = createTask.value;
     taskAdd.classList.add('task');
-    //taskAdd.addEventListener('click', changeBackground);
     listOfTasks.appendChild(taskAdd);
     return;
   }
@@ -36,45 +34,43 @@ function clearTextInput() {
 function selectedTask(event) {
   let listSelected = document.getElementsByClassName('selected');
   for (let index = 0; index < listSelected.length; index += 1) {
-      listSelected[index].classList.remove('selected');
+    listSelected[index].classList.remove('selected');
   }
-  event.target.className = 'selected';
   event.target.classList.add('selected');
 }
 
-// todo: marca os itens já realizados com um traço horizontal
-// function completedTask() {
-//   for (let i = 0; i < locateSelectedItens.length; i +=1) {
-//     locateSelectedItens[i].classList.toggle('completed');
-//     locateSelectedItens[i].addEventListener('dblclick', completedTask)
-//   }
+// *marca os itens já realizados com um traço horizontal
+function lineCompleted(event) {
+  event.target.classList.toggle('completed');
+}
 
-// }
+
 
 
 //  *Limpa todos os itens da lista 
 function cleanAll() {
   let list = document.querySelectorAll('li');
-  for (let i = 0; i < list.length; i +=1) {
-    list[i].remove();
+  for (let index = 0; index < list.length; index +=1) {
+    list[index].remove();
   }   
 }
-    
+
 // todo: *Limpa todos os itens que já foram realizados
-  // function cleanAllCompleted() {
-  //   for (let i = 0; i < completedTask.length; i +=1) {
-  //     completedTask[i].remove();
-  //   }
-  // }
-    
-    
+function cleanAllCompleted() {
+  let completedTask = document.querySelectorAll('.completed');
+  for (let i = 0; i < completedTask.length; i +=1) {
+    completedTask[i].remove();
+  }
+}
+
+
 // *Realiza todas as ações addEventListener
 function readAddEventListener() {    
-  listOfTasks.addEventListener ("click", selectedTask);
+  listOfTasks.addEventListener ('click', selectedTask);
+  listOfTasks.addEventListener('dblclick', lineCompleted);
   buttonTaskCreator.addEventListener('click', createTaskItem);
   buttonTaskCreator.addEventListener('click', clearTextInput);
   cleanButton.addEventListener('click', cleanAll);
-  //cleanCompletedButton.addEventListener('click', cleanAllCompleted); 
+  cleanCompletedButton.addEventListener('click', cleanAllCompleted);
 }
 readAddEventListener();
-    
