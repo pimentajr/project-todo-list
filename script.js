9f210aa39bb1e57c5a8c3d9749680ec1462c909d
WalterTNFC <walter.tad@hotmail.com>
2021-04-17 20:26:56 -0300

    test

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
-//window.onload = function(){
+	
+let addTask = document.getElementById('criar-tarefa');
+let listTask =  document.getElementById('lista-tarefas');
+let textNewTask = document.getElementById('texto-tarefa')
 
-  let addTarefas = document.getElementById('criar-tarefa');
-	let listTarefas =  document.getElementById('lista-tarefas');
-	let textNewTarefa = document.getElementById('texto-tarefa')
-	let newElement = document.createElement('li');
+function addTasks(){
+	addTask.addEventListener('click', function() {
 
-  addTarefas.addEventListener('click', function(){
-		
+		let newElement = document.createElement('li');
 		newElement.innerText = textNewTarefa.value;
 		listTarefas.appendChild(newElement);
 		textNewTarefa.value = '';
 
-  });
+	});
+}
+addTask();
 
-//}
+function clickAndChangeColor(color){
+
+	listTask.addEventListener('click', (event) => {
+		event.target.style.backgroundColor = color;
+	});
+} 
+
+clickAndChangeColor ('rgb(128, 128, 128)');
