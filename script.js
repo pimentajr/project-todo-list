ce4c2e4e05b2dac8bd65338fdec562ee9cc618d1
Halysson2 <halyssonsena83@gmail.com>
2021-04-17 18:07:59 -0300

    adicionando questão 6

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const taskAdded = document.getElementById('criar-tarefa');
 const text = document.getElementById('texto-tarefa');
 const listeOl = document.getElementById('lista-tarefas');
+const generalErase = document.getElementById('apaga-tudo')
+const removeFinished = document.getElementById('remove-finalizados')
 
 taskAdded.addEventListener('click', () => {
     const newLi = document.createElement('li');
@@ -8,5 +10,16 @@ newLi.innerText = text.value
 listeOl.appendChild(newLi);
 text.value = ''
 
-
 })
+function selectColor() {
+    listeOl.addEventListener('click', (event) => {
+        const toColor = event.toColor;
+        const selectedColor = document.querySelector('.selected');
+        if(selectColor){
+            selectColor.classList.remove('selected');
+        }else{
+       toColor.classList.add('selected');
+        }
+    })
+}
+selectColor()
