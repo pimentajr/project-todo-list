04450df02c85a5a0b2612e14f7e44aa99686f98b
BeatrizR <biarl.ramalhorl@gmail.com>
2021-04-17 17:21:31 -0300

    requisito 05

---
+++ script.js
@@ -0,0 +1,19 @@
+const task = document.querySelector('#texto-tarefa');
+const taskList = document.querySelector('#lista-tarefas');
+const taskCreate = document.querySelector('#criar-tarefa')
+
+
+function addTask(string) {
+  if (task.value !== '') {
+    const newTask = document.createElement('li');
+    newTask.className.add('task');
+    newTask.innerText = string;
+    taskList.appendChild(newTask);
+  }
+  task.focus();
+  task.value = ''; 
+}
+
+taskCreate.addEventListener('click', () => {
+  addTask(task.value);
+});
