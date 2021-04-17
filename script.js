791f8b7c3676c2faa82e453297b6515506eaaec9
IgorMendez <igor.emp@hotmail.com>
2021-04-17 12:28:12 -0300

    Exercício 9

--- script.js
+++ script.js
@@ -1,33 +1,40 @@
-const criarTarefa = document.getElementById('criar-tarefa')
-const textoTarefa = document.getElementById('texto-tarefa')
-const listTarefas = document.getElementById('lista-tarefas')
+const criarTarefa = document.getElementById('criar-tarefa');
+const textoTarefa = document.getElementById('texto-tarefa');
+const listTarefas = document.getElementById('lista-tarefas');
+const listaTarefaArr = listTarefas.children;
 
 function getText() {
-  const criaLista = document.createElement('li')
-  listTarefas.appendChild(criaLista)
-  if(textoTarefa.value !== '') {
-    listTarefas.lastChild.innerText = textoTarefa.value
-    textoTarefa.value = ''
+  const criaLista = document.createElement('li');
+  listTarefas.appendChild(criaLista);
+  if (textoTarefa.value !== '') {
+    listTarefas.lastChild.innerText = textoTarefa.value;
+    textoTarefa.value = '';
   } else {
-    listTarefas.lastChild.remove()
+    listTarefas.lastChild.remove();
   }
 }
 
-function removeColor(e) {
-  for (let index = 0; index < listTarefas.childNodes.length; index++) {
-    listTarefas.childNodes[index].classList.remove('back-ground')
-    
+function rowText(div) {
+  if (div.path[0].classList.value === 'row') {
+    div.path[0].classList.remove('row');
+  } else {
+    div.path[0].classList.add('row');
+  }
+}
+
+function removeColor() {
+  for (let index = 0; index < listTarefas.childNodes.length; index += 1) {
+    listTarefas.childNodes[index].style.backgroundColor = '';
+    listaTarefaArr[index].addEventListener('dblclick', rowText);
   }
 }
 
 function setListColor(e) {
-  removeColor()
-  if(e.target.innerText) {
-    e.target.classList.add('back-ground')
+  removeColor();
+  if (e.target.innerText) {
+    e.target.style.backgroundColor = 'rgb(128,128,128)';
   }
 }
 
-listTarefas.addEventListener('click', setListColor)
-criarTarefa.addEventListener('click', getText)
-
-// console.log(criarTarefa)
+listTarefas.addEventListener('click', setListColor);
+criarTarefa.addEventListener('click', getText);
