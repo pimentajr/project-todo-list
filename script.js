cc291be551bbc63f845c6cd08bd65b9f100e3b87
BeatrizR <biarl.ramalhorl@gmail.com>
2021-04-17 18:29:15 -0300

    corrigindo erros

--- script.js
+++ script.js
@@ -1,24 +1,16 @@
-const task = document.querySelector('#texto-tarefa');
+const input = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
+
+function addTask(text) {
+    const newTask = document.createElement('li');
+    newTask.className.add = 'task';
+    newTask.innerText = text;
+    return newTask;
+}
 const taskCreate = document.querySelector('#criar-tarefa');
 
-function addTask(string) {
-  if ((task.value !== '')) {
-    const newTask = document.createElement('li');
-    newTask.className.add('task');
-    newTask.innerText = string;
-    taskList.appendChild(newTask);
-  }
-  task.focus();
-  task.value = '';
-}
-
 taskCreate.addEventListener('click', () => {
-  addTask(task.value);
-});
-
-task.addEventListener('keydown', (en) => {
-  if (en.key === 'Enter') {
-    addTask(task.value);
-  }
+  const text = input.value;
+  taskList.appendChild(addTask(text));
+  input.value = '';
 });
