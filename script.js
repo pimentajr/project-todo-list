1130ed78f2069bbb1836a8b510ba9738629dec68
fernando lasmar <f3rnandolasmar@gmail.com>
2021-04-17 10:41:59 -0300

    requisito 11

--- script.js
+++ script.js
@@ -41,3 +41,16 @@ function limparLista() {
   });
 }
 limparLista();
+
+// 11
+const limparFinalizados = document.getElementById('remover-finalizados');
+const tarefaFinalizados = document.getElementsByClassName('completed');
+function finalizados() {
+  limparFinalizados.addEventListener('click', () => {
+    for (let index = 0; tarefaFinalizados.length; index -= 1) {
+      const removeFinalizados = tarefaFinalizados[index]; // nova variavel para receber itens da lista que quero remover
+      lista.removeChild(removeFinalizados);
+    }
+  });
+}
+finalizados();
