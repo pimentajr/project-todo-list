3364780414d460729de8d137b9f29ee755dd3bba
Nalu <aluziapantoja@gmail.com>
2021-04-18 13:26:13 -0300

    Erros lint

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-//5 e 6
+// 5 e 6
 const criarTarefa = document.getElementById('criar-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
 const tarefa = document.getElementById('texto-tarefa')
-
 criarTarefa.addEventListener('click', () => {
-   const itemLista = document.createElement('li');
-   itemLista.innerText = tarefa.value;
-   listaTarefa.appendChild(itemLista);
-   tarefa.value = '';
+  const itemLista = document.createElement('li');
+  itemLista.innerText = tarefa.value;
+  listaTarefa.appendChild(itemLista);
+  tarefa.value = '';
 })
 
-//7 e 8
+// 7 e 8
 function selectedItem() {
-    listaTarefa.addEventListener('click', (event) => {
-        const evTarget = event.target;
-        const itemSelecionado = document.querySelector('.selected');
-        if (itemSelecionado) {
-            itemSelecionado.classList.remove('selected');
-        } 
-        evTarget.classList.add('selected');
-    })
+  listaTarefa.addEventListener('click', (event) => {
+    const evTarget = event.target;
+     const itemSelecionado = document.querySelector('.selected');
+    if (itemSelecionado) {
+      itemSelecionado.classList.remove('selected');
+    } 
+    evTarget.classList.add('selected');
+  });
 }
 
 selectedItem();
 
-//9
+// 9
 function dbSelectedItem () {
-    listaTarefa.addEventListener('dblclick', (event) => {
-        if (event.target.classList.contains('completed')) {
-          event.target.classList.remove('completed');
-        } else {
-          event.target.classList.add('completed');  
-        }
-    })
+  listaTarefa.addEventListener('dblclick', (event) => {
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');  
+    }
+  });
 }
 
 dbSelectedItem();
 
-//10
+// 10
 const apagar = document.getElementById('apaga-tudo');
-function deleteAll () {
-    apagar.addEventListener('click', () => {
-      listaTarefa.innerHTML = '';
-    })
+function deleteAll() {
+  apagar.addEventListener('click', () => {
+    listaTarefa.innerHTML = '';
+  });
 }
 
 deleteAll();
