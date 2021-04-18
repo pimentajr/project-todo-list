bfa3b65041b46820e1c9483061654b1bc30aa498
Michel Pereira <michelbecauro@gmail.com>
2021-04-18 08:49:28 -0300

    alguns ajustes na organização do código

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let itemS = []; // Array a ser atualizado com os <li> que vão sendo criados ou removidos. Isso ajuda as funções terem a mesma referência. 
+// [Sobre a declaração abaixo] "itemS" é um array a ser atualizado com os elementos <li> que vão sendo criados ou removidos. Usado para que as funções tenham a mesma referência.
+// Pegar elemento pai usando getElementByTagname(), acredito que não darai certo. Fiz umas tentativas e deu erado.
+// Como não existem elementos filhos para <ol>, este elemento fica com retorno nulo, logo FOR causa erros de exceções com arrays Nulos (fonte: https://www.guj.com.br/t/como-saber-se-o-array-e-nulo-ou-vazio/191165/2)
+let itemS = [];  
 
 function criar() {
 	const bt = document.getElementById('criar-tarefa');
@@ -29,19 +32,7 @@ function dblClick() {
 	const ol = document.getElementById('lista-tarefas');
 	ol.addEventListener('dblclick', function(event) {
 		let clicado = event.target;
-		clicado.classList.toggle('completed'); 
-	});
-}
-
-function apagarTudo() {
-	const bt = document.getElementById('apaga-tudo');
-	var ol = document.getElementById('lista-tarefas');
-	bt.addEventListener('click', function() {
-		for (let index=0; index <itemS.length ; index += 1) {
-			let li = itemS[index];
-			ol.removeChild(li);
-		}
-		itemS = []; // Esvazia o array de referência de <li>. Mas acho que um "itemS.splice(index,1)"" dentro do FOR também resolveria.	
+		clicado.classList.toggle('completed'); // a função toggle() verifica se a classe especificada existe; se sim, remove-a, se não, adiciona-a.
 	});
 }
 
@@ -56,12 +47,24 @@ function removerFinalizados() {
 				itemS.splice(index,1); // splice() remove itens de array | Fonte (W3S): https://www.w3schools.com/jsref/jsref_splice.asp
 
 				// [SOBRE PRÓXIMA LINHA de código] 
-				// Decrementei o index porque a array de referência de "length" --- o "itemS" ---, tem seu tamanho dimuido ao remover-se um de seus items com splice(), mudando todo o seu RANGE de index. 
+				// Decrementei o index porque a array de referência de "length" --- o "itemS" ---, tem seu tamanho diminuido ao remover-se um de seus items com splice(), mudando todo o seu RANGE de index. 
 				// Então, observei que o FOR não atualiza a referência de tamanho (length) obtido de "itemS".
 				// Portanto, decrementar o index cada hora que cai nesse IF, é um forma de não perder algum item do array que se queira remover, mas que assume um novo index (menor, óbvio) diferente do RANGE que o FOR tem.
 				index -= 1;	
 			}
 		}
+	});
+}
+
+function apagarTudo() {
+	const bt = document.getElementById('apaga-tudo');
+	var ol = document.getElementById('lista-tarefas');
+	bt.addEventListener('click', function() {
+		for (let index=0; index <itemS.length ; index += 1) {
+			let li = itemS[index];
+			ol.removeChild(li);
+		}
+		itemS = []; // Esvazia o array de referência de <li>. Mas acho que um "itemS.splice(index,1)", junto com decrementação do "index" dentro do FOR, também resolveria.	
 	});
 }
 
