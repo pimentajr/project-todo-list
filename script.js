c6e0df48ac8802f7c0ba7018d6c406eebc8571cc
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-04-18 00:16:59 -0300

    Desafio 9 concluido - Adicionado função de marcar concluido

--- script.js
+++ script.js
@@ -2,28 +2,40 @@ function colorChange(event) {
   const task = event.target;
   const selectedTasks = document.querySelector('.selected');
 
-  if (task.className === 'selected') {
+  if (task.classList === 'selected') {
     task.classList.remove('selected');
     return;
   }
   if (selectedTasks) {
     selectedTasks.classList.remove('selected');
   }
-  task.className = 'selected';
+  task.classList.add('selected');
+}
+
+function completedTasks(event) {
+  const task = event.target;
+  console.log(task.classList);
+  if (task.classList[0] === 'completed') {
+    task.classList.remove('completed');
+    return;
+  }
+  task.classList.add('completed');
 }
 
 function taskAdder() {
   const taskToAdd = document.querySelector('#texto-tarefa').value;
-  const createdElement = document.createElement('li');
-  createdElement.append(taskToAdd);
-
-  document.querySelector('#lista-tarefas').appendChild(createdElement);
+  if (taskToAdd) {
+    const createdElement = document.createElement('li');
+    createdElement.append(taskToAdd);
+    document.querySelector('#lista-tarefas').appendChild(createdElement);
+  }
 
   document.querySelector('#texto-tarefa').value = '';
 
   const allTask = document.querySelectorAll('li');
   for (let index = 0; index < allTask.length; index += 1) {
     allTask[index].addEventListener('click', colorChange);
+    allTask[index].addEventListener('dblclick', completedTasks);
   }
 }
 
