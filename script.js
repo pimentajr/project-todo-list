e529fe3b6304afd1f7182ffed12c226e14178e6b
Michel Pereira <michelbecauro@gmail.com>
2021-04-18 03:32:40 -0300

    JS Requsito 10: Done

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let itemS = [];
+let itemS = []; // Array a ser atualizado com os <li> que vão sendo criados ou removidos. Isso ajuda as funções terem a mesma referência. 
 
 function criar() {
 	const bt = document.getElementById('criar-tarefa');
@@ -18,15 +18,14 @@ function selecionar() {
 	const ol = document.getElementById('lista-tarefas');
 	ol.addEventListener('click', function(event){
 		let clicado = event.target;
-
-		for (let index=0; index <itemS.length ; index += 1) {
-			itemS[index].style.backgroundColor = '';
-			clicado.style.backgroundColor = 'rgb(128, 128, 128';
+		for (let index=0; index <itemS.length ; index += 1) { // Limpa backgroundColor de todos items.
+			itemS[index].style.backgroundColor = ''; 
 		}
+		clicado.style.backgroundColor = 'rgb(128, 128, 128';
 	});
 }
 
-function dblClick () {
+function dblClick() {
 	const ol = document.getElementById('lista-tarefas');
 	ol.addEventListener('dblclick', function(event) {
 		let clicado = event.target;
@@ -34,10 +33,24 @@ function dblClick () {
 	});
 }
 
+function apagarTudo() {
+	const bt = document.getElementById('apaga-tudo');
+	var ol = document.getElementById('lista-tarefas');
+	bt.addEventListener('click', function() {
+		for (let index=0; index <itemS.length ; index += 1) {
+			let li = itemS[index];
+			ol.removeChild(li);
+		}
+		itemS = []; // Esvazia o array de referência de <li>. Mas acho que um "itemS.splice(index,1)"" dentro do FOR também resolveria.	
+	});
+}
+
 function start() {
   criar();
 	selecionar();
 	dblClick();
+	apagarTudo();
+	// removerFinalizados();
 }
 
 window.onload = start;
