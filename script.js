60c2df548db5e0b841c49e568cea5fa632271976
guinhoanjos <75916978+guinhoanjos@users.noreply.github.com>
2021-04-18 18:11:28 -0300

    consertando lint

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 window.onload = function () {
   document.querySelector('#lista-tarefas').innerHTML = localStorage.savedList;
-}
+};
 document.getElementById('criar-tarefa').addEventListener('click', function() {
     let liCriada = document.createElement('li');
     liCriada.innerText = document.getElementById('texto-tarefa').value;
     document.getElementById('lista-tarefas').appendChild(liCriada);
-    document.getElementById('texto-tarefa').value = ''
-})
+    document.getElementById('texto-tarefa').value = '';
+});
 document.getElementById('lista-tarefas').addEventListener('click', function (event) {
     if (document.getElementsByClassName('selected').length > 0) {
         document.getElementsByClassName('selected')[0].classList.remove('selected');
@@ -14,27 +14,27 @@ document.getElementById('lista-tarefas').addEventListener('click', function (eve
     } else {
         event.target.classList.toggle('selected');
     }
-})
+});
 
 document.getElementById('lista-tarefas').addEventListener('dblclick', function (event) {
     event.target.classList.toggle('completed');
-})
+});
 
 document.getElementById('apaga-tudo').addEventListener('click', function() {
     while (document.getElementById('lista-tarefas').firstChild) {
         document.getElementById('lista-tarefas').removeChild(document.getElementById('lista-tarefas').firstChild);
     }
-})
+});
 
 function deleteCompleted() {
-    for (let index = document.querySelectorAll('li').length -1; index >= 0; index -= 1) {
+    for (let index = document.querySelectorAll('li').length - 1; index >= 0; index -= 1) {
         if (document.querySelectorAll('li')[index].className.includes('completed')) {
             document.querySelectorAll('li')[index].remove();
         }
     }
 }
 
-document.getElementById('remover-finalizados').addEventListener('click', deleteCompleted)
+document.getElementById('remover-finalizados').addEventListener('click', deleteCompleted);
 
 function deleteSelected() {
     const selected = document.querySelector('.selected');
