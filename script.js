f7c411383740327a867327c8386db732b01529c0
WalterTNFC <walter.tad@hotmail.com>
2021-04-18 19:03:40 -0300

    Item 9 e 10

--- script.js
+++ script.js
@@ -13,10 +13,12 @@ function addTasks(){
 let bot = document.getElementById('criar-tarefa');
 bot.addEventListener('click', addTasks)
 
-//item 7
+//item 7 e 8
+
+let listElement = document.getElementsByClassName('listClass');
 function clickAndChangeColor(color){
 
-	let listElement = document.getElementsByClassName('listClass');
+
 	listTask.addEventListener('click', function(event) {
 
 		for(let index=0; index < listElement.length ; index += 1){
@@ -26,10 +28,38 @@ function clickAndChangeColor(color){
 				event.target.style.backgroundColor = color;
 			}
 		}
-	
 	});
 } 
 
 clickAndChangeColor('rgb(128, 128, 128)');
 
+//item 9
 
+ function doubleClick(){
+
+ 	listTask.addEventListener('dblclick', function (event) {
+
+		if (event.target.classList.contains('completed')) {
+			event.target.classList.remove('completed');
+		}
+		else{
+			event.target.classList.add('completed');
+		}
+	}); 
+
+} 
+
+doubleClick();
+
+
+// item 10
+let clearBot = document.querySelector('#apaga-tudo');
+
+ function clear(){
+	clearBot.addEventListener("click", function () {
+		listTask.innerHTML= '';
+	});
+} 
+clear();
+
+// item 11
