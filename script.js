f14874f9a573b9e8a77ec13742def99f6e79e19f
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-18 20:53:02 -0300

    requisito 7 completado

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
 const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
 const INPUT_TEXTO_TAREFA = document.getElementById('texto-tarefa');
+const LI_LISTA_AFAZERES = document.getElementsByTagName('li');
 
+let lastTarget;
 
 function insertNewListItem() {
   const NEW_LI = document.createElement('li');
@@ -14,6 +16,18 @@ function eraseInputValue() {
   INPUT_TEXTO_TAREFA.value = '';
 }
 
+function selectedLi(event) {
+  const currentTarget = event.target;
+
+  if (currentTarget.style.backgroundColor === "") {
+    currentTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+  } else {
+    currentTarget.removeAttribute("style");
+  }
+}
+
 window.onload = () => {
-  BTN_CRIAR_TAREFA.addEventListener('click', insertNewListItem)
+  BTN_CRIAR_TAREFA.addEventListener('click', insertNewListItem);
+
+  OL_LISTA_TAREFAS.addEventListener('click', selectedLi);
 }
