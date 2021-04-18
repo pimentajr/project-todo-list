26ec5e42a5fc69cebf626050de78a4bf7cf653ca
Lais Simenikim <l.simenikim@gmail.com>
2021-04-18 19:47:37 -0300

    add requisito 7 e 8

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-let botao = document.getElementById('criar-tarefa');
-let listaTarefas = document.getElementById('lista-tarefas'); //ol
-let textoTarefa = document.getElementById('texto-tarefa'); //input
+const botao = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas'); // ol
+const textoTarefa = document.getElementById('texto-tarefa'); // input
 
-botao.addEventListener('click', addTarefaNaLista);
+function backColorItemSelected(event) {
+  const selectedItem = document.querySelector('.selected');
+
+  if (selectedItem !== null) {
+    selectedItem.classList.remove('selected');
+  }
+
+  event.target.classList.add('selected');
+}
 
 function addTarefaNaLista() {
   if (textoTarefa.value !== '') {
-    let itemLista = document.createElement('li');
+    const itemLista = document.createElement('li');
     itemLista.innerText = textoTarefa.value;
     textoTarefa.value = '';
     listaTarefas.appendChild(itemLista);
+    itemLista.addEventListener('click', backColorItemSelected);
   }
 }
+
+botao.addEventListener('click', addTarefaNaLista);
