e8226b340a36c9253e8e6cfb5f8e67464bd8b255
Nalu <aluziapantoja@gmail.com>
2021-04-18 13:29:48 -0300

    Erros lint 2

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // 5 e 6
 const criarTarefa = document.getElementById('criar-tarefa');
 const listaTarefa = document.getElementById('lista-tarefas');
-const tarefa = document.getElementById('texto-tarefa')
+const tarefa = document.getElementById('texto-tarefa');
 criarTarefa.addEventListener('click', () => {
   const itemLista = document.createElement('li');
   itemLista.innerText = tarefa.value;
   listaTarefa.appendChild(itemLista);
   tarefa.value = '';
-})
+});
 
 // 7 e 8
 function selectedItem() {
   listaTarefa.addEventListener('click', (event) => {
     const evTarget = event.target;
-     const itemSelecionado = document.querySelector('.selected');
+    const itemSelecionado = document.querySelector('.selected');
     if (itemSelecionado) {
       itemSelecionado.classList.remove('selected');
-    } 
+    }
     evTarget.classList.add('selected');
   });
 }
@@ -24,12 +24,12 @@ function selectedItem() {
 selectedItem();
 
 // 9
-function dbSelectedItem () {
+function dbSelectedItem() {
   listaTarefa.addEventListener('dblclick', (event) => {
     if (event.target.classList.contains('completed')) {
       event.target.classList.remove('completed');
     } else {
-      event.target.classList.add('completed');  
+      event.target.classList.add('completed'); 
     }
   });
 }
@@ -46,16 +46,16 @@ function deleteAll() {
 
 deleteAll();
 
-//11
+// 11
 const apagarFinal = document.getElementById('remover-finalizados');
 const complete = document.getElementsByClassName('completed');
-function deleteFinal () {
-    apagarFinal.addEventListener('click', () => {
-      console.log(complete);
-        for (let i = complete.length - 1; i >= 0; i -= 1) {
+function deleteFinal() {
+  apagarFinal.addEventListener('click', () => {
+    console.log(complete);
+      for (let i = complete.length - 1; i >= 0; i -= 1) {
         complete[i].remove();
       }
-    })
+  });
 }
 
 deleteFinal();
