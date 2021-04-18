3ca5dbabf37885005c40f02157251a064e5d2f9a
Ivaldo Souza <souza1976ivaldo@gmail.com>
2021-04-18 10:48:17 -0300

    Adicionadno requisitos de 12-1

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const textoTarefa = document.querySelector('input#texto-tarefa');
 const criarTarefa = document.getElementById('criar-tarefa');
 const lista = document.querySelector('ol#lista-tarefas');
 const apagaTudo = document.getElementById('apaga-tudo');
-const removerFinalizados = document.getElementById('remover-finalizados')
+const removerFinalizados = document.getElementById('remover-finalizados');
 const completed = document.getElementsByClassName('completed');
 const salvarTarefas = document.getElementById('salvar-tarefas');
 
@@ -42,12 +42,12 @@ apagaTudo.addEventListener('click', () => {
 removerFinalizados.addEventListener('click', () => {
   for (let index = completed.length - 1; index >= 0; index -= 1) {
     completed[index].remove();
-  };
+  }
 });
 
 salvarTarefas.addEventListener('click', () => {
-  localStorage.setItem(lista, lista.innerHTML )
+  localStorage.setItem(lista, lista.innerHTML);
 })
 
 const saved = localStorage.getItem(lista);
-  lista.innerHTML = saved;
+lista.innerHTML = saved;
