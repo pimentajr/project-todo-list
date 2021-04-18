7a73a85b573bc6181f79162d857889197e8271e0
WalterTNFC <walter.tad@hotmail.com>
2021-04-18 15:53:26 -0300

    Item 8

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-
 let listTask =  document.getElementById('lista-tarefas');
 
-
-
 function addTasks(){
-		let input = document.getElementById('texto-tarefa');
-		let newElement = document.createElement('li');
-		newElement.innerText = input.value;
-		listTask.appendChild(newElement);
-		input.value = '';
+	let input = document.getElementById('texto-tarefa');
+	let newElement = document.createElement('li');
+	newElement.className = 'listClass';
+	newElement.innerText = input.value;
+	listTask.appendChild(newElement);
+	input.value = '';
 
 }
 
 let bot = document.getElementById('criar-tarefa');
 bot.addEventListener('click', addTasks)
 
-
+//item 7
 function clickAndChangeColor(color){
 
+	let listElement = document.getElementsByClassName('listClass');
 	listTask.addEventListener('click', function(event) {
-		event.target.style.backgroundColor = color;
+
+		for(let index=0; index < listElement.length ; index += 1){
+			//atribuir nenhuma cor aos itens da lista
+			listElement[index].style.backgroundColor = null;
+			if(listElement){
+				event.target.style.backgroundColor = color;
+			}
+		}
+	
 	});
 } 
 
-clickAndChangeColor('rgb(128, 128, 128)'); 
+clickAndChangeColor('rgb(128, 128, 128)');
+
+
