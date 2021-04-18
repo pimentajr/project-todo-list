f7f65b4f9e20997846301247cb05ae084f0afa39
Reginara Gomes <refagomes@gmail.com>
2021-04-18 16:10:52 -0300

    Corrige codigo e linter requisito 5

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const listItems = document.getElementById(taskList);
 function createTask() {
   const button = document.getElementById('criar-tarefa');
 
-  button.addEventListener('click', ) {
+  button.addEventListener('click', () => {
     const inputUser = document.getElementById('texto-tarefa');
     const list = document.createElement('li');
     list.className = 'list-item';
@@ -13,6 +13,7 @@ function createTask() {
     listItems.appendChild(list);
 
     inputUser.value = '';
-  };
+  )};
 }
 createTask();
+
