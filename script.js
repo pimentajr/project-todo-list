16744773172970a9c29c7c49295632cefead48d1
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-04-17 21:51:46 -0300

    Desafio 7 concluido - adicionado highligth ao clicar em uma tarefa

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function colorChange(event) {
+  const task = event.target;
+
+  if (task.className === 'selected') {
+    task.classList.remove('selected');
+    return;
+  }
+  task.className = 'selected';
+}
+
 function taskAdder() {
   const taskToAdd = document.querySelector('#texto-tarefa').value;
   const createdElement = document.createElement('li');
@@ -6,6 +16,11 @@ function taskAdder() {
   document.querySelector('#lista-tarefas').appendChild(createdElement);
 
   document.querySelector('#texto-tarefa').value = '';
+
+  const taskSelected = document.querySelectorAll('li');
+  for (let index = 0; index < taskSelected.length; index += 1) {
+    taskSelected[index].addEventListener('click', colorChange);
+  }
 }
 
 window.onload = () => {
