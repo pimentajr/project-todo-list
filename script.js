ea2e7a62c7caa08c3664ace76e36b0416e2252d4
AlyssonSene <alysson_silva@outlook.com>
2021-04-19 19:03:29 -0300

    botao adicionar corrigido

--- script.js
+++ script.js
@@ -4,18 +4,18 @@ function newTask() {
   const text = document.createTextNode(inputValue);
   li.appendChild(text);
   if (inputValue === '') {
-    alert("Campo vazio, informe uma tarefa!");
-  }
+    alert('Campo vazio, informe uma tarefa!');
+  } 
   else {
     document.getElementById('lista-tarefas').appendChild(li);
   }
   document.getElementById('texto-tarefa').value = "";
-  var span = document.createElement("button");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
+  var span = document.createElement('button');
+  var txt = document.createTextNode('\u00D7');
+  span.className = 'close';
   span.appendChild(txt);
   li.appendChild(span);
-  for (let index = 0; index < close.length; index++) {
+  for (let index = 0; index < close.length; index += 1) {
     close[index].onclick = function() {
       var div = this.parentElement;
       div.parentNode.removeChild(div);
@@ -25,7 +25,7 @@ function newTask() {
 
 function creatButtonClosed() {
   var myNodelist = document.getElementsByTagName("li");
-  for (let index = 0; index < myNodelist.length; index++) {
+  for (let index = 0; index < myNodelist.length; index += 1) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
     span.className = "close";
@@ -36,7 +36,7 @@ function creatButtonClosed() {
 creatButtonClosed();
 
 var close = document.getElementsByClassName("close");
-for (let index = 0; index < close.length; index++) {
+for (let index = 0; index < close.length; index += 1) {
   close[index].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
