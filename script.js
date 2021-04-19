bcad18230b5e3fef441502c5adcb871e8310a5c9
Paulo Henrique de Paula Bomfim <paulopbomfim@gmail.com>
2021-04-18 23:34:37 -0300

    Desafio 10 concluido - Adicionado botão de remover tudo

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function colorChange(event) {
   const task = event.target;
   const selectedTasks = document.querySelector('.selected');
-
-  if (task.classList === 'selected') {
+  if (task.classList[0] === 'selected' || task.classList[1] === 'selected') {
     task.classList.remove('selected');
     return;
   }
+
   if (selectedTasks) {
     selectedTasks.classList.remove('selected');
   }
@@ -39,7 +39,19 @@ function taskAdder() {
   }
 }
 
+function taskEraser() {
+  const tasks = document.querySelectorAll('li');
+  if (tasks.length > 0) {
+    for (let index = 0; index < tasks.length; index += 1) {
+      tasks[index].remove('li');
+    }
+  }
+}
+
 window.onload = () => {
   const addButton = document.querySelector('#criar-tarefa');
   addButton.addEventListener('click', taskAdder);
+
+  const removeAllButton = document.querySelector('#apaga-tudo');
+  removeAllButton.addEventListener('click', taskEraser);
 };
