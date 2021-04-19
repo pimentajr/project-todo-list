612e04acf224e6458ed166b14e93e7fe1b3b8f3f
Bernardo Prado <bernardoprado22@yahoo.com.br>
2021-04-19 14:52:59 -0300

    corrige mais ESLINT

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 const btnCreatTask = document.querySelector('#criar-tarefa');
 const listTask = document.querySelector('#lista-tarefas');
-const taskContainer = document.querySelector('#container'); 
+const taskContainer = document.querySelector('#container');
 const inputField = document.querySelector('#texto-tarefa');
 
 function addTask() {
   const task = inputField.value;
   if (task !== '') {
-    const listItem = document.createElement('li');  
+    const listItem = document.createElement('li');
     listItem.className = 'new-item';
     listItem.innerText = task;
     listTask.appendChild(listItem);
 
     taskContainer.reset();
   } else {
-    alert ('Tarefa inválida');
-  }  
+    alert('Tarefa inválida');
+  }
 }
 
 btnCreatTask.addEventListener('click', addTask);
