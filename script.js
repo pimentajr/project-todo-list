944bbc4a7ced042958241f78c0b08a5f119fdd4f
Reginara Gomes <refagomes@gmail.com>
2021-04-19 18:14:44 -0300

    Adiciona requisito 7 e 8

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 const createTask = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
+
 createTask.addEventListener('click', () => {
   const listItem = document.createElement('li');
   listItem.innerText = task.value;
   taskList.appendChild(listItem);
   task.value = '';
 });
+
+function selectedTask() {
+  taskList.addEventListener('click', (event) => {
+    const targetEvent = event.target;
+    const selectedItem = document.querySelector('.selected');
+    if (selectedItem) {
+      selectedItem.classList.remove('selected');
+    }
+    targetEvent.classList.add('selected');
+  });
+}
+
+selectedTask();
+
+
