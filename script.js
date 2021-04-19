989dd8500b246fa5da86afe26df81cedea6abbcc
Uriel Bochi <urielbochi@gmail.com>
2021-04-19 10:42:14 -0300

    requisito 9

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ const buttonCreateTask = document.getElementById('criar-tarefa');
 let entry = document.getElementById('texto-tarefa');
 //Para onde as tarefas vão
 let tasklist = document.querySelector('#lista-tarefas');
+// Classe que risca a frase
+const scratch = document.querySelector('.completed');
+
+let flag = 0
+
 
 function createTasks() {
     buttonCreateTask.addEventListener('click', () =>{
@@ -17,7 +22,6 @@ function createTasks() {
 //Requisito 7 e 8
 // Clicar em um ítem da lista dá ao objeto a classe color, que altera o background para cinza
 function backgroundColor() {
-    //Criação do evento
     tasklist.addEventListener('click', (event) => {
         //Guardando a classe em uma variavel para poder manipular
       const classColor = document.querySelector('.color');
@@ -27,12 +31,35 @@ function backgroundColor() {
       }
       event.target.classList.add('color'); // retorna o elemento que disparou o evento
     });
-  
-  
   }
+
+ 
+  //Requisito 9, riscar e desriscar com um duplo clique
+  function riskWord() {
+    tasklist.addEventListener('dblclick', function(e){
+        let li = e.target.closest('li')
+        let nodes = Array.from(tasklist.children)
+        let index = nodes.indexOf(li)
+        if (flag === 0){
+            nodes[index].classList.add('completed')
+            flag = 1;
+        }
+        else {
+            nodes[index].classList.remove('completed')
+            flag = 0;
+        }
+
+    })
+}
+
+  
+console.log(tasklist[0])
+
+
   
 
 console.log(tasklist)
 
 createTasks()
 backgroundColor()
+riskWord()
