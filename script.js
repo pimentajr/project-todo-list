28b5cc16cc9dbd6b526b8e9e40f3c8ea69bd6566
Uriel Bochi <urielbochi@gmail.com>
2021-04-19 07:59:04 -0300

    exercício 7 e 8

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonCreateTask = document.getElementById('criar-tarefa');
 //Aqui é o input
 let entry = document.getElementById('texto-tarefa');
 //Para onde as tarefas vão
-let tasklist = document.getElementById('lista-tarefas');
+let tasklist = document.querySelector('#lista-tarefas');
 
 function createTasks() {
     buttonCreateTask.addEventListener('click', () =>{
@@ -13,4 +13,26 @@ function createTasks() {
         entry.value= ''
     });
 }
+
+//Requisito 7 e 8
+// Clicar em um ítem da lista dá ao objeto a classe color, que altera o background para cinza
+function backgroundColor() {
+    //Criação do evento
+    tasklist.addEventListener('click', (event) => {
+        //Guardando a classe em uma variavel para poder manipular
+      const classColor = document.querySelector('.color');
+      // Se color = true em algum elemento, então, remova a classe.
+      if(classColor){
+        classColor.classList.remove('color');
+      }
+      event.target.classList.add('color'); // retorna o elemento que disparou o evento
+    });
+  
+  
+  }
+  
+
+console.log(tasklist)
+
 createTasks()
+backgroundColor()
