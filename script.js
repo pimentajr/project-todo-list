8f7ddafe925ac7372fcbdc5f94c26a642cb33498
Thiago Gomes <thiagog.macedom@gmail.com>
2021-04-19 01:28:43 -0300

    requisito 10 concluido sem estilo

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// Creating new list item
-const button = document.getElementById('criar-tarefa');
+// Some global variables
+const buttonNewTask = document.getElementById('criar-tarefa');
+const buttonCleanAll = document.getElementById('apaga-tudo');
+const buttonCleanCompleted = document.getElementById('remover-finalizados');
 const inputText = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 
+// Creating new list item
 function teste() {
   const newTask = inputText.value;
   const newListTask = document.createElement('li');
@@ -12,8 +15,7 @@ function teste() {
 
   inputText.value = '';
 }
-
-button.addEventListener('click', teste);
+buttonNewTask.addEventListener('click', teste);
 
 // Changing item background
 function giveSelect(e) {
@@ -24,7 +26,6 @@ function giveSelect(e) {
   }
   clickedItem.className += ' selected';
 }
-
 taskList.addEventListener('click', giveSelect);
 
 // Changing text decoration
@@ -36,3 +37,12 @@ function changeDecoration(e) {
   }
 }
 taskList.addEventListener('dblclick', changeDecoration);
+
+// Button clean all
+function cleanAll() {
+  const list = document.querySelectorAll('li');
+  for (let index = 0; index < list.length; index += 1) {
+    list[index].remove();
+  }
+}
+buttonCleanAll.addEventListener('click', cleanAll);
