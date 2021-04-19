3632cd41eb0c1b281e9645950bf4c70cf9e940f0
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-19 00:59:54 -0300

    requisito 9 completado

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const INPUT_TEXTO_TAREFA = document.getElementById('texto-tarefa');
 const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
 const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
+const LI_COMPLETED = document.getElementsByClassName('completed')
 
-let lastTarget;
+let lastTargetSelected;
 
 function insertNewListItem() {
   const NEW_LI = document.createElement('li');
@@ -14,16 +15,26 @@ function insertNewListItem() {
 function selectLi(event) {
   const CURRENT_TARGET = event.target;
 
-  if (CURRENT_TARGET != lastTarget) {
-    CURRENT_TARGET.style.backgroundColor = 'rgb(128, 128, 128)';
-    if (lastTarget != undefined) {
-      lastTarget.removeAttribute('style');
+  if (CURRENT_TARGET != lastTargetSelected) {
+    CURRENT_TARGET.classList.add('selected');
+    if (lastTargetSelected != undefined) {
+      lastTargetSelected.classList.remove('selected');
     }
   }
-  lastTarget = CURRENT_TARGET;
+  lastTargetSelected = CURRENT_TARGET;
+}
+
+function completedTask(event) {
+  const CURRENT_TARGET = event.target;
+  if (CURRENT_TARGET.classList[0] === 'completed' || CURRENT_TARGET.classList[1] === 'completed') {
+    CURRENT_TARGET.classList.remove('completed');
+  } else {
+    CURRENT_TARGET.classList.add('completed');
+  }
 }
 
 window.onload = () => {
   BTN_CRIAR_TAREFA.addEventListener('click', insertNewListItem);
   OL_LISTA_TAREFAS.addEventListener('click', selectLi);
+  OL_LISTA_TAREFAS.addEventListener('dblclick', completedTask);
 };
