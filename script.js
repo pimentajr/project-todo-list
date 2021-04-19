2ad6193bf4cb003f7d79369dbf7502b0da39d7d9
Cris Lustosa <cmlustosa@gmail.com>
2021-04-19 18:18:06 -0300

    Corrige erro de sintaxe no HTML

--- script.js
+++ script.js
@@ -3,6 +3,29 @@ const addTaskButton = document.getElementById('criar-tarefa');
 
 const taskList = document.getElementById('lista-tarefas');
 
+
+function toggleTaskCompleted(e) {
+  const task = e.target;
+
+  task.classList.toggle('completed');
+}
+
+
+
+let selectedTask;
+
+function selectTask(e) {
+  const task = e.target;
+
+  task.style.backgroundColor = 'rgb(128, 128, 128)';
+  
+
+  if (selectedTask) {
+    selectedTask.style.backgroundColor = null;
+  }
+  selectedTask = task;
+}
+
 function addTask(text) {
   taskList.insertAdjacentHTML('beforeend', 
   `<li class="task">${text}</li>`
@@ -25,3 +48,11 @@ addTaskInput.addEventListener('keydown', (e) => {
     clearInput();
   }
 })
+
+addTaskInput.addEventListener('keydown', (e) => {
+  if (e.key === 'Enter') {
+    addTask(addTaskInput.value);
+    clearInput();
+  }
+});
+
