032b5af85171ca9e9076bb6b548b06481a9bb058
AlyssonSene <alysson_silva@outlook.com>
2021-04-19 16:09:23 -0300

    adiciona botao apaga tudo

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 function newTask() {
   const li = document.createElement('li');
+  li.className = "checked";
   const inputValue = document.getElementById('texto-tarefa').value;
   const text = document.createTextNode(inputValue);
   li.appendChild(text);
@@ -15,7 +16,6 @@ function newTask() {
   span.className = "close";
   span.appendChild(txt);
   li.appendChild(span);
-
   for (let index = 0; index < close.length; index++) {
     close[index].onclick = function() {
       var div = this.parentElement;
@@ -23,9 +23,7 @@ function newTask() {
     }
   }
 }
-
 var myNodelist = document.getElementsByTagName("li");
-
 for (let index = 0; index < myNodelist.length; index++) {
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -35,10 +33,28 @@ for (let index = 0; index < myNodelist.length; index++) {
 }
 
 var close = document.getElementsByClassName("close");
-
 for (let index = 0; index < close.length; index++) {
   close[index].onclick = function() {
     var div = this.parentElement;
     div.style.display = "none";
   }
 }
+
+function deleteAllButton()
+{
+     var apagar = document.createElement('BUTTON');
+     var buttonName = document.createTextNode("Apaga tudo");
+     apagar.id = "apaga-tudo";  
+     apagar.appendChild(buttonName);   
+     apagar.onclick = function()
+     {
+        let lista = document.getElementById('lista-tarefas');
+        console.log(lista);
+        lista.innerHTML = "";
+     }
+     let menu = document.getElementById('menu');
+     menu.appendChild(apagar);    
+     
+}
+
+deleteAllButton();
