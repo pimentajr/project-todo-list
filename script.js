810700f8b61171c5e890abdf9dad4060d0380be0
Taís Carvalho <taispcarvalho@gmail.com>
2021-04-19 11:48:01 -0300

    4, 5, 6 - adicionando item na lista,input nao esta zerando

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let listaTarefas = document.createElement('ol');
-listaTarefas.className = 'lista-tarefas';
-document.body.appendChild(listaTarefas);
+let listTarefas = document.getElementById('lista-tarefas'); //Lista a ser criada
+let criarTarefa = document.getElementById('criar-tarefa'); //BUTTON
+let textoTarefa = document.getElementById('texto-tarefa'); //INPUT
+
+// adicionando item na lista e zerando o input
+function criarItem() {
+    let listItem = document.createElement('li');
+    let inputValue = textoTarefa.value;
+
+    listItem.innerText = inputValue;
+    listTarefas.appendChild(listItem);
+    inputValue = '';
+}
+
+criarTarefa.addEventListener('click', criarItem)
