e51e7b1d42521e3027c40116a27cb34fbb67723e
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-19 01:36:31 -0300

    requisito 11 completado

--- script.js
+++ script.js
@@ -2,8 +2,10 @@ const INPUT_TEXTO_TAREFA = document.getElementById('texto-tarefa');
 const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
 const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
 const BTN_APAGA_TUDO = document.getElementById('apaga-tudo');
+const BTN_REMOVER_FINALIZADOS = document.getElementById('remover-finalizados');
 
 let lastTargetSelected;
+let completedTaskArray = [];
 
 function insertNewListItem() {
   const NEW_LI = document.createElement('li');
@@ -28,8 +30,16 @@ function completedTask(event) {
   const CURRENT_TARGET = event.target;
   if (CURRENT_TARGET.classList[0] === 'completed' || CURRENT_TARGET.classList[1] === 'completed') {
     CURRENT_TARGET.classList.remove('completed');
+    completedTaskArray.splice(completedTaskArray.length-1, 1);
   } else {
     CURRENT_TARGET.classList.add('completed');
+    completedTaskArray.push(CURRENT_TARGET);
+  }
+}
+
+function removeCompletedTasks() {
+  for (const INDEX of completedTaskArray) {
+    INDEX.remove();
   }
 }
 
@@ -40,6 +50,7 @@ function clearList() {
 window.onload = () => {
   BTN_CRIAR_TAREFA.addEventListener('click', insertNewListItem);
   BTN_APAGA_TUDO.addEventListener('click', clearList);
+  BTN_REMOVER_FINALIZADOS.addEventListener('click', removeCompletedTasks);
   OL_LISTA_TAREFAS.addEventListener('click', selectLi);
   OL_LISTA_TAREFAS.addEventListener('dblclick', completedTask);
 };
