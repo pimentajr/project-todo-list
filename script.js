9d81c32ed64898c2a9cfb7641ce617cb0890fe83
Luís Seixas <luiseixas2@gmail.com>
2021-04-19 09:53:28 -0300

    requisito 9

--- script.js
+++ script.js
@@ -11,14 +11,21 @@ function addTask() {
 const button = document.getElementById('criar-tarefa');
 button.addEventListener('click', addTask);
 
-function changeBackgroundLi(event) {
-  const getListItem = document.querySelectorAll('li');
-  const targetItem = event.target;
-  for (let index = 0; index < getListItem.length; index += 1) {
-    getListItem[index].classList.remove('selected');
+function changeBackgroundLi(e) {
+  const getListIten = document.querySelectorAll('li');
+  const targetIten = e.target;
+  for (let index = 0; index < getListIten.length; index += 1) {
+    getListIten[index].classList.remove('selected');
   }
-  targetItem.classList.add('selected');
+  targetIten.classList.add('selected');
 }
 
 const taskList = document.querySelector('#lista-tarefas');
-taskList.addEventListener('click', changeBackgroundLi);
+taskList.addEventListener('click', changeBackgroundLi); 
+
+function riskTask (e) {
+  e.target.classList.toggle('completed')
+}
+
+const element = document.querySelector('#lista-tarefas');
+element.addEventListener('dblclick', riskTask); 
