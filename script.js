d7d5b5f279b5bf188c0868e8664e0b58428bca94
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-19 14:16:27 -0300

    corrigindo erros

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function addToList() {
   const list = document.getElementById('texto-tarefa'); // onde é digitado a tarefa
   const listValue = list.value; // .value mostra que sera pego o conteudo dentro do input
   const itemList = document.createElement('li'); // cria uma tag li
-  if (listValue === '') {// se o list value estiver vazio e a pessoa tentar enviar, recebera o alerta
+  if (listValue === '') { // se o list value estiver vazio e a pessoa tentar enviar, recebera o alerta
     alert('Escreva uma Tarefa'); // nao aparece mais pois ao iniciar nao estou chamando a função
   } else {
     itemList.innerHTML = listValue; // itemlist irá receber o que foi digitado
@@ -16,7 +16,7 @@ buttonAdd.addEventListener('click', addToList);
 
 // requisitos 7 e 8
 function changeColor(evento) {
-  if(evento.target.tagName === 'LI') { // ira ocorrer nos elementos com a tag li
+  if (evento.target.tagName === 'LI') { // ira ocorrer nos elementos com a tag li
     for (let index = 0; index < orderList.children.length; index += 1) {
       orderList.children[index].classList.remove('selected'); // remove a classe selected dos itens ,quando o target é adicionado
     }
@@ -29,13 +29,13 @@ orderList.addEventListener('click', changeColor);
 function lineItem(event3) {
   if (event3.target.classList.contains('completed')) { // verifica se o target possui a classe completed
     event3.target.classList.remove('completed'); // se tiver sera removido a classe completed
-} else {
+  } else {
     event3.target.classList.add('completed'); // se for clicado e nao tiver a classe completed ela sera adicionada
   }
 }
 orderList.addEventListener('dblclick', lineItem);
 
-//*Consultei o repositório de islene Gomes para resolver esta parte
+// * Consultei o repositório de islene Gomes para resolver esta parte
 /* https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while*/
 //** Link: https://github.com/tryber/sd-011-project-todo-list/pull/15/files /*
 // requisito 10
@@ -51,7 +51,7 @@ buttonDelete.addEventListener('click', removeItens); // ao clicar no buttonDelet
 const removeFinished = document.getElementById('remover-finalizados');
 function removeFinishedItens() {
   const completedItens = document.querySelectorAll('.completed'); // pega todos os elementos da classe completed;
-  for (let index = 0; index < completedItens.length; index += 1) { // varre os elementos; 
+  for (let index = 0; index < completedItens.length; index += 1) { // varre os elementos;
     orderList.removeChild(completedItens[index]); // remove todos os filhos que possuem tal classe;
   }
 }
