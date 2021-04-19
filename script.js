3805f1d36fa7ee37674abcc959e0b4585080843d
João Victor Kikuti <joaovictorkikuti@gmail.com>
2021-04-18 21:05:02 -0300

    código otimizado e erros do lint arrumados

--- script.js
+++ script.js
@@ -1,33 +1,25 @@
-const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
-const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
 const INPUT_TEXTO_TAREFA = document.getElementById('texto-tarefa');
-const LI_LISTA_AFAZERES = document.getElementsByTagName('li');
-
-let lastTarget;
+const OL_LISTA_TAREFAS = document.getElementById('lista-tarefas');
+const BTN_CRIAR_TAREFA = document.getElementById('criar-tarefa');
 
 function insertNewListItem() {
   const NEW_LI = document.createElement('li');
   NEW_LI.innerText = INPUT_TEXTO_TAREFA.value;
   OL_LISTA_TAREFAS.appendChild(NEW_LI);
-  eraseInputValue();
-}
-
-function eraseInputValue() {
   INPUT_TEXTO_TAREFA.value = '';
 }
 
-function selectedLi(event) {
-  const currentTarget = event.target;
+function selectLi(event) {
+  const CURRENT_TARGET = event.target;
 
-  if (currentTarget.style.backgroundColor === "") {
-    currentTarget.style.backgroundColor = 'rgb(128, 128, 128)';
+  if (CURRENT_TARGET.style.backgroundColor === '') {
+    CURRENT_TARGET.style.backgroundColor = 'rgb(128, 128, 128)';
   } else {
-    currentTarget.removeAttribute("style");
+    CURRENT_TARGET.removeAttribute('style');
   }
 }
 
 window.onload = () => {
   BTN_CRIAR_TAREFA.addEventListener('click', insertNewListItem);
-
-  OL_LISTA_TAREFAS.addEventListener('click', selectedLi);
-}
+  OL_LISTA_TAREFAS.addEventListener('click', selectLi);
+};
