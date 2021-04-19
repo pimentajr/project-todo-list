7a0b1f7dc115264339b260d9a22996ca0497a139
NataliaSRiber <nataliaribeiro@unifei.edu.br>
2021-04-19 14:05:29 -0300

    corrigindo erros

--- script.js
+++ script.js
@@ -17,17 +17,17 @@ buttonAdd.addEventListener('click', addToList);
 // requisitos 7 e 8
 function changeColor(evento) {
   if(evento.target.tagName === 'LI') { // ira ocorrer nos elementos com a tag li
-    for (let index = 0; index < orderList.children.length; index +=1 ) {
-       orderList.children[index].classList.remove('selected'); // remove a classe selected dos itens ,quando o target é adicionado
+    for (let index = 0; index < orderList.children.length; index += 1) {
+      orderList.children[index].classList.remove('selected'); // remove a classe selected dos itens ,quando o target é adicionado
     }
     evento.target.classList.add('selected');
   }
-} 
+}
 orderList.addEventListener('click', changeColor);
 
 // requisito 9
 function lineItem(event3) {
-  if(event3.target.classList.contains('completed')) { // verifica se o target possui a classe completed
+  if (event3.target.classList.contains('completed')) { // verifica se o target possui a classe completed
     event3.target.classList.remove('completed'); // se tiver sera removido a classe completed
 } else {
     event3.target.classList.add('completed'); // se for clicado e nao tiver a classe completed ela sera adicionada
@@ -35,12 +35,14 @@ function lineItem(event3) {
 }
 orderList.addEventListener('dblclick', lineItem);
 
-// /*https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while */
+//*Consultei o repositório de islene Gomes para resolver esta parte
+/* https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while*/
+//** Link: https://github.com/tryber/sd-011-project-todo-list/pull/15/files /*
 // requisito 10
 const buttonDelete = document.getElementById('apaga-tudo'); // chama o botão de delete
 function removeItens() {
   while (orderList.children.length > 0) { // enquanto essa condição for verdadeira a condição sera avaliada;
-    orderList.children[0].remove();  // vai passando pela orderList e vai removendo o item na posição zero;
+    orderList.children[0].remove(); // vai passando pela orderList e vai removendo o item na posição zero;
   }
 }
 buttonDelete.addEventListener('click', removeItens); // ao clicar no buttonDelete a função sera executada;
@@ -48,9 +50,9 @@ buttonDelete.addEventListener('click', removeItens); // ao clicar no buttonDelet
 // requisito 11
 const removeFinished = document.getElementById('remover-finalizados');
 function removeFinishedItens() {
-  const completedItens = document.querySelectorAll('.completed');
-  for (let index = 0; index < completedItens.length; index += 1) {
-        orderList.removeChild(completedItens[index]);
-    }
+  const completedItens = document.querySelectorAll('.completed'); // pega todos os elementos da classe completed;
+  for (let index = 0; index < completedItens.length; index += 1) { // varre os elementos; 
+    orderList.removeChild(completedItens[index]); // remove todos os filhos que possuem tal classe;
+  }
 }
 removeFinished.addEventListener('click', removeFinishedItens);
