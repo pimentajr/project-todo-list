0f74a878baefa247eab9aad10d0903f00f6ed3fb
Diogo Bontempo <diogo_bontempo@hotmail.com>
2021-04-19 16:05:38 -0300

    questao 9

--- script.js
+++ script.js
@@ -17,7 +17,18 @@ adicionarNovaTarefa();
 
 //questa 07 e 08
 
-    
+//function paintGrey() {
+   // list.addEventListener('click', (event) => {
+    //  const selected = document.querySelector('.fundo-cinza');
+  
+     // if (selected != null) {
+     //   selected.classList.remove('fundo-cinza');
+      //}
+      //event.target.classList.add('fundo-cinza');
+    //});
+  //}
+  
+  //paintGrey();    
   
  
 //questao 9
