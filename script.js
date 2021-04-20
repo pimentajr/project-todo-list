05b1002f042f37959d7b485b229fabc928e35f95
JoaoCabrera <joaocabrerati@gmail.com>
2021-04-20 04:20:04 -0300

    Resolve requisito 7

--- script.js
+++ script.js
@@ -5,7 +5,18 @@ function taskAdd() {
   let newTask = document.createElement('li');
   newTask.innerText = document.getElementById('texto-tarefa').value;
   listaTarefas.appendChild(newTask);
-
   document.getElementById('texto-tarefa').value = '';
 }
 criarTarefa.addEventListener('click', taskAdd);
+
+listaTarefas.addEventListener('click', function(event) {
+  let tarefas = document.querySelectorAll('li')
+  for (let i = 0; i < tarefas.length; i += 1) {
+    tarefas[i].classList = 'noSelected';
+    
+    event.target.classList = 'selected'
+
+    let taskSelected = document.querySelector('.selected');
+    taskSelected.style.backgroundColor = 'rgb(128, 128, 128)'
+  }
+});
