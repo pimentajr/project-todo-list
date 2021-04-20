5e27e257bd86bd29884b09af3acbc72708a62632
Felipe Carvalho <fecarvalho066@gmail.com>
2021-04-20 11:53:00 -0300

    [Felipe Carvalho] - Requisitos 6 e 7

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const tarefa = document.getElementById('texto-tarefa');
 const botao = document.getElementById('criar-tarefa');
 const toDoList = document.getElementById('lista-tarefas');
+
+function selectedItem(event) {
+  const selected = document.querySelector('.selected');
+  if (selected !== null) {
+    selected.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
+}
+
 function listaDeTarefas() {
   if (tarefa.value !== '') {
     const tarefas = document.createElement('li');
     tarefas.innerText = tarefa.value;
     tarefa.value = '';
     toDoList.appendChild(tarefas);
+    tarefas.addEventListener('click', selectedItem);
   }
 }
 botao.addEventListener('click', listaDeTarefas);
