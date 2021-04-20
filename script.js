80aa96050639b55189e55d4955b9ba9a741f8cc0
nick <pereiranicolas713@gmail.com>
2021-04-20 01:49:53 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,20 +2,19 @@ const buttonadd = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
 // requisito 5 
-function addTask(event) {
+function addTask() {
   const item = document.createElement('li');
   item.className = 'item';
   taskList.appendChild(item);
   item.innerText = task.value;
   task.value = '';
-
 }
 
+buttonadd.addEventListener('click', addTask);
 // requisito 7 e 8
 function selectTask() {
   const task = document.querySelector('#lista-tarefas');
   const items = document.getElementsByTagName('li');
-
   task.addEventListener('click', (event) => {
     const selected = event.target;
     for (let index = 0; index < items.length; index += 1) {
@@ -26,4 +25,17 @@ function selectTask() {
 }
 
 selectTask();
-buttonadd.addEventListener('click', addTask);
+//9
+function concludedTask() {
+  taskList.addEventListener('dblclick',(event)=> {
+    const conclued = event.target;
+    if(conclued.classList.contains('completed')) {
+      conclued.classList.remove('completed');
+    } else {
+      conclued.classList.add('completed');
+    }
+  })
+}
+concludedTask();
+
+
