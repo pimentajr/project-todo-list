773517776ce851e84e325fdd033e0ad9c7765876
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-20 06:37:10 -0300

    requisito9

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ let toDoContainer = document.getElementById('lista-tarefas');
 let inputField = document.getElementById("texto-tarefa");
 
 addToDoButton.addEventListener('click', function(){
+    
     var paragraph = document.createElement('li');
     paragraph.classList.add('paragraph-styling');
+    paragraph.setAttribute("id", "list-item");
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
+    
     paragraph.addEventListener('dblclick', function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
+        paragraph.classList.add('linha');
+        paragraph.classList.remove('linha');
     })
     paragraph.addEventListener('click', function(){
         paragraph.classList.add('grayRGD')
     })
+    
+
 })
+
 //codigo que me estudei: https://www.youtube.com/watch?v=n3X7m0GzFRY&ab_channel=CodingMindBrasil
