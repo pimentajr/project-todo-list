079d6c54ac2b2038c2ca2bef4861d1aa38c26ede
nick <pereiranicolas713@gmail.com>
2021-04-20 02:59:47 -0300

    ajeitando para entregar

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const buttonadd = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const task = document.getElementById('texto-tarefa');
 const buttonClear = document.getElementById('apaga-tudo');
-// requisito 5 
+
 function addTask() {
   const item = document.createElement('li');
   item.className = 'item';
@@ -10,11 +10,11 @@ function addTask() {
   item.innerText = task.value;
   task.value = '';
 }
-// requisito 7 e 8
+
 function selectTask() {
-  const task = document.querySelector('#lista-tarefas');
+  const taskSelect = document.querySelector('#lista-tarefas'); 
   const items = document.getElementsByTagName('li');
-  task.addEventListener('click', (event) => {
+  taskSelect.addEventListener('click', (event) => {
     const selected = event.target;
     for (let index = 0; index < items.length; index += 1) {
       items[index].style.backgroundColor = '';
@@ -24,25 +24,23 @@ function selectTask() {
 }
 selectTask();
 buttonadd.addEventListener('click', addTask);
-//9
+
 function concludedTask() {
-  taskList.addEventListener('dblclick',(event)=>{
+  taskList.addEventListener('dblclick', (event) => {
     const conclued = event.target;
-    if(conclued.classList.contains('completed')) {
+    if (conclued.classList.contains('completed')) {
       conclued.classList.remove('completed');
-    }else {
+    } else {
       conclued.classList.add('completed');
     }
-  })
-};
+  });
+}
 concludedTask();
-//10 
-function clear (){
+
+function clear () {
   const itemCheck = document.getElementsByClassName('item');
-  for(let index = itemCheck.length - 1; index >= 0; index -= 1) {
+  for (let index = itemCheck.length - 1; index >= 0; index -= 1) {
     itemCheck[index].remove();
   }
 }
-buttonClear.addEventListener('click',clear);
-
-
+buttonClear.addEventListener('click', clear);
