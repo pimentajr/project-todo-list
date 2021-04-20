defe597ae4e1d0698ece6c88984c2c88a383c44e
Lucas Santos <lucassantos_dev@outlook.com>
2021-04-20 06:41:39 -0300

    hm

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
-let tarefaInput = document.getElementById('texto-tarefa');
-let botaoAdd = document.getElementById('criar-tarefa');
-let listaTarefas = document.getElementById('lista-tarefas');
+const tarefaInput = document.getElementById('texto-tarefa');
+const botaoAdd = document.getElementById('criar-tarefa');
+const listaTarefas = document.getElementById('lista-tarefas');
+const taskList = document.getElementsByClassName('item');
 
 function criarTarefa() {
   if (tarefaInput.value === '') {
-        return false;
-    }
+    return false;
+  }
   // Criando um novo elemento
-    let newItem = document.createElement('li');
-    // adicionando o valor do input ao elemento criando
-    newItem.textContent = tarefaInput.value;
-    // limpando input
-    tarefaInput.value = '';
-    //  listaTarefas é pai de newItem
-    listaTarefas.appendChild(newItem);
+  const newItem = document.createElement('li');
+  // TENTANDO ADD CLASSE
+  newItem.classList.add('item');
+  // adicionando o valor do input ao elemento criando
+  newItem.textContent = tarefaInput.value;
+  // limpando input
+  tarefaInput.value = '';
+  //  listaTarefas é pai de newItem
+  listaTarefas.appendChild(newItem);
 }
 
-// add evento de click, passando a função acima
+function isSelected(event) {
+  for (let i = 0; i < taskList.length; i += 1) {
+    taskList[i].classList = 'noSelected';
+    event.target.classList = 'selected';
+    const taskSelected = document.querySelector('.selected');
+    taskSelected.style.backgroundColor = 'rgb(128, 128, 128)';
+    if (taskList[i].className === 'noSelected') {
+      taskList[i].style.backgroundColor = 'white';
+    }
+  }
+}
 botaoAdd.addEventListener('click', criarTarefa);
-
+taskList.addEventListener('click', isSelected);
