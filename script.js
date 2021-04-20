369c37e1d496b6d25d4ac72a3c65c3e9cf458e9d
Peter Leite <peterleite.dev@gmail.com>
2021-04-19 23:14:39 -0300

    task 6 e 7 adaptei a funçao que usei no pixel art

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const newTaskButton = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 
-
 newTaskButton.addEventListener('click', addTask);
 
 function addTask() {
-  let newTaskItem = document.createElement('li');
+  const newTaskItem = document.createElement('li');
   newTaskItem.innerText = document.getElementById('texto-tarefa').value;
   taskList.appendChild(newTaskItem);
 
   document.getElementById('texto-tarefa').value = '';
 }
+
+function classChanger (event) {
+  let tasks = document.querySelectorAll('li')
+  for (let index = 0; index < tasks.length; index += 1) {
+    tasks[index].className = '';
+  }
+  let taskDone = event.target;
+  taskDone.className = 'selected';
+}
+
+taskList.addEventListener('click', classChanger)
