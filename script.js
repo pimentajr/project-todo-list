08b8676f5b301eb7cc918b779af489213a315f40
Halysson2 <halyssonsena83@gmail.com>
2021-04-20 10:45:21 -0300

    recomeçando o projeto com um comitão bomba

--- script.js
+++ script.js
@@ -1,70 +1,26 @@
-const taskAdded = document.getElementById('criar-tarefa');
-const text = document.getElementById('texto-tarefa');
-const listeOl = document.getElementById('lista-tarefas');
-const resetEverything = document.getElementById('apaga-tudo')
-const removeFinished = document.getElementById('remove-finalizados')
-const moverUp = document.getElementById('mover-cima')
-const moverDown = document.getElementById('mover-baixo')
-// correta
-taskAdded.addEventListener('click', () => {
-    const newLi = document.createElement('li');
-newLi.innerText = text.value
-listeOl.appendChild(newLi);
-text.value = ''
+const listaDeTarefas = document.getElementById('lista-tarefas');
+const criarTarefa = document.getElementById('criar-tarefa');
 
-})
+function addTask () {
 
-function selectColor() {
-    listeOl.addEventListener('click', (event) =>{  
-        const toColor = event.toColor;
-        const selectedColor = document.querySelector('.selected');
-        if(selectColor) {
-            selectColor.classList.remove('selected');
-        }
-       event.toColor.classList.add('selected');
-  });
-}     
+  let taskNew = document.createElement('li');
+  taskNew.innerText = document.getElementById('texto-tarefa').value;
+  listaDeTarefas.appenchild(taskNew);
+
+  document.getElementById('texto-tarefa').value = '';
+}
+
+criarTarefa.addEventListener('click', addTask);
+
+listaDeTarefas.addEventListener('click', function(event){
+  let tarefas = document.querySelectorAll('li')
+  for(let index = 0; index < tarefas.length; index += 1){
+    tarefas[index].classList = 'selectedNo';
     
-function completingTask() {
-listeOl.addEventListener('dblclick', (event) => {
-    const aList = event.target.classList;
-    if(aList.contains('completed')) {
-        aList.remove('completed');
-    } else {
-        aList.add('completed');
-    }
-  });
-}
+    event.target.classList = 'selected'
 
-function deleteTasks() {
-  resetEverything.addEventListener('click', () => {
-    listeOl.innerHTML = ''
-  });
-}
-
-//remove finalizados
-function deleteAllTasks(e) {
-  const removeFinished = e.target.classList;
-  if(e.target.classList[2]=== 'completed') {
-    e.target.classList.remove('completed');
-  }else{
-    e.target.classList.add('completed')
+    let selectedTask = document.querySelector('.selected');
+    selectedTask.getElementsByClassName.backgroundcolor = 'rgb(128, 128, 128)' 
+    
   }
-}
-
-moverUp.addEventListener('click', () => {
-  const selected = document.querySelector('.selected');
-  if (selected) {
-    const father = selected.parentNode;
-    if (selected.previousElementSibling) {
-      father.insertBefore(selected, selected.previousElementSibling);
-    }
-  }
-});
-
-listeOl.addEventListener('click', selectColor)
-listeOl.addEventListener('click', completingTask)
-selectColor();
-completingTask();
-deleteTasks();
-deleteAllTasks;
+})
