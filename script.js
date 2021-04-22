4f25008b1320028ef2faf654f84f8f2cdccfec8f
Ana Jacomassi <anajacomassip@gmail.com>
2021-04-22 14:54:38 -0300

    Adiciona e remove line-through duplo click item

--- script.js
+++ script.js
@@ -12,18 +12,29 @@ taskButton.addEventListener('click', () => {
 const taskList = document.getElementById('lista-tarefas');
 // Adicionando evento para saber se algum item na lista de tarefas foi clicado
 taskList.addEventListener('click', () => {
-// guardando em clickedItem o item que foi clicado
+  // guardando em clickedItem o item que foi clicado
   let clickedItem = event.target;
-// Listar elementos li
-let liCollection = document.getElementsByTagName('li');
-// caminahndo na lista de tarefas
-for (let index = 0; index < liCollection.length; index++) {
-// verifica se algum item possui backGround cinza 
-  if (liCollection[index].style.backgroundColor === 'rgb(128, 128, 128)') {
-// alterando para branco
-    liCollection[index].style.backgroundColor = 'white';
+  // Listar elementos li
+  let liCollection = document.getElementsByTagName('li');
+  // caminahndo na lista de tarefas
+  for (let index = 0; index < liCollection.length; index++) {
+    // verifica se algum item possui backGround cinza 
+    if (liCollection[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      // alterando para branco
+      liCollection[index].style.backgroundColor = 'white';
+    }
   }
-}
-// item clicado pinta de cinza
-clickedItem.style.backgroundColor = 'rgb(128, 128, 128)';
+  // item clicado pinta de cinza
+  clickedItem.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+
+
+// evento para reconhecer duplo clickedItem
+taskList.addEventListener('dblclick', () => {
+  let dblClickedItem = event.target;
+  if (dblClickedItem.classList[0] === 'completed') {
+    dblClickedItem.classList.remove('completed');    
+  } else {
+    dblClickedItem.classList.add('completed');
+  }
 })
