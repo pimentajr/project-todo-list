07958eaefafe112d730831f2ac4e3bed31277f5b
Halysson2 <halyssonsena83@gmail.com>
2021-04-22 14:39:23 -0300

    adicionando requisitos 1, 2 e 3

--- script.js
+++ script.js
@@ -1,64 +1,18 @@
-const taskAdded = document.getElementById('criar-tarefa');
-const text = document.getElementById('texto-tarefa');
-const listeOl = document.getElementById('lista-tarefas');
-const resetEverything = document.getElementById('apaga-tudo')
-const removeFinished = document.getElementById('remove-finalizados')
-const moverUp = document.getElementById('mover-cima')
-const moverDown = document.getElementById('mover-baixo')
-// correta
-taskAdded.addEventListener('click', () => {
-    const newLi = document.createElement('li');
-newLi.innerText = text.value
-listeOl.appendChild(newLi);
-text.value = ''
+const title = document.createElement('header');
+title.innerText = 'Minha Lista de Tarefas';
+document.body.appendChild (title)
 
-})
+const paragrafo = document.createElement('p');
+paragrafo.innerText = 'Clique duas vezes para marca-lo como completo';
+paragrafo.id = 'funcionamento';
+document.body.appendChild(paragrafos)
 
-function selectColor() {
-    listeOl.addEventListener('click', (event) =>{  
-        const toColor = event.target;
-        const selectedColor = document.querySelector('.selected');
-        if(selectColor) {
-            selectColor.classList.remove('.selected');
-        }
-     
-        event.target.classList.add('.selected');
-        
-  });
-}     
-selectColor();
-    
-function completingTask() {
-listeOl.addEventListener('dblclick', (event) => {
-    const aList = event.target.classList;
-    if(aList.contains('completed')) {
-        aList.remove('completed');
-    } else {
-        aList.add('completed');
-    }
-  });
+function createTextInput (){
+  const div = document.createElement('div');
+ document.body.appendChild(div);
+ const creatInput = document.createElement('input');
+  creatInput.setAttribute('type', 'text');
+  creatInput.id = 'texto-tarefa';
+div.appendChild(input)
 }
-completingTask()
-
-function deleteTasks() {
-  resetEverything.addEventListener('click', () => {
-    listeOl.innerHTML = ''
-  });
-}
-deleteTasks()
-
-//remove finalizados
-function deleteAllTasks(e) {
-  const removeFinished = e.target.classList;
-  if(e.target.classList[2]=== 'completed') {
-    e.target.classList.removeEventListener('completed');
-  }else{
-    e.target.classList.add('completed')
-  }
-}
-
-deleteAllTasks()
-
-listeOl.addEventListener('click', selectColor)
-listeOl.addEventListener('dblclick', completingTask)
-
+createTextInput();
