dfca03437d9165b68c1e397220e47c057890b01a
Rafael Bamberg <raafaelbamberg@gmail.com>
2021-04-27 16:33:26 -0300

    requisitos 789

--- script.js
+++ script.js
@@ -3,75 +3,39 @@ let toDoContainer = document.getElementById('lista-tarefas');
 let inputField = document.getElementById("texto-tarefa");
 let eraseList = document.getElementById("apaga-tudo")
 
+function selectItem(event) {
+    let selectOne = document.querySelector('.grayRGB');
+    if (selectOne !== null) {
+        selectOne.classList.remove('grayRGB');
+    }
+    event.target.classList.add('grayRGB');
+}
+
+function completeTask(event) {
+    console.log(event.target)
+    if(event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+    } else {
+        event.target.classList.add('completed');
+    }
+}
+
 addToDoButton.addEventListener('click', function(){
     
-    var paragraph = document.createElement('li');
-    paragraph.classList.add('paragraph-styling');
-    paragraph.setAttribute("id", "list-item");
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
+    let listItem = document.createElement('li');
+    listItem.classList.add('paragraph-styling');
+    listItem.setAttribute('id', "list-item");
+    listItem.innerText = inputField.value;
+    toDoContainer.appendChild(listItem);
     inputField.value = "";
     
-    paragraph.addEventListener('dblclick', function(){
-        paragraph.classList.add('linha');
-    });
-    paragraph.addEventListener('click', function(){
-        paragraph.classList.add('grayRGD')
-    });
+    listItem.addEventListener('dblclick', completeTask);
+    listItem.addEventListener('click', selectItem);
    
 })
 
+eraseList.addEventListener('click',function(e){
+   toDoContainer.innerHTML = '';
 });
-
-function selecionarTarefa() {
-  lista.addEventListener('click', (event) => {
-    const acao = event.target;
-    const itemSelecionado = document.querySelector('.selected');
-    if (itemSelecionado) {
-      itemSelecionado.classList.remove('selected');
-    }
-    acao.classList.add('selected');
-  });
-}
-selecionarTarefa();
-
-function tarefaFinalizada() {
-  lista.addEventListener('dblclick', (event) => {
-    const acao = event.target;
-    if (acao.classList.contains('completed')) {
-      acao.classList.remove('completed');
-    } else {
-      acao.classList.add('completed');
-    }
-  });
-}
-tarefaFinalizada();
-
-
-function deleteAllList() {
-  const deleteTudo = document.getElementById('apaga-tudo');
-
-  deleteTudo.addEventListener('click', () => {
-    const itemLista = document.querySelectorAll('.itemLista');
-    for (let index = 0; index < itemLista.length; index += 1) {
-      lista.removeChild(itemLista[index]);
-    }
-  });
-}
-
-deleteAllList();
-
-function deleteSelecionados() {
-  const delSelecao = document.getElementById('remover-finalizados');
-
-  delSelecao.addEventListener('click', () => {
-    const itemLista = document.querySelectorAll('.completed');
-    for (let index = 0; index < itemLista.length; index += 1) {
-      lista.removeChild(itemLista[index]);
-    }
-  });
-}
-
-deleteSelecionados();
 
 //codigo que me estudei: https://www.youtube.com/watch?v=n3X7m0GzFRY&ab_channel=CodingMindBrasil
