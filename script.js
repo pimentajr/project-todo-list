2175cc2ee8e629db3fed2b10c4ec4f2366865b3c
Xenon-Neko <darkankher@gmail.com>
2021-04-28 09:46:47 -0300

    Requisito 7 ok

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
  const text = document.getElementById('texto-tarefa');
  const list = document.getElementById('lista-tarefas');
 
-
-
  // Função adicionar tarefas.
 
 function adicionarTarefas() {
@@ -12,4 +10,11 @@ function adicionarTarefas() {
   tarefa.innerText = text.value;
   list.appendChild(tarefa);
   text.value = ' ';
+  tarefa.addEventListener('click', selecionarTarefa);
+}
+
+ // Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'
+
+  function selecionarTarefa(event) {
+    event.target.classList.toggle('selecionado');
 }
