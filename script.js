fee174c3fe3efa2bb21d4775c6e6c5ce257ed5b5
Caio-Veloso <caio.veloso@gmail.com>
2021-04-28 10:40:48 -0300

    Questao 7

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 const btnCreateTask = document.querySelector('#criar-tarefa');
 const inputTask = document.querySelector('#texto-tarefa');
 const olTask = document.querySelector('#lista-tarefas');
+const liTask = document.createElement('li');
 
 btnCreateTask.addEventListener('click', () => {
-  const liTask = document.createElement('li');
   liTask.innerText = inputTask.value;
   olTask.appendChild(liTask);
   inputTask.value = '';
 });
+
+liTask.addEventListener('click', () => {
+  liTask.style.backgroundColor = 'rgb(128,128,128)';
+})  
