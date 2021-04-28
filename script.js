5624eb9f02ed204ae5a31bfede9dde1dee3494dd
Xenon-Neko <darkankher@gmail.com>
2021-04-28 10:26:33 -0300

    Requisito 9 ok

--- script.js
+++ script.js
@@ -10,21 +10,31 @@ function adicionarTarefas() {
   tarefa.innerText = text.value;
   list.appendChild(tarefa);
   text.value = ' ';
-  tarefa.addEventListener('click', selecionarTarefa);
+  tarefa.addEventListener('click', selecionarTarefa); 
+  tarefa.addEventListener('dblclick', tarefaConcluida);
 }
 
  // Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'.
 
-  function selecionarTarefa(event) {
-    retirarClasse() ;
-    event.target.classList.toggle('selecionado');
+function selecionarTarefa(sTarefa) {
+  retirarClasse() ;
+  sTarefa.target.classList.toggle('selecionado');
 }
 
-//Função que retira da classe 'selecionado' uma tarefa previamente selecionada caso uma nova tarefa seja selecionada. 
+ //Função que retira da classe 'selecionado' uma tarefa previamente selecionada caso uma nova tarefa seja selecionada. 
 function retirarClasse() {
-    const tarefaSelecionada = document.querySelectorAll('.selecionado');
-    let index = ' ';
-    for (index = 0; index < tarefaSelecionada.length; index += 1) {
-        tarefaSelecionada[index].classList.remove('selecionado');
-    }
-  }
+  const tarefaSelecionada = document.querySelectorAll('.selecionado');
+  let index = ' ';
+  for (index = 0; index < tarefaSelecionada.length; index += 1) {
+    tarefaSelecionada[index].classList.remove('selecionado');
+}
+}
+
+// Função que ao dar duplo clique deixa a tarefa riscada, indicando que a mesma foi concluída.
+function tarefaConcluida(cTarefa) {
+    if (cTarefa.className === 'completed') {
+      cTarefa.target.classList.remove('completed');
+    } else {
+      cTarefa.target.classList.toggle('completed');
+}
+}
