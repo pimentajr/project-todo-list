f4f717fd4c8b2969f38d943bc3163dcd07b5b78f
YanParoni <yan.almeida.paroni@gmail.com>
2021-04-28 08:01:09 -0300

    linter

--- script.js
+++ script.js
@@ -5,83 +5,84 @@ const clearCompleted = document.querySelector('#remover-finalizados');
 const saveTasks = document.querySelector('#salvar-tarefas');
 const removeSelected = document.querySelector('#remover-selecionado');
 const input = document.querySelector('#texto-tarefa');
-const lis =document.querySelector('.lis');
 const upMove = document.getElementById('mover-cima');
-const moveDown = document.getElementById('mover-baixo')
+const moveDown = document.getElementById('mover-baixo');
 
-    createTask.addEventListener('click', () => {
-    if (input.value === '') {
-      alert('Insira uma tarefa')
-    } else {
-    let li = document.createElement('li');
+createTask.addEventListener('click', () => {
+  if (input.value === '') {
+    alert('Insira uma tarefa');
+  } else {
+    const li = document.createElement('li');
     localStorage.setItem('input', input.value);
     li.innerText = localStorage.getItem('input');
     li.classList.add('lis');
     list.appendChild(li);
     input.value = '';
-    }})
+  }
+});
 
-    list.addEventListener('dblclick',clearComp);
-    list.addEventListener('click', selectedTask);
-
-    function selectedTask(event){
-     let list = document.querySelectorAll('.lis');
-      list.forEach(element => {
-          if(element.classList.contains('selected')){
-              element.classList.remove('selected')
-          }
-      });
-       event.target.classList.add('selected')
+function selectedTask(event) {
+  const lista = document.querySelectorAll('.lis');
+  lista.forEach((element) => {
+    if (element.classList.contains('selected')) {
+      element.classList.remove('selected');
     }
+  });
+  event.target.classList.add('selected');
+}
+list.addEventListener('click', selectedTask);
 
-    removeSelected.addEventListener('click',clearSelected);
-    function clearSelected(){
-        let selected = document.getElementsByClassName('lis');
-        for(index = 0;index <selected.length; index +=1){
-            if(selected[index].classList.contains('selected')){
-                selected[index].parentNode.removeChild(selected[index])
-            }
-        }
+function clearSelected() {
+  const selected = document.getElementsByClassName('lis');
+  for (let index = 0; index < selected.length; index += 1) {
+    if (selected[index].classList.contains('selected')) {
+      selected[index].parentNode.removeChild(selected[index]);
     }
-    
-    upMove.addEventListener('click', upItem);
-    function upItem(){
-        let selected = document.querySelector('.selected');
-            if(selected.previousSibling !==null){
-                list.insertBefore(selected,selected.previousElementSibling);
-            }
-        }
-    
-    moveDown.addEventListener('click',downItem)
-        function downItem(){
-            let selected = document.querySelector('.selected');
-            if (selected.nextSibling !==null){
-                list.insertBefore(selected.nextSibling,selected)
-            }
-        }
+  }
+}
+removeSelected.addEventListener('click', clearSelected);
 
-        function clearComp(event){
-            if(event.target.classList.contains('completed')){
-                event.target.classList.remove('completed')
-                event.target.classList.remove('selected')
-            } else{
-                event.target.classList.add('completed')
-            }}
-        
-            clearCompleted.addEventListener('click', removeComp);
-            function removeComp(){
-                let selected = document.getElementsByClassName('completed');
-                let count = 0;
-               while(selected.length){
-                selected[count].parentNode.removeChild(selected[count])
-               }
-            }
+function upItem() {
+  const selected = document.querySelector('.selected');
+  if (selected.previousSibling !== null) {
+    list.insertBefore(selected, selected.previousElementSibling);
+  }
+}
+upMove.addEventListener('click', upItem);
 
-            clearTasks.addEventListener('click',removeAll);
-            function removeAll(){
-                let selected = document.getElementsByClassName('lis');
-                let count = 0;
-                while(selected.length){
-                    selected[count].parentNode.removeChild(selected[count]);
-                }
-            }
+function downItem() {
+  const selected = document.querySelector('.selected');
+  if (selected.nextSibling !== null) {
+    list.insertBefore(selected.nextSibling, selected);
+  }
+}
+moveDown.addEventListener('click', downItem);
+
+function clearComp(event) {
+  if (event.target.classList.contains('completed')) {
+    event.target.classList.remove('completed');
+    event.target.classList.remove('selected');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
+list.addEventListener('dblclick', clearComp);
+
+function removeComp() {
+  const selected = document.getElementsByClassName('completed');
+  const count = 0;
+  while (selected.length) {
+    selected[count].parentNode.removeChild(selected[count]);
+  }
+}
+clearCompleted.addEventListener('click', removeComp);
+
+function removeAll() {
+  const selected = document.getElementsByClassName('lis');
+  const count = 0;
+  while (selected.length) {
+    selected[count].parentNode.removeChild(selected[count]);
+  }
+}
+
+clearTasks.addEventListener('click', removeAll);
