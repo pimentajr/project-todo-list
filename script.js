b8be9ecc4ec05e18313d9ee4146442f4696943aa
Eduardo-A-P-Teixeira <eduardo.apteixeira00@gmail.com>
2021-04-28 13:01:30 -0300

    Requisitos 1 ao 10 - verificando 9

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-const buttonAdd = document.getElementById('criar-tarefa');
+window.onload = () => {
+  backgroundSelected();
+  finishedItem();
+  fapagaTudo();
+//  clearComplet();
+//  saveList();
+//  clearSelec();
+};
 
+const buttonAdd = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefas');
 
 function addTask() {
   const inputTask = document.getElementById('texto-tarefa');
   const newTask = document.createElement('li');
   newTask.innerText = inputTask.value;
-  const taskList = document.getElementById('lista-tarefas');
   taskList.appendChild(newTask);
   inputTask.value = '';
 }
 
 buttonAdd.addEventListener('click', addTask);
+
+function backgroundSelected() {
+  taskList.addEventListener('click', (event) => {
+    const paintGrey = event.target;
+    const selectedTask = document.querySelectorAll('li');
+    for (let index = 0; index < selectedTask.length; index += 1) {
+      selectedTask[index].classList.remove('selected');
+    }
+    paintGrey.classList.add('selected');
+  });
+}
+
+function finishedItem() {
+  taskList.addEventListener('dblclick', (event) => {
+    const crossTask = event.target;
+    crossTask.classList.toggle('completed');
+  });
+}
+
+function fapagaTudo() {
+  const apagaTudo = document.querySelector('#apaga-tudo');
+  apagaTudo.addEventListener('click', () => {
+    taskList.innerHTML = '';
+  });
+}
