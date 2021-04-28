c28d65083163ff266f9f32331583b8bcbb8c3e30
Xenon-Neko <darkankher@gmail.com>
2021-04-28 10:04:09 -0300

    Requisito 8 ok

--- script.js
+++ script.js
@@ -13,8 +13,18 @@ function adicionarTarefas() {
   tarefa.addEventListener('click', selecionarTarefa);
 }
 
- // Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'
+ // Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'.
 
   function selecionarTarefa(event) {
+    retirarClasse() ;
     event.target.classList.toggle('selecionado');
 }
+
+//Função que retira da classe 'selecionado' uma tarefa previamente selecionada caso uma nova tarefa seja selecionada. 
+function retirarClasse() {
+    const tarefaSelecionada = document.querySelectorAll('.selecionado');
+    let index = ' ';
+    for (index = 0; index < tarefaSelecionada.length; index += 1) {
+        tarefaSelecionada[index].classList.remove('selecionado');
+    }
+  }
