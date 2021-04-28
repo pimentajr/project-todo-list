2bc84a667e68dad6efae1948335030cab87c577e
Xenon-Neko <darkankher@gmail.com>
2021-04-28 12:37:00 -0300

    Codigo formatado

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
- //Constantes declaradas que referenciam a um id.
- 
- const text = document.getElementById('texto-tarefa');
- const list = document.getElementById('lista-tarefas');
+//Constantes declaradas que referenciam a um id.
 
- // Função adicionar tarefas.
+const text = document.getElementById('texto-tarefa');
+const list = document.getElementById('lista-tarefas');
+
+// Função adicionar tarefas.
 
 function adicionarTarefas() {
   const tarefa = document.createElement('li');
@@ -11,53 +11,52 @@ function adicionarTarefas() {
   tarefa.innerText = text.value;
   list.appendChild(tarefa);
   text.value = ' ';
-  tarefa.addEventListener('click', selecionarTarefa); 
+  tarefa.addEventListener('click', selecionarTarefa);
   tarefa.addEventListener('dblclick', tarefaConcluida);
 }
 
- // Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'.
+// Função que deixa o item selecionado e atribui ao elemento a classe 'selecionado'.
 
 function selecionarTarefa(sTarefa) {
-  retirarClasse() ;
+  retirarClasse();
   sTarefa.target.classList.toggle('selecionado');
 }
 
- //Função que retira da classe 'selecionado' uma tarefa previamente selecionada caso uma nova tarefa seja selecionada. 
+//Função que retira da classe 'selecionado' uma tarefa previamente selecionada caso uma nova tarefa seja selecionada. 
 
- function retirarClasse() {
+function retirarClasse() {
   const tarefaSelecionada = document.querySelectorAll('.selecionado');
   let index = ' ';
   for (index = 0; index < tarefaSelecionada.length; index += 1) {
     tarefaSelecionada[index].classList.remove('selecionado');
-}
+  }
 }
 
 // Função que ao dar duplo clique deixa a tarefa riscada, indicando que a mesma foi concluída.
 
 function tarefaConcluida(cTarefa) {
-    if (cTarefa.className === 'completed') {
-      cTarefa.target.classList.remove('completed');
-    } else {
-      cTarefa.target.classList.toggle('completed');
-}
+  if (cTarefa.className === 'completed') {
+    cTarefa.target.classList.remove('completed');
+  } else {
+    cTarefa.target.classList.toggle('completed');
+  }
 }
 
 // Função que é executada ao clique do botão 'apagar', limpando a lista de tarefas
 
 function apagarTarefas() {
-    const tarefas = document.querySelectorAll('.tarefa');
-    if (tarefas.length !== 0) {
-      list.innerHTML = ' ';
-    }
+  const tarefas = document.querySelectorAll('.tarefa');
+  if (tarefas.length !== 0) {
+    list.innerHTML = ' ';
   }
+}
 
- // Função que é executada ao clique do botão 'apagar tarefas concluídas' apagando somente as tarefas marcadas como finalizadas.
+// Função que é executada ao clique do botão 'apagar tarefas concluídas' apagando somente as tarefas marcadas como finalizadas.
 
- function apagarTarefasFinalizadas() {
-    const tarefasFinalizadas = document.querySelectorAll('.completed');
-    let index = ' ';
-    for (let index = 0; index < tarefasFinalizadas.length; index += 1) {
-        tarefasFinalizadas[index].remove();
-    }
+function apagarTarefasFinalizadas() {
+  const tarefasFinalizadas = document.querySelectorAll('.completed');
+  let index = ' ';
+  for (let index = 0; index < tarefasFinalizadas.length; index += 1) {
+    tarefasFinalizadas[index].remove();
   }
-
+}
