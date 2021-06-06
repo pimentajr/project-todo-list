9057540ad7e2a8886b87f5eff7e905f5ff3e2ed6
Laura Gusmao <laurargus@gmail.com>
2021-06-05 21:23:01 -0300

    Adiciona botoes de salvar lista e deletar lista salva

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
+const listaTarefas = 'lista-tarefas';
 /**
- * Cria elemento li contendo o texto (string) recedido e adiciona a classe 'task'
+ * Cria elemento li contendo o texto (string) recebido e adiciona a classe 'task'
  */
 function createTask(text) {
   const task = document.createElement('li');
@@ -11,7 +12,7 @@ function createTask(text) {
  * Adiciona valor do input (tarefa) à lista de tarefas
  */
 function addTask() {
-  const listItems = document.getElementById('lista-tarefas');
+  const listItems = document.getElementById(listaTarefas);
   const input = document.getElementById('texto-tarefa');
   if (input.value === '') {
     alert('Digite uma tarefa.');
@@ -22,6 +23,9 @@ function addTask() {
   }
 }
 
+/**
+ * Ao disparadado um evento, remove classe 'selected' de todos os elementos, caso existam, e adiciona classe 'selected' no elemento alvo do evento.
+ */
 function taskClassSelected(event) {
   const selected = document.querySelectorAll('.selected');
   for (let index = 0; index < selected.length; index += 1) {
@@ -30,18 +34,28 @@ function taskClassSelected(event) {
   event.target.classList.add('selected');
 }
 
+/**
+ * Ao disparado um evento, caso exista a classe 'completed' no elemento alvo do evento, ela é removida. Caso contrário, se ela não exista, a mesma é adicionada ao elemento.
+ */
 function taskClassCompleted(event) {
   const task = event.target;
   task.classList.toggle('completed');
 }
 
+/**
+ * Remove todos os elementos com a classe 'task'.
+ */
 function deleteList() {
   const items = document.querySelectorAll('.task');
-  for (let index = 0; index < items.length; index += 1) {
-    items[index].remove();
-  }
+  // for (let index = 0; index < items.length; index += 1) {
+  //   items[index].remove();
+  // }
+  items.forEach((item) => item.remove());
 }
 
+/**
+ * Remove todos os elementos com classe 'completed'.
+ */
 function removeCompleted() {
   const completed = document.querySelectorAll('.completed');
   for (let index = 0; index < completed.length; index += 1) {
@@ -49,14 +63,52 @@ function removeCompleted() {
   }
 }
 
+function saveTasks() {
+  const itemsList = document.getElementsByTagName('li');
+  for (let index = 0; index < itemsList.length; index += 1) {
+    itemsList[index].classList.add('saved-item');
+    const obj = {
+      text: itemsList[index].innerText,
+      class: itemsList[index].className,
+    };
+    localStorage.setItem(index, JSON.stringify(obj));
+  }
+  alert('Lista salva!');
+}
+
+function getTasks() {
+  const list = document.getElementById(listaTarefas);
+  for (let index = 0; index < localStorage.length; index += 1) {
+    const listItem = document.createElement('li');
+    const obj = JSON.parse(localStorage.getItem(index));
+    listItem.innerText = obj.text;
+    listItem.className = obj.class;
+    list.appendChild(listItem);
+  }
+}
+
+function deleteSavedTasks() {
+  localStorage.clear();
+  const itemsList = document.getElementsByTagName('li');
+  for (let index = 0; index < itemsList.length; index += 1) {
+    itemsList[index].classList.remove('saved-item');
+  }
+  alert('Lista apagada!');
+}
+
 window.onload = function init() {
   const addButton = document.querySelector('#criar-tarefa');
   addButton.addEventListener('click', addTask);
-  const listItems = document.getElementById('lista-tarefas');
+  const listItems = document.getElementById(listaTarefas);
   listItems.addEventListener('click', taskClassSelected);
   listItems.addEventListener('dblclick', taskClassCompleted);
   const deleteButton = document.querySelector('#apaga-tudo');
   deleteButton.addEventListener('click', deleteList);
   const removeCompletedButton = document.querySelector('#remover-finalizados');
   removeCompletedButton.addEventListener('click', removeCompleted);
+  const saveButton = document.getElementById('salvar-tarefas');
+  saveButton.addEventListener('click', saveTasks);
+  getTasks();
+  const deleteSavedButton = document.getElementById('apaga-lista-salva');
+  deleteSavedButton.addEventListener('click', deleteSavedTasks);
 };
